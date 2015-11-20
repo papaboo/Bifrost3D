f43ad1660800de4f53efb74c1212a6f7dfcf5c17
Asger Hoedt <asgerhoedt@gmail.com>
2015-11-20 21:28:52 +0100

    Added typedefs for double vectors

--- cogs/Cogwheel/Math/Vector.h
+++ cogs/Cogwheel/Math/Vector.h
@@ -190,10 +190,13 @@ inline bool almostEqual(Vector4<T> lhs, Vector4<T> rhs, unsigned short maxUlps =
 // Typedefs.
 //*************************************************************************
 
+typedef Vector2<double> Vector2d;
 typedef Vector2<float> Vector2f;
 typedef Vector2<int> Vector2i;
+typedef Vector3<double> Vector3d;
 typedef Vector3<float> Vector3f;
 typedef Vector3<int> Vector3i;
+typedef Vector4<double> Vector4d;
 typedef Vector4<float> Vector4f;
 typedef Vector4<int> Vector4i;
 
