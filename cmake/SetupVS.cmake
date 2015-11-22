220c9d3b911bc929d90b8820448a69dea2aa440b
Asger Hoedt <asgerhoedt@gmail.com>
2015-11-22 19:58:55 +0100

    OpenMP sis setup for all projects

--- cmake/SetupVS.cmake
+++ cmake/SetupVS.cmake
@@ -1,3 +1,9 @@
 if(MSVC)
+  find_package(OpenMP REQUIRED)
+  if (OPENMP_FOUND)
+    # Add OpenMP to Viual Studio compiler flags
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+  endif()
+
   add_definitions(-D_SCL_SECURE_NO_WARNINGS) # VS should not warn about 'unsafe' calls to std::_Copy_impl
 endif()
