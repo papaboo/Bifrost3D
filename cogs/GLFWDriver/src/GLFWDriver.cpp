4d42b97d01287758a6766b447bda642f6b083885
Asger Hoedt <asgerhoedt@gmail.com>
2015-12-21 22:10:12 +0100

    * Split GLFWDriver into a header and implementation file. (Yes Window and Engine are still inside the header. We'll move the later)

---
+++ cogs/GLFWDriver/src/GLFWDriver.cpp
@@ -0,0 +1,113 @@
+// Cogwheel GLFW main.
+// ----------------------------------------------------------------------------
+// Copyright (C) 2015, Cogwheel. See AUTHORS.txt for authors
+//
+// This program is open source and distributed under the New BSD License. See
+// LICENSE.txt for more detail.
+// ----------------------------------------------------------------------------
+
+#include <GLFWDriver.h>
+
+#include <glfw/glfw3.h>
+
+#include <Input/Keyboard.h>
+#include <Input/Mouse.h>
+
+using Cogwheel::Core::Engine;
+using Cogwheel::Input::Keyboard;
+using Cogwheel::Input::Mouse;
+using Cogwheel::Math::Vector2i;
+
+static Keyboard* g_keyboard = NULL;
+static Mouse* g_mouse = NULL;
+
+namespace GLFWDriver {
+
+void run(on_launch_callback on_launch) {
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    // TODO Splash screen. With possibility to print resource processing text.
+
+    // Create engine.
+    Engine engine = Engine();
+    on_launch(engine);
+
+    Cogwheel::Core::Window& engine_window = engine.get_window();
+    GLFWwindow* window = glfwCreateWindow(engine_window.get_width(), engine_window.get_height(), engine_window.get_name().c_str(), NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    // glfwSetWindowSizeCallback(window, windowSizeCallback);
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    { // Setup keyboard
+        g_keyboard = new Keyboard();
+        engine.set_keyboard(g_keyboard);
+        GLFWkeyfun keyboard_callback = [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+            if (action == GLFW_REPEAT)
+                return;
+            g_keyboard->key_tapped(Keyboard::Key(key), action == GLFW_PRESS);
+        };
+        glfwSetKeyCallback(window, keyboard_callback);
+    }
+
+    { // Setup mouse
+        double mouse_pos_x, mouse_pos_y;
+        glfwGetCursorPos(window, &mouse_pos_x, &mouse_pos_y);
+        g_mouse = new Mouse(Vector2i(int(mouse_pos_x), int(mouse_pos_y)));
+        engine.set_mouse(g_mouse);
+        
+        static GLFWcursorposfun mouse_position_callback = [](GLFWwindow* window, double x, double y) {
+            g_mouse->set_position(Vector2i(int(x), int(y)));
+        };
+        glfwSetCursorPosCallback(window, mouse_position_callback);
+
+        static GLFWmousebuttonfun mouse_button_callback = [](GLFWwindow* window, int button, int action, int mods) {
+            if (action == GLFW_REPEAT || button > Mouse::BUTTON_COUNT)
+                return;
+
+            g_mouse->button_tapped(button, action == GLFW_PRESS);
+        };
+        glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+        static GLFWscrollfun mouse_scroll_callback = [](GLFWwindow* window, double horizontalScroll, double verticalScroll) {
+            g_mouse->add_scroll_delta(float(verticalScroll));
+        };
+        glfwSetScrollCallback(window, mouse_scroll_callback);
+    }
+
+    double previous_time = glfwGetTime();
+
+    while (!glfwWindowShouldClose(window)) {
+        g_keyboard->per_frame_reset();
+        g_mouse->per_frame_reset();
+        glfwPollEvents();
+
+        // Poll and update time.
+        double current_time = glfwGetTime();
+        float delta_time = float(current_time - previous_time);
+        previous_time = current_time;
+            
+        engine.do_loop(delta_time);
+
+        glfwSwapBuffers(window);
+
+        if (engine.requested_quit())
+            glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    // Cleanup.
+    delete g_keyboard;
+    delete g_mouse;
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+} // NS GLFWDriver
--- cogs/GLFWDriver/src/GLFWDriver.h
+++ cogs/GLFWDriver/src/GLFWDriver.h
@@ -9,8 +9,6 @@
 #ifndef _COGWHEEL_GLFW_DRIVER_H_
 #define _COGWHEEL_GLFW_DRIVER_H_
 
-#include <glfw/glfw3.h>
-
 #include <Input/Keyboard.h>
 #include <Input/Mouse.h>
 
@@ -92,103 +90,11 @@ private:
 } // NS Core
 } // NS Cogwheel
 
-using Cogwheel::Core::Engine;
-using Cogwheel::Input::Keyboard;
-using Cogwheel::Input::Mouse;
-using Cogwheel::Math::Vector2i;
-
-static Keyboard* g_keyboard = NULL;
-static Mouse* g_mouse = NULL;
-
 namespace GLFWDriver {
 
 typedef void(*on_launch_callback)(Cogwheel::Core::Engine& engine);
-void run(on_launch_callback on_launch) {
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
 
-    // TODO Splash screen. With possibility to print resource processing text.
-
-    // Create engine.
-    Engine engine = Engine();
-    on_launch(engine);
-
-    Cogwheel::Core::Window& engine_window = engine.get_window();
-    GLFWwindow* window = glfwCreateWindow(engine_window.get_width(), engine_window.get_height(), engine_window.get_name().c_str(), NULL, NULL);
-
-    if (!window) {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-
-    // glfwSetWindowSizeCallback(window, windowSizeCallback);
-
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-
-    { // Setup keyboard
-        g_keyboard = new Keyboard();
-        engine.set_keyboard(g_keyboard);
-        GLFWkeyfun keyboard_callback = [](GLFWwindow* window, int key, int scancode, int action, int mods) {
-            if (action == GLFW_REPEAT)
-                return;
-            g_keyboard->key_tapped(Keyboard::Key(key), action == GLFW_PRESS);
-        };
-        glfwSetKeyCallback(window, keyboard_callback);
-    }
-
-    { // Setup mouse
-        double mouse_pos_x, mouse_pos_y;
-        glfwGetCursorPos(window, &mouse_pos_x, &mouse_pos_y);
-        g_mouse = new Mouse(Vector2i(int(mouse_pos_x), int(mouse_pos_y)));
-        engine.set_mouse(g_mouse);
-        
-        static GLFWcursorposfun mouse_position_callback = [](GLFWwindow* window, double x, double y) {
-            g_mouse->set_position(Vector2i(int(x), int(y)));
-        };
-        glfwSetCursorPosCallback(window, mouse_position_callback);
-
-        static GLFWmousebuttonfun mouse_button_callback = [](GLFWwindow* window, int button, int action, int mods) {
-            if (action == GLFW_REPEAT || button > Mouse::BUTTON_COUNT)
-                return;
-
-            g_mouse->button_tapped(button, action == GLFW_PRESS);
-        };
-        glfwSetMouseButtonCallback(window, mouse_button_callback);
-
-        static GLFWscrollfun mouse_scroll_callback = [](GLFWwindow* window, double horizontalScroll, double verticalScroll) {
-            g_mouse->add_scroll_delta(float(verticalScroll));
-        };
-        glfwSetScrollCallback(window, mouse_scroll_callback);
-    }
-
-    double previous_time = glfwGetTime();
-
-    while (!glfwWindowShouldClose(window)) {
-        g_keyboard->per_frame_reset();
-        g_mouse->per_frame_reset();
-        glfwPollEvents();
-
-        // Poll and update time.
-        double current_time = glfwGetTime();
-        float delta_time = float(current_time - previous_time);
-        previous_time = current_time;
-            
-        engine.do_loop(delta_time);
-
-        glfwSwapBuffers(window);
-
-        if (engine.requested_quit())
-            glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-
-    // Cleanup.
-    delete g_keyboard;
-    delete g_mouse;
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-}
+void run(on_launch_callback on_launch);
 
 } // NS GLFWDriver
 
