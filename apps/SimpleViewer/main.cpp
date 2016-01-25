5d8a202f77086c7dcfe8c93762b13ebb9cb30993
Asger Hoedt <asgerhoedt@gmail.com>
2016-01-25 22:39:28 +0100

    Camera navigation module.

--- apps/SimpleViewer/main.cpp
+++ apps/SimpleViewer/main.cpp
@@ -4,46 +4,102 @@
 #include <Core/IModule.h>
 #include <Input/Keyboard.h>
 #include <Input/Mouse.h>
+#include <Scene/Camera.h>
 
 #include <OptiXRenderer/Renderer.h>
 
 #include <cstdio>
 #include <iostream>
 
-using namespace Cogwheel;
+using namespace Cogwheel::Core;
+using namespace Cogwheel::Input;
+using namespace Cogwheel::Math;
+using namespace Cogwheel::Scene;
 
-class DebugInput final : public Core::IModule {
+class Navigation final : public IModule {
 public:
 
-    void apply() override {
-        Core::Engine* engine = Core::Engine::get_instance();
+    Navigation(SceneNodes::UID node_ID) 
+        : m_node_ID(node_ID)
+        , m_vertical_rotation(0.0f) 
+        , m_horizontal_rotation(0.0f)
+    { }
 
-        int keys_pressed = 0;
-        int halftaps = 0;
-        for (int k = 0; k < (int)Input::Keyboard::Key::KeyCount; ++k) {
-            keys_pressed += engine->get_keyboard()->is_pressed(Input::Keyboard::Key(k));
-            halftaps += engine->get_keyboard()->halftaps(Input::Keyboard::Key(k));
+    void apply() override {
+        Engine* engine = Engine::get_instance();
+        const Keyboard* keyboard = engine->get_keyboard();
+        const Mouse* mouse = engine->get_mouse();
+
+        SceneNode node = m_node_ID;
+        Transform transform = node.get_global_transform();
+
+        { // Translation
+            float strafing = 0.0f;
+            if (keyboard->is_pressed(Keyboard::Key::A))
+                strafing = engine->get_time().get_smooth_delta_time();
+            if (keyboard->is_pressed(Keyboard::Key::D))
+                strafing -= engine->get_time().get_smooth_delta_time();
+
+            float forward = 0.0f;
+            if (keyboard->is_pressed(Keyboard::Key::W))
+                forward = engine->get_time().get_smooth_delta_time();
+            if (keyboard->is_pressed(Keyboard::Key::S))
+                forward -= engine->get_time().get_smooth_delta_time();
+
+            Vector3f translation_offset = transform.rotation * Vector3f(strafing, 0.0f, forward);
+            transform.translation += translation_offset;
         }
 
-        printf("Keys held down %u and total halftaps %u\n", keys_pressed, halftaps);
+        { // Rotation
+            if (mouse->get_left_button().is_pressed) {
+                m_vertical_rotation += degrees_to_radians(mouse->get_delta().x);
+
+                // Clamp horizontal rotation to -89 and 89 degrees to avoid turning the camera on it's head and the singularities of cross products at the poles.
+                m_horizontal_rotation += degrees_to_radians(mouse->get_delta().y);
+                m_horizontal_rotation = clamp(m_horizontal_rotation, -PI<float>() * 0.49f, PI<float>() * 0.49f);
+
+                transform.rotation = Quaternionf::from_angle_axis(m_vertical_rotation, Vector3f::up()) * Quaternionf::from_angle_axis(m_horizontal_rotation, Vector3f::right());
+            }
+        }
+
+        node.set_global_transform(transform);
     }
 
     std::string get_name() override {
-        return "Input Debugger";
+        return "Navigation";
     }
+
+private:
+    SceneNodes::UID m_node_ID;
+    float m_vertical_rotation;
+    float m_horizontal_rotation;
 };
 
+
 void initializer(Cogwheel::Core::Engine& engine) {
-    std::cout << "Initialize baby!" << std::endl;
     engine.get_window().set_name("SimpleViewer");
 
-    // engine.add_mutating_module(new DebugInput());
+    SceneNodes::allocate(64u);
+
+    { // Add camera
+        Cameras::allocate(1u);
+        SceneNodes::UID cam_node_ID = SceneNodes::create("Cam");
+
+        Matrix4x4f perspective_matrix, inverse_perspective_matrix;
+        CameraUtils::compute_perspective_projection(0.1f, 100.0f, PI<float>() / 4.0f, 8.0f / 6.0f,
+            perspective_matrix, inverse_perspective_matrix);
+
+        Cameras::UID cam_ID = Cameras::create(cam_node_ID, perspective_matrix, inverse_perspective_matrix);
+
+        engine.add_mutating_module(new Navigation(cam_node_ID));
+    }
 }
 
 void initialize_window(Cogwheel::Core::Window& window) {
-    Core::Engine::get_instance()->add_non_mutating_module(new OptiXRenderer::Renderer());
+    Engine::get_instance()->add_non_mutating_module(new OptiXRenderer::Renderer());
 }
 
 void main(int argc, char** argv) {
+    
     GLFWDriver::run(initializer, initialize_window);
 }
