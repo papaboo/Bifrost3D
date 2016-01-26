4af59231f7ec6fb93901dafe3bce360faf0a6d18
Asger Hoedt <asgerhoedt@gmail.com>
2016-01-26 21:07:26 +0100

    ONly disable VS14 warnings if VS14 is detected.

--- cmake/SetupVS.cmake
+++ cmake/SetupVS.cmake
@@ -10,11 +10,15 @@ if(MSVC)
   add_definitions(-D_SCL_SECURE_NO_WARNINGS) # VS should not warn about 'unsafe' calls to std::_Copy_impl
   
   add_definitions(-wd4514) # Unreferenced inline function has been removed.
-  add_definitions(-wd4710) # Function not inlined
-  add_definitions(-wd4711) # Function selected for automatic inline expansion.
   
   add_definitions(-wd4625) # copy constructor was implicitly defined as deleted
   add_definitions(-wd4626) # assignment operator was implicitly defined as deleted
-  add_definitions(-wd5026) # move constructor was implicitly defined as deleted
-  add_definitions(-wd5027) # move assignment operator was implicitly defined as deleted
+
+  add_definitions(-wd4710) # Function not inlined
+  add_definitions(-wd4711) # Function selected for automatic inline expansion.
+
+  if (MSVC14)
+      add_definitions(-wd5026) # move constructor was implicitly defined as deleted
+      add_definitions(-wd5027) # move assignment operator was implicitly defined as deleted
+  endif()
 endif()
