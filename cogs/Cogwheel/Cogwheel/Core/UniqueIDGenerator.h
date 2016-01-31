a924bf6ceadca729c10ba9294b21e8793ca4e804
Asger Hoedt <asgerhoedt@gmail.com>
2016-01-31 12:54:49 +0100

    Add getters and setters to UID. UID data is accessed with bit operations for faster access.

--- cogs/Cogwheel/Cogwheel/Core/UniqueIDGenerator.h
+++ cogs/Cogwheel/Cogwheel/Core/UniqueIDGenerator.h
@@ -19,7 +19,8 @@ namespace Core {
 // but a unique ID is also created pr SceneNode to distinguish all nodes.
 // See http://bitsquid.blogspot.de/2011/09/managing-decoupling-part-4-id-lookup.html.
 // Future work
-// * We could generate something like OwnedUIDs, that can only be stored in one place an will erase themselves when they are no longer needed.
+// * Enable assert in has(UID). It requires the last free element to always point inside the array, preferably at the sentinel element. If that happens, then I need to special case erase() to the case where next_element is 0, because then last_element is invalid.
+// * We could generate something like OwnedUIDs, that can only be stored in one place and will erase themselves when they are no longer needed.
 // ** In order to avoid the user accidentally deleting an OwnedUID, the UID member would of course have to be unaccessable. (or require an owned ID for erase, but then it needs to be used everywhere!)
 //----------------------------------------------------------------------------
 template <typename T>
@@ -34,29 +35,33 @@ public:
     //------------------------------------------------------------------------
     struct UID final {
     private:
-        unsigned int m_incarnation : 8;
-        unsigned int m_ID : 24;
+        unsigned int m_ID_incarnation;
 
         // Make the TypedUIDGenerator a friend class to allow it to construct UIDs.
         friend class TypedUIDGenerator;
 
-        UID(unsigned int id, unsigned int incarnation) : m_incarnation(incarnation), m_ID(id) { }
+        UID(unsigned int id, unsigned int incarnation) : m_ID_incarnation((incarnation << 24u) | id) {}
+
+        inline void set_ID(unsigned int id) { m_ID_incarnation = (m_ID_incarnation & 0xFF000000) | id; }
+        inline unsigned int get_incarnation_count() const { return m_ID_incarnation >> 24u; }
+        inline void increment_incarnation() { m_ID_incarnation += 0x01000000; }
+
     public:
         static const unsigned int MAX_IDS = 0xFFFFFF;
 
         // Creates a sentinel UID that will never be valid.
-        UID() : m_incarnation(0u), m_ID(0u) { } // NOTE AVH Should really be private or non-existent, but that requires not using the UID in any containers that needs default initialization
+        UID() : m_ID_incarnation(0u) { } // NOTE AVH Should really be private or non-existent, but that requires not using the UID in any containers that needs default initialization
         static inline UID invalid_UID() { return UID(0u, 0u); }
 
         // The ID.
-        inline unsigned int ID() const { return m_ID; }
+        inline unsigned int get_ID() const { return m_ID_incarnation & MAX_IDS; }
 
         // Implicit conversion to unsigned int is a shorthand way of accessing the ID.
-        inline operator unsigned int() const { return m_ID; }
+        inline operator unsigned int() const { return get_ID(); }
 
         // Equality operator.
         inline bool operator==(const UID& rhs) const {
-            return m_ID == rhs.m_ID && m_incarnation == rhs.m_incarnation;
+            return m_ID_incarnation == rhs.m_ID_incarnation;
         }
     };
 
@@ -74,7 +79,7 @@ public:
             : m_id(id), m_UID_generator(UID_generator) { }
         inline ConstIterator& operator++() {
             ++m_id;
-            while (!m_UID_generator.has(*m_id) && (*this) != m_UID_generator.end())
+            while (*m_id < m_UID_generator.capacity() && !m_UID_generator.has(*m_id))
                 ++m_id;
             return *this;
         }
@@ -111,7 +116,7 @@ public:
 
     inline ConstIterator end() const { return ConstIterator(m_IDs + m_capacity, *this); }
 
-    // Debug! std::string UIDGenerator::to_string();
+    // Debug! std::string to_string();
 
 private:
     // Delete copy constructors to avoid having multiple versions of the same UID generator.
