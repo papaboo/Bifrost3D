d7496e5a9e972bb0b03765bd95b9e55cae81eb16
Asger Hoedt <asgerhoedt@gmail.com>
2016-03-28 19:27:58 +0200

    ObjLoader loads basic material info as well.

--- cogs/ObjLoader/ObjLoader/ObjLoader.cpp
+++ cogs/ObjLoader/ObjLoader/ObjLoader.cpp
@@ -10,13 +10,17 @@
 
 #include <ObjLoader/ObjLoader.h>
 
+#include <Cogwheel/Assets/Material.h>
 #include <Cogwheel/Assets/Mesh.h>
 #include <Cogwheel/Assets/MeshModel.h>
+#include <Cogwheel/Core/Array.h>
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <ObjLoader/tiny_obj_loader.h>
 
+using namespace Cogwheel;
 using namespace Cogwheel::Assets;
+using namespace Cogwheel::Core;
 using namespace Cogwheel::Scene;
 
 namespace ObjLoader {
@@ -46,28 +50,42 @@ SceneNodes::UID load(const std::string& path) {
     split_path(directory, filename, path);
 
     std::vector<tinyobj::shape_t> shapes;
-    std::vector<tinyobj::material_t> materials;
+    std::vector<tinyobj::material_t> tiny_materials;
     std::string error;
-    bool obj_loaded = tinyobj::LoadObj(shapes, materials, error, path.c_str(), directory.c_str());
+    bool obj_loaded = tinyobj::LoadObj(shapes, tiny_materials, error, path.c_str(), directory.c_str());
 
     if (!error.empty()) {
-        printf("ObjLoader::load error: %s.\n", error.c_str());
+        printf("ObjLoader::load error: '%s'.\n", error.c_str());
         if (!obj_loaded)
             return SceneNodes::UID::invalid_UID();
     }
 
     SceneNodes::UID root_ID = shapes.size() > 1u ? SceneNodes::create(std::string(filename.begin(), filename.end()-4)) : SceneNodes::UID::invalid_UID();
 
-    for (size_t i = 0; i < shapes.size(); i++) {
+    Core::Array<Materials::UID> materials = Core::Array<Materials::UID>(unsigned int(tiny_materials.size()));
+    for (size_t i = 0; i < tiny_materials.size(); ++i) {
+        tinyobj::material_t tiny_mat = tiny_materials[i];
+
+        Materials::Data material_data;
+        material_data.base_color = Math::RGB(tiny_mat.diffuse[0], tiny_mat.diffuse[1], tiny_mat.diffuse[2]);
+        material_data.base_roughness = sqrt(sqrt(2.0f / (tiny_mat.shininess + 2.0f))); // Map from blinn shininess to material roughness.
+        material_data.metallic = 0.0f; // TODO Can I determine this at all? Maybe from the type ID?
+        material_data.specularity = (tiny_mat.specular[0] + tiny_mat.specular[1] + tiny_mat.specular[2]) / 3.0f;
+
+        materials[unsigned int(i)] = Materials::create(tiny_mat.name, material_data);
+    }
+
+    for (size_t i = 0; i < shapes.size(); ++i) {
+
+        tinyobj::mesh_t tiny_mesh = shapes[i].mesh;
 
         Meshes::UID mesh_ID = Meshes::UID::invalid_UID();
         { // Create mesh 
-            assert((shapes[i].mesh.indices.size() % 3) == 0); // Assert that indices are for triangles.
-            assert((shapes[i].mesh.positions.size() % 3) == 0); // Assert that positions are three dimensional.
-            assert((shapes[i].mesh.normals.size() % 3) == 0); // Assert that normals are three dimensional.
-            assert((shapes[i].mesh.texcoords.size() % 2) == 0); // Assert that texcoords are two dimensional.
+            assert((tiny_mesh.indices.size() % 3) == 0); // Assert that indices are for triangles.
+            assert((tiny_mesh.positions.size() % 3) == 0); // Assert that positions are three dimensional.
+            assert((tiny_mesh.normals.size() % 3) == 0); // Assert that normals are three dimensional.
+            assert((tiny_mesh.texcoords.size() % 2) == 0); // Assert that texcoords are two dimensional.
 
-            tinyobj::mesh_t tiny_mesh = shapes[i].mesh;
             mesh_ID = Meshes::create(shapes[i].name, unsigned int(tiny_mesh.indices.size() / 3u), unsigned int(tiny_mesh.positions.size() / 3u));
 
             // TODO Verify if any of these arrays are null.
@@ -86,7 +104,10 @@ SceneNodes::UID load(const std::string& path) {
         else
             root_ID = node_ID;
 
-        MeshModels::UID model_ID = MeshModels::create(node_ID, mesh_ID, Materials::UID::invalid_UID()); // TODO Handle materials properly.
+        Materials::UID material_ID = tiny_mesh.material_ids.size() == 0 ? 
+                                     Materials::UID::invalid_UID() : 
+                                     materials[tiny_mesh.material_ids[0]];
+        MeshModels::UID model_ID = MeshModels::create(node_ID, mesh_ID, material_ID);
     }
 
     return root_ID;
