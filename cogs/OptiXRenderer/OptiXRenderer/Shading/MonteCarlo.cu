f3f540aa0fec729505eea59ab1590caf8837b7c2
Asger Hoedt <asgerhoedt@gmail.com>
2016-04-21 18:17:50 +0200

    RIS implementation. Currently hardcoded to grab 4 samples. Could be dynamically adjusted based on PDF.

--- cogs/OptiXRenderer/OptiXRenderer/Shading/MonteCarlo.cu
+++ cogs/OptiXRenderer/OptiXRenderer/Shading/MonteCarlo.cu
@@ -41,10 +41,73 @@ rtDeclareVariable(float3, geometric_normal, attribute geometric_normal, );
 rtDeclareVariable(float3, shading_normal, attribute shading_normal, );
 rtDeclareVariable(float2, texcoord, attribute texcoord, );
 
+// Variables used for split screen debugging.
+rtDeclareVariable(uint2, g_launch_index, rtLaunchIndex, );
+rtBuffer<float4, 2>  g_accumulation_buffer;
+
+//-----------------------------------------------------------------------------
+// Light sampling.
+//-----------------------------------------------------------------------------
+
+// Sample a single light source, evaluates the material's response to the light and 
+// stores the combined response in the light source's radiance member.
+__inline_dev__ LightSample sample_single_light(const DefaultShading& material, const TBN& world_shading_tbn) {
+    int light_index = min(g_light_count - 1, int(monte_carlo_PRD.rng.sample1f() * g_light_count));
+    const SphereLight& light = g_lights[light_index];
+    LightSample light_sample = LightSources::sample_radiance(light, monte_carlo_PRD.position, monte_carlo_PRD.rng.sample2f());
+    light_sample.radiance *= g_light_count; // Scale up radiance to account for only sampling one light.
+
+    // Inline the material response into the light sample's radiance.
+    const float3 shading_light_direction = world_shading_tbn * light_sample.direction;
+    const float3 bsdf_response = material.evaluate(monte_carlo_PRD.direction, shading_light_direction);// TODO Extend material and BRDFs with methods for evaluating contribution and PDF at the same time.
+    light_sample.radiance *= bsdf_response;
+
+    float N_dot_L = dot(world_shading_tbn.get_normal(), light_sample.direction);
+    light_sample.radiance *= abs(N_dot_L) / light_sample.PDF;
+
+    // Apply MIS weights if the light isn't a delta function and if a new material ray will be spawned, i.e. it isn't the final bounce.
+    bool delta_light = LightSources::is_delta_light(light, monte_carlo_PRD.position);
+    bool apply_MIS = !delta_light && monte_carlo_PRD.bounces < g_max_bounce_count;
+    if (apply_MIS) {
+        float bsdf_PDF = material.PDF(monte_carlo_PRD.direction, shading_light_direction);
+        float mis_weight = RNG::power_heuristic(light_sample.PDF, bsdf_PDF); // TODO Check if the BSDF material PDF is valid. If it isn't we then disable MIS intirely? Or set contribution to black?
+
+        light_sample.radiance *= mis_weight;
+    }
+
+    return light_sample;
+}
+
+// Take multiple light samples and from that set pick one based on the contribution of the light scaled by the material.
+// TODO Link RIS paper.
+__inline_dev__ LightSample reestimated_light_samples(const DefaultShading& material, const TBN& world_shading_tbn, int samples) {
+    LightSample light_sample = sample_single_light(material, world_shading_tbn);
+    for (int s = 1; s < samples; ++s) {
+        LightSample new_light_sample = sample_single_light(material, world_shading_tbn);
+        float light_weight = average(light_sample.radiance);
+        float new_light_weight = average(new_light_sample.radiance);
+        float new_light_probability = new_light_weight / (light_weight + new_light_weight);
+        if (monte_carlo_PRD.rng.sample1f() < new_light_probability) {
+            light_sample = new_light_sample;
+            light_sample.radiance /= new_light_probability;
+        } else
+            light_sample.radiance /= 1.0f - new_light_probability;
+    }
+    light_sample.radiance /= samples;
+
+    // NOTE If we want to use the accumlated path PDF later for filtering or firefly removal, then it's possible that we'd get better results by adjusting the PDF instead of just the total contribution.
+
+    return light_sample;
+}
+
+//-----------------------------------------------------------------------------
+// Closest hit integrators.
+//-----------------------------------------------------------------------------
+
 __inline_dev__ void closest_hit_not_MIS() {
     // const float3 world_geometric_normal = normalize(rtTransformNormal(RT_OBJECT_TO_WORLD, geometric_normal));
     const float3 world_shading_normal = normalize(rtTransformNormal(RT_OBJECT_TO_WORLD, shading_normal));
-    const float3 forward_shading_normal = dot(world_shading_normal, -ray.direction) >= 0.0f ? world_shading_normal : -world_shading_normal;
+    const float3 forward_shading_normal = -dot(world_shading_normal, ray.direction) >= 0.0f ? world_shading_normal : -world_shading_normal;
 
     const TBN world_shading_tbn = TBN(forward_shading_normal);
 
@@ -56,6 +119,7 @@ __inline_dev__ void closest_hit_not_MIS() {
     DefaultShading material = DefaultShading(material_parameter);
 
     // Sample light sources.
+    // TODO Use RIS light sampling here as well. But wait until I have a scene with multiple area light sources.
     for (int i = 0; i < g_light_count; ++i) {
         const SphereLight& light = g_lights[i];
         LightSample light_sample = LightSources::sample_radiance(light, monte_carlo_PRD.position, monte_carlo_PRD.rng.sample2f());
@@ -91,7 +155,7 @@ __inline_dev__ void closest_hit_not_MIS() {
 __inline_dev__ void closest_hit_MIS() {
     // const float3 world_geometric_normal = normalize(rtTransformNormal(RT_OBJECT_TO_WORLD, geometric_normal));
     const float3 world_shading_normal = normalize(rtTransformNormal(RT_OBJECT_TO_WORLD, shading_normal));
-    const float3 forward_shading_normal = dot(world_shading_normal, -ray.direction) >= 0.0f ? world_shading_normal : -world_shading_normal;
+    const float3 forward_shading_normal = -dot(world_shading_normal, ray.direction) >= 0.0f ? world_shading_normal : -world_shading_normal;
 
     const TBN world_shading_tbn = TBN(forward_shading_normal);
 
@@ -100,33 +164,11 @@ __inline_dev__ void closest_hit_MIS() {
     monte_carlo_PRD.direction = world_shading_tbn * -ray.direction;
 
     const Material& material_parameter = g_materials[material_index];
-    DefaultShading material = DefaultShading(material_parameter);
+    const DefaultShading material = DefaultShading(material_parameter);
 
     // Sample a light source.
-    bool apply_MIS = false;
     if (g_light_count != 0) {
-        int light_index = min(g_light_count - 1, int(monte_carlo_PRD.rng.sample1f() * g_light_count));
-        const SphereLight& light = g_lights[light_index];
-        LightSample light_sample = LightSources::sample_radiance(light, monte_carlo_PRD.position, monte_carlo_PRD.rng.sample2f());
-        light_sample.radiance *= g_light_count; // Scale up radiance to account for only sampling one light.
-
-        // Inline the material response into the light sample's radiance.
-        const float3 shading_light_direction = world_shading_tbn * light_sample.direction;
-        const float3 bsdf_response = material.evaluate(monte_carlo_PRD.direction, shading_light_direction);// TODO Extend material and BRDFs with methods for evaluating contribution and PDF at the same time.
-        light_sample.radiance *= bsdf_response;
-
-        float N_dot_L = dot(world_shading_tbn.get_normal(), light_sample.direction);
-        light_sample.radiance *= abs(N_dot_L) / light_sample.PDF;
-
-        // Apply MIS weights if the light isn't a delta function and if a new material ray will be spawned, i.e. it isn't the final bounce.
-        bool delta_light = LightSources::is_delta_light(light, monte_carlo_PRD.position);
-        apply_MIS = !delta_light && monte_carlo_PRD.bounces < g_max_bounce_count;
-        if (apply_MIS) {
-            float bsdf_PDF = material.PDF(monte_carlo_PRD.direction, shading_light_direction);
-            float mis_weight = RNG::power_heuristic(light_sample.PDF, bsdf_PDF); // TODO Check if the BSDF material PDF is valid. If it isn't we then disable MIS intirely? Or set contribution to black?
-
-            light_sample.radiance *= mis_weight;
-        }
+        const LightSample light_sample = reestimated_light_samples(material, world_shading_tbn, 4);
 
         if (light_sample.radiance.x > 0.0f || light_sample.radiance.y > 0.0f || light_sample.radiance.z > 0.0f) {
             ShadowPRD shadow_PRD = { light_sample.radiance };
@@ -149,12 +191,9 @@ __inline_dev__ void closest_hit_MIS() {
     monte_carlo_PRD.bounces += 1u;
 }
 
-// rtDeclareVariable(uint2, g_launch_index, rtLaunchIndex, );
-// rtBuffer<float4, 2>  g_accumulation_buffer;
-
 RT_PROGRAM void closest_hit() {
     // if (g_launch_index.x * 2 < g_accumulation_buffer.size().x)
-        // closest_hit_not_MIS();
+    //     closest_hit_not_MIS();
     // else
         closest_hit_MIS();
 }
@@ -181,7 +220,7 @@ RT_PROGRAM void light_closest_hit() {
 
     float3 light_radiance = LightSources::evaluate(light, ray.origin, ray.direction);
 
-    bool next_event_estimated = monte_carlo_PRD.bounces != 0;
+    bool next_event_estimated = monte_carlo_PRD.bounces != 0; // Was next event estimated at previous intersection.
     bool apply_MIS = monte_carlo_PRD.bsdf_MIS_PDF > 0.0f;
     if (apply_MIS) {
         // Calculate MIS weight and scale the radiance by it.
