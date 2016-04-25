ebf5593ae9842d56462568db77b7d452bb9fd7c7
Asger Hoedt <asgerhoedt@gmail.com>
2016-04-25 10:21:46 +0200

    Remove unsigned char constructors from RGBA.

--- cogs/Cogwheel/Cogwheel/Math/Color.h
+++ cogs/Cogwheel/Cogwheel/Math/Color.h
@@ -191,9 +191,6 @@ public:
     RGBA()
         : r(0.0f), g(0.0f), b(0.0f), a(1.0f) { }
 
-    RGBA(unsigned char r, unsigned char g, unsigned char b, unsigned char a)
-        : r(r / 255.0f), g(g / 255.0f), b(b / 255.0f), a(a / 255.0f) { }
-
     RGBA(float r, float g, float b, float a)
         : r(r), g(g), b(b), a(a) { }
 
