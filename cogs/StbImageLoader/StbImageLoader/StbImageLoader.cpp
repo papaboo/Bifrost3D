bdee7647debeebf4c921a32f4a3675946e49c733
Asger Hoedt <asgerhoedt@gmail.com>
2016-05-03 16:35:40 +0200

    Stb image loader.

---
+++ cogs/StbImageLoader/StbImageLoader/StbImageLoader.cpp
@@ -0,0 +1,47 @@
+// Cogwheel stb image loader.
+// ---------------------------------------------------------------------------
+// Copyright (C) 2015-2016, Cogwheel. See AUTHORS.txt for authors
+//
+// This program is open source and distributed under the New BSD License. See
+// LICENSE.txt for more detail.
+// ---------------------------------------------------------------------------
+
+#pragma warning(disable : 4996)
+
+#include <StbImageLoader/StbImageLoader.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <StbImageLoader/stb_image.h>
+
+using namespace Cogwheel::Assets;
+using namespace Cogwheel::Math;
+
+namespace StbImageLoader {
+
+static PixelFormat resolve_format(int channels) {
+    switch (channels) {
+    case 4:
+        return PixelFormat::RGBA32;
+    case 3:
+        return PixelFormat::RGB24;
+    }
+    return PixelFormat::Unknown;
+}
+
+Images::UID load(const std::string& path) {
+    
+    int width, height, channels;
+    unsigned char* loaded_data = stbi_load(path.c_str(), &width, &height, &channels, 0);
+
+    PixelFormat pixel_format = resolve_format(channels);
+    if (pixel_format == PixelFormat::Unknown)
+        return Images::UID::invalid_UID();
+
+    Images::UID image_ID = Images::create(path, pixel_format, Vector2ui(width, height));
+    void* pixel_data = Images::get_pixels(image_ID);
+    memcpy(pixel_data, loaded_data, sizeof(unsigned char) * width * height * channels);
+
+    return image_ID;
+}
+
+} // NS StbImageLoader
---
+++ cogs/StbImageLoader/StbImageLoader/StbImageLoader.h
@@ -0,0 +1,27 @@
+// Cogwheel stb image loader.
+// ---------------------------------------------------------------------------
+// Copyright (C) 2015-2016, Cogwheel. See AUTHORS.txt for authors
+//
+// This program is open source and distributed under the New BSD License. See
+// LICENSE.txt for more detail.
+// ---------------------------------------------------------------------------
+
+#ifndef _COGWHEEL_ASSETS_STB_IMAGE_LOADER_H_
+#define _COGWHEEL_ASSETS_STB_IMAGE_LOADER_H_
+
+#include <Cogwheel/Assets/Image.h>
+#include <string>
+
+namespace StbImageLoader {
+
+// -----------------------------------------------------------------------
+// Loads an image file.
+// Basic support for png, exr, jpg and more.
+// Future work
+// * HDR support.
+// -----------------------------------------------------------------------
+Cogwheel::Assets::Images::UID load(const std::string& filename);
+
+} // NS StbImageLoader
+
+#endif // _COGWHEEL_ASSETS_STB_IMAGE_LOADER_H_
