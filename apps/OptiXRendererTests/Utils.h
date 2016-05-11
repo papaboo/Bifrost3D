ba47adfea010318ab6bff97b263aeb9b03bc3891
Asger Hoedt <asgerhoedt@gmail.com>
2016-05-11 19:38:05 +0200

    Normal and float3 comparison/equality helpers.

--- apps/OptiXRendererTests/Utils.h
+++ apps/OptiXRendererTests/Utils.h
@@ -9,9 +9,42 @@
 #ifndef _OPTIXRENDERERTEST_UTILS_H_
 #define _OPTIXRENDERERTEST_UTILS_H_
 
-#include <algorithm>
+#include <optixu/optixu_math_namespace.h>
 
-namespace OptiXRenderer {
+#include <algorithm>
+#include <string>
+
+//-----------------------------------------------------------------------------
+// Comparison helpers.
+//-----------------------------------------------------------------------------
+
+inline bool almost_equal_eps(float lhs, float rhs, float eps) {
+    return lhs < rhs + eps && lhs + eps > rhs;
+}
+
+static bool equal_float3(optix::float3 lhs, optix::float3 rhs) {
+    return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z;
+}
+
+#define EXPECT_FLOAT3_EQ(expected, actual) EXPECT_PRED2(equal_float3, expected, actual)
+
+static bool equal_normal(optix::float3 lhs, optix::float3 rhs, float epsilon) {
+    return abs(lhs.x - rhs.x) < epsilon && abs(lhs.y - rhs.y) < epsilon && abs(lhs.z - rhs.z) < epsilon;
+}
+
+#define EXPECT_NORMAL_EQ(expected, actual, epsilon) EXPECT_PRED3(equal_normal, expected, actual, epsilon)
+
+//-----------------------------------------------------------------------------
+// To string functions.
+//-----------------------------------------------------------------------------
+
+inline std::ostream& operator<<(std::ostream& s, const optix::float3 v) {
+    return s << "[x: " << v.x << ", y: " << v.y << ", z: " << v.z << "]";
+}
+
+//-----------------------------------------------------------------------------
+// Stable pairwise summation.
+//-----------------------------------------------------------------------------
 
 // Inplace iterative pairwise summation.
 // Uses the input iterators to store the temporaries.
@@ -40,11 +73,5 @@ typename std::iterator_traits<InputIterator>::value_type sort_and_pairwise_summa
     std::sort(begin, end);
     return pairwise_summation(begin, end);
 }
-
-inline bool almost_equal_eps(float lhs, float rhs, float eps) {
-    return lhs < rhs + eps && lhs + eps > rhs;
-}
-
-} // NS OptiXRenderer
 
 #endif // _OPTIXRENDERERTEST_UTILS_H_
