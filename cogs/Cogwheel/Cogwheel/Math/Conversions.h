5c3a7c8204895052013c0eceac6f7a8f07b534a8
Asger Hoedt <asgerhoedt@gmail.com>
2016-07-20 22:06:01 +0200

    Simpler matrix to quaternion conversion, based on the one found in pbrt.

--- cogs/Cogwheel/Cogwheel/Math/Conversions.h
+++ cogs/Cogwheel/Cogwheel/Math/Conversions.h
@@ -31,43 +31,38 @@ inline Matrix3x3<T> to_matrix3x3(Quaternion<T> q) {
 }
 
 // Constructs a Quaternion from a rotation matrix.
-// http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
+// See the quaternion constructor in pbrt v3: Quaternion::ToTransform().
 template <typename T>
 inline Quaternion<T> to_quaternion(const Matrix3x3<T> m) {
     // Compute the trace of the matrix
     T trace = m[0][0] + m[1][1] + m[2][2];
-
-    const T quarter = T(0.25);
-    const T half = T(0.5);
-    const T one = T(1);
-    const T two = T(2);
-    // Conversion depends on the trace sign
-    if (trace >= T(0)) {
-        T s = half / sqrt(trace + one);
-        return normalize(Quaternion<T>((m[2][1] - m[1][2]) * s,
-                                       (m[0][2] - m[2][0]) * s,
-                                       (m[1][0] - m[0][1]) * s,
-                                       quarter / s));
+    
+    if (trace > T(0)) {
+        // Compute w from matrix trace, then xyz
+        // 4w^2 = m[0][0] + m[1][1] + m[2][2] + m[3][3] (but m[3][3] == 1)
+        T s = std::sqrt(trace + T(1.0));
+        T real = s * T(0.5);
+        s = 0.5f / s;
+        Vector3<T> imaginary = Vector3<T>(m[2][1] - m[1][2],
+                                          m[0][2] - m[2][0],
+                                          m[1][0] - m[0][1]) * s;
+        return Quaternion<T>(imaginary, real);
     } else {
-        if (m[0][0] > m[1][1] && m[0][0] > m[2][2]) {
-            T s = two * sqrt(one + m[0][0] - m[1][1] - m[2][2]);
-            return normalize(Quaternion<T>(quarter * s,
-                                           (m[0][1] + m[1][0]) / s,
-                                           (m[0][2] + m[2][0]) / s,
-                                           (m[2][1] - m[1][2]) / s));
-        } else if (m[1][1] > m[2][2]) {
-            T s = two * sqrt(one + m[1][1] - m[0][0] - m[2][2]);
-            return normalize(Quaternion<T>((m[0][1] + m[1][0]) / s,
-                                           quarter * s,
-                                           (m[1][2] + m[2][1]) / s,
-                                           (m[0][2] - m[2][0]) / s));
-        } else {
-            T s = two * sqrt(one + m[2][2] - m[0][0] - m[1][1]);
-            return normalize(Quaternion<T>((m[0][2] + m[2][0]) / s,
-                                           (m[1][2] + m[2][1]) / s,
-                                           quarter * s,
-                                           (m[1][0] - m[0][1]) / s));
-        }
+        // Compute largest of x, y, or z, then remaining components
+        const int nxt[3] = { 1, 2, 0 };
+        int i = 0;
+        if (m[1][1] > m[0][0]) i = 1;
+        if (m[2][2] > m[i][i]) i = 2;
+        int j = nxt[i];
+        int k = nxt[j];
+        T s = std::sqrt((m[i][i] - (m[j][j] + m[k][k])) + 1.0f);
+        Vector3<T> imaginary;
+        imaginary[i] = s * 0.5f;
+        if (s != 0.f) s = 0.5f / s;
+        T real = (m[k][j] - m[j][k]) * s;
+        imaginary[j] = (m[j][i] + m[i][j]) * s;
+        imaginary[k] = (m[k][i] + m[i][k]) * s;
+        return Quaternion<T>(imaginary, real);
     }
 }
 
@@ -84,9 +79,9 @@ inline Matrix4x4f to_matrix4x4(Transform t) {
     const Matrix3x3f r = to_matrix3x3(t.rotation);
     const float s = t.scale;
     Matrix4x4f res = { s * r[0][0], s * r[0][1], s * r[0][2], t.translation.x,
-        s * r[1][0], s * r[1][1], s * r[1][2], t.translation.y,
-        s * r[2][0], s * r[2][1], s * r[2][2], t.translation.z,
-        0.0f, 0.0f, 0.0f, 1.0f };
+                       s * r[1][0], s * r[1][1], s * r[1][2], t.translation.y,
+                       s * r[2][0], s * r[2][1], s * r[2][2], t.translation.z,
+                       0.0f,        0.0f,        0.0f,        1.0f };
     return res;
 }
 } // NS Math
