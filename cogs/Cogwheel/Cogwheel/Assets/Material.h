90997c9e3438ac1ba6f571bcc437ab28bc2d1e84
Asger Hoedt <asgerhoedt@gmail.com>
2016-07-26 20:30:31 +0200

    Everything is inline., everything is cool when no functions are called, everything is inlined!

--- cogs/Cogwheel/Cogwheel/Assets/Material.h
+++ cogs/Cogwheel/Cogwheel/Assets/Material.h
@@ -19,7 +19,9 @@
 namespace Cogwheel {
 namespace Assets {
 
-// TODO Description
+// ---------------------------------------------------------------------------
+// Cogwheel material properties container.
+// ---------------------------------------------------------------------------
 class Materials final {
 public:
     typedef Core::TypedUIDGenerator<Materials> UIDGenerator;
@@ -40,14 +42,14 @@ public:
 
     static inline unsigned int capacity() { return m_UID_generator.capacity(); }
     static void reserve(unsigned int new_capacity);
-    static bool has(Materials::UID material_ID) { return m_UID_generator.has(material_ID); }
+    static inline bool has(Materials::UID material_ID) { return m_UID_generator.has(material_ID); }
 
     static Materials::UID create(const std::string& name, const Data& data);
     static void destroy(Materials::UID material_ID);
 
-    static ConstUIDIterator begin() { return m_UID_generator.begin(); }
-    static ConstUIDIterator end() { return m_UID_generator.end(); }
-    static Core::Iterable<ConstUIDIterator> get_iterable() { return Core::Iterable<ConstUIDIterator>(begin(), end()); }
+    static inline ConstUIDIterator begin() { return m_UID_generator.begin(); }
+    static inline ConstUIDIterator end() { return m_UID_generator.end(); }
+    static inline Core::Iterable<ConstUIDIterator> get_iterable() { return Core::Iterable<ConstUIDIterator>(begin(), end()); }
 
     static inline std::string get_name(Materials::UID material_ID) { return m_names[material_ID]; }
     static inline void set_name(Materials::UID material_ID, const std::string& name) { m_names[material_ID] = name; }
@@ -80,7 +82,7 @@ public:
     }
 
     typedef std::vector<UID>::iterator ChangedIterator;
-    static Core::Iterable<ChangedIterator> get_changed_materials() {
+    static inline Core::Iterable<ChangedIterator> get_changed_materials() {
         return Core::Iterable<ChangedIterator>(m_materials_changed.begin(), m_materials_changed.end());
     }
 
@@ -99,10 +101,10 @@ private:
     static std::vector<UID> m_materials_changed;
 };
 
+// ---------------------------------------------------------------------------
+// Material UID wrapper.
+// ---------------------------------------------------------------------------
 class Material final {
-private:
-    const Materials::UID m_ID;
-
 public:
     // -----------------------------------------------------------------------
     // Class management.
@@ -123,18 +125,21 @@ public:
     inline void set_name(const std::string& name) { Materials::set_name(m_ID, name); }
 
     inline Math::RGB get_base_tint() { return Materials::get_base_tint(m_ID); }
-    void set_base_tint(Math::RGB tint) { Materials::set_base_tint(m_ID, tint); }
+    inline void set_base_tint(Math::RGB tint) { Materials::set_base_tint(m_ID, tint); }
     inline Textures::UID get_base_tint_texture_ID() { return Materials::get_base_tint_texture_ID(m_ID); }
-    void set_base_tint(Textures::UID tint_texture_ID) { Materials::set_base_tint_texture_ID(m_ID, tint_texture_ID); }
+    inline void set_base_tint(Textures::UID tint_texture_ID) { Materials::set_base_tint_texture_ID(m_ID, tint_texture_ID); }
     inline float get_base_roughness() { return Materials::get_base_roughness(m_ID); }
-    void set_base_roughness(float roughness) { Materials::set_base_roughness(m_ID, roughness); }
+    inline void set_base_roughness(float roughness) { Materials::set_base_roughness(m_ID, roughness); }
     inline float get_specularity() { return Materials::get_specularity(m_ID); }
-    void set_specularity(float specularity) { Materials::set_specularity(m_ID, specularity); }
+    inline void set_specularity(float specularity) { Materials::set_specularity(m_ID, specularity); }
     inline float get_metallic() { return Materials::get_metallic(m_ID); }
-    void set_metallic(float metallic) { Materials::set_metallic(m_ID, metallic); }
+    inline void set_metallic(float metallic) { Materials::set_metallic(m_ID, metallic); }
 
     inline unsigned char get_changes() { return Materials::get_changes(m_ID); }
     inline bool has_changes(unsigned char change_bitmask) { return Materials::has_changes(m_ID, change_bitmask); }
+
+private:
+    const Materials::UID m_ID;
 };
 
 } // NS Assets
