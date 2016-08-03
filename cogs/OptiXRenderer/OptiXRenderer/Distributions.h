b5c440c4d2d36e6f2bab91fdb57e5aa2d758f09d
Asger Hoedt <asgerhoedt@gmail.com>
2016-08-03 21:20:18 +0200

    Added GGX distribution.

--- cogs/OptiXRenderer/OptiXRenderer/Distributions.h
+++ cogs/OptiXRenderer/OptiXRenderer/Distributions.h
@@ -22,7 +22,7 @@ struct __align__(16) DirectionalSample {
 };
 
 //==============================================================================
-// Uniform cone Distribution.
+// Uniform cone distribution.
 //==============================================================================
 namespace Cone {
 
@@ -44,27 +44,63 @@ __inline_all__ DirectionalSample sample(float cos_theta_max, optix::float2 rando
 } // NS Cone
 
 //==============================================================================
-// Cosine Distribution.
+// Cosine distribution.
 //==============================================================================
 namespace Cosine {
 
-__inline_all__ float PDF(float cos_theta) {
-    return cos_theta / PIf;
-}
+    __inline_all__ float PDF(float abs_cos_theta) {
+        return abs_cos_theta * RECIP_PIf;
+    }
 
-__inline_all__ DirectionalSample sample(optix::float2 random_sample) {
-    float phi = 2.0f * PIf * random_sample.x;
-    float r2 = random_sample.y;
-    float r = sqrt(1.0f - r2);
-    float z = sqrt(r2);
+    __inline_all__ DirectionalSample sample(optix::float2 random_sample) {
+        float phi = 2.0f * PIf * random_sample.x;
+        float r2 = random_sample.y;
+        float r = sqrt(1.0f - r2);
+        float z = sqrt(r2);
 
-    DirectionalSample res;
-    res.direction = optix::make_float3(cos(phi) * r, sin(phi) * r, z);
-    res.PDF = z / PIf;
-    return res;
-}
+        DirectionalSample res;
+        res.direction = optix::make_float3(cos(phi) * r, sin(phi) * r, z);
+        res.PDF = z * RECIP_PIf;
+        return res;
+    }
 
 } // NS Cosine
+
+//==============================================================================
+// GGX distribution.
+// Future work
+// * Reference equations in Walter07.
+//==============================================================================
+namespace GGX {
+
+    __inline_all__ float D(float alpha, float abs_cos_theta) {
+        float alpha_sqrd = alpha * alpha;
+        float cos_theta_sqrd = abs_cos_theta * abs_cos_theta;
+        float tan_theta_sqrd = optix::fmaxf(1.0f - cos_theta_sqrd, 0.0f) / cos_theta_sqrd;
+        float cos_theta_cubed = cos_theta_sqrd * cos_theta_sqrd;
+        float foo = alpha_sqrd + tan_theta_sqrd; // No idea what to call this.
+        return alpha_sqrd / (PIf * cos_theta_cubed * foo * foo);
+    }
+
+    __inline_all__ float PDF(float alpha, float abs_cos_theta) {
+        return D(alpha, abs_cos_theta) * abs_cos_theta;
+    }
+
+    __inline_all__ DirectionalSample sample(float alpha, optix::float2 random_sample) {
+        float phi = random_sample.y * (2.0f * PIf);
+
+        float tan_theta_sqrd = alpha * alpha * random_sample.x / (1.0f - random_sample.x);
+        float cos_theta = 1.0f / sqrt(1.0f + tan_theta_sqrd);
+
+        float r = sqrt(optix::fmaxf(1.0f - cos_theta * cos_theta, 0.0f));
+
+        DirectionalSample res;
+        res.direction = optix::make_float3(cos(phi) * r, sin(phi) * r, cos_theta);
+        res.PDF = PDF(alpha, cos_theta); // We have to be able to inline this to reuse some temporaries.
+        return res;
+    }
+
+} // NS GGX
 
 } // NS Distributions
 } // NS OptiXRenderer
