92cd59400409f13878d8d8f37c316bc57ede83f1
Asger Hoedt <asgerhoedt@gmail.com>
2016-08-03 21:22:48 +0200

    Slight optimization of GGX sample and using the GGX implementation in Distributions instead of own.

--- cogs/OptiXRenderer/OptiXRenderer/Shading/BSDFs/GGX.h
+++ cogs/OptiXRenderer/OptiXRenderer/Shading/BSDFs/GGX.h
@@ -9,6 +9,7 @@
 #ifndef _OPTIXRENDERER_BSDFS_MICROFACET_H_
 #define _OPTIXRENDERER_BSDFS_MICROFACET_H_
 
+#include <OptiXRenderer/Distributions.h>
 #include <OptiXRenderer/Types.h>
 
 namespace OptiXRenderer {
@@ -20,7 +21,7 @@ namespace BSDFs {
 // Sources:
 // * Walter et al 07.
 // * https://github.com/tunabrain/tungsten/blob/master/src/core/bsdfs/Microfacet.hpp
-// TODO
+// Future work
 // * Reference equations in Walter07 and implement different G terms.
 // * Create an optimized version where some expressions have been cancelled out.
 //----------------------------------------------------------------------------
@@ -34,25 +35,10 @@ __inline_all__ float roughness_from_alpha(float alpha) {
     return sqrt(alpha);
 }
 
-__inline_all__ float square(float v) {
-    return v * v;
-}
-
-__inline_all__ float D(float alpha, const optix::float3& halfway) {
-    // TODO Do we need this check here? Can we move it further out? Wait until we have a rough transmitting material.
-    if (halfway.z < 0.0f)
-        return 0.0f;
-
-    float alpha_sqrd = alpha * alpha;
-    float cos_theta_sqrd = halfway.z * halfway.z;
-    float tan_theta_sqrd = optix::fmaxf(1.0f - cos_theta_sqrd, 0.0f) / cos_theta_sqrd;
-    float cos_theta_cubed = cos_theta_sqrd * cos_theta_sqrd;
-    return alpha_sqrd/ (PIf * cos_theta_cubed * square(alpha_sqrd + tan_theta_sqrd));
-}
-
 // TODO This isn't the smith or schlick geometric term. Which is it?
 __inline_all__ float G1(float alpha, const optix::float3& w, const optix::float3& halfway) {
     // Check if the w vector projected onto the halfway vector is in the same hemisphere as the halfway vector and the normal, otherwise the response is black.
+    // TODO We know that this is always the case when sampling, so ignore the checks. What about evaluation? It's not guaranteed, but it would be silly evaluate incompatible directions.
     if (optix::dot(w, halfway) * w.z <= 0.0f)
         return 0.0f;
 
@@ -66,31 +52,13 @@ __inline_all__ float G(float alpha, const optix::float3& wo, const optix::float3
     return G1(alpha, wo, halfway) * G1(alpha, wi, halfway);
 }
 
-namespace Distribution {
-
-__inline_all__ float PDF(float alpha, const optix::float3& halfway) {
-    return D(alpha, halfway) * halfway.z;
-}
-
-__inline_all__ optix::float3 sample(float alpha, optix::float2 random_sample) {
-    float phi = random_sample.y * (2.0f * PIf);
-
-    float tan_theta_sqrd = alpha * alpha * random_sample.x / (1.0f - random_sample.x);
-    float cos_theta = 1.0f / sqrt(1.0f + tan_theta_sqrd);
-    
-    float r = sqrt(optix::fmaxf(1.0f - cos_theta * cos_theta, 0.0f));
-    return optix::make_float3(cos(phi) * r, sin(phi) * r, cos_theta);
-}
-
-} // NS Distribution
-
 //----------------------------------------------------------------------------
 // GGX BSDF, Walter et al 07.
 //----------------------------------------------------------------------------
 
 __inline_all__ float evaluate(float alpha, const optix::float3& wo, const optix::float3& wi, const optix::float3& halfway) {
     float G = GGX::G(alpha, wo, wi, halfway);
-    float D = GGX::D(alpha, halfway);
+    float D = Distributions::GGX::D(alpha, halfway.z);
     float F = 1.0f; // No fresnel.
     return (D * F * G) / (4.0f * wo.z * wi.z);
 }
@@ -108,26 +76,31 @@ __inline_all__ BSDFSample sample(const optix::float3& tint, float alpha, const o
 
     BSDFSample bsdf_sample;
 
-    // Distribution::DirectionalSample halfway_sample = sample(random_sample); TODO
-    optix::float3 halfway = Distribution::sample(alpha, random_sample);
-    float PDF = Distribution::PDF(alpha, halfway);
-    bsdf_sample.direction = optix::reflect(-wo, halfway);
-    bool discardSample = PDF < 0.00001f || bsdf_sample.direction.z < 0.00001f; // Discard samples if the pdf is too low (precision issues) or if the new direction points into the surface (energy loss).
+    Distributions::DirectionalSample halfway_sample = Distributions::GGX::sample(alpha, random_sample);
+    bsdf_sample.direction = optix::reflect(-wo, halfway_sample.direction);
+    bool discardSample = halfway_sample.PDF < 0.00001f || bsdf_sample.direction.z < 0.00001f; // Discard samples if the pdf is too low (precision issues) or if the new direction points into the surface (energy loss).
     if (discardSample) return BSDFSample::none();
 
-    bsdf_sample.weight = evaluate(tint, alpha, wo, bsdf_sample.direction, halfway); // TODO Optimize!! We already have the PDF, so we really just need to divide by cos_theta and apply the G term and normalize.
-    bsdf_sample.PDF = PDF / (4.0f * optix::dot(wo, halfway));
+    bsdf_sample.PDF = halfway_sample.PDF / (4.0f * optix::dot(wo, halfway_sample.direction));
 
+    { // Evaluate using the already computed sample PDF, which is D * cos_theta.
+        const optix::float3& wi = bsdf_sample.direction; // alias for readability.
+        float G = GGX::G(alpha, wo, wi, halfway_sample.direction);
+        float D = halfway_sample.PDF / halfway_sample.direction.z;
+        float F = 1.0f;
+        bsdf_sample.weight = tint * ((D * F * G) / (4.0f * wo.z * wi.z));
+    }
     return bsdf_sample;
 }
 
 __inline_all__ float PDF(float alpha, const optix::float3& wo, const optix::float3& wi) {
     const optix::float3 halfway = optix::normalize(wo + wi);
 
-    if (optix::dot(wo, halfway) < 0.0f)
+    // NOTE Do we need these checks? What happens when the BSDF is a BTDF and the last one fails?
+    if (optix::dot(wo, halfway) < 0.0f || halfway.z < 0.0f)
         return 0.0f;
 
-    return Distribution::PDF(alpha, halfway) / (4.0f * optix::dot(wo, halfway));
+    return Distributions::GGX::PDF(alpha, halfway.z) / (4.0f * optix::dot(wo, halfway));
 }
 
 } // NS GGX
