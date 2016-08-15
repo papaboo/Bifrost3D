0cfbff78e8ea1c375d63b46ce1c02c784ef7a389
Asger Hoedt <asgerhoedt@gmail.com>
2016-08-15 16:11:12 +0200

    namespace local 'using namespace optix'.

--- cogs/OptiXRenderer/OptiXRenderer/Shading/BSDFs/GGX.h
+++ cogs/OptiXRenderer/OptiXRenderer/Shading/BSDFs/GGX.h
@@ -24,8 +24,10 @@ namespace BSDFs {
 //----------------------------------------------------------------------------
 namespace GGX {
 
+using namespace optix;
+
 __inline_all__ float alpha_from_roughness(float roughness) {
-    return optix::fmaxf(0.00000000001f, roughness * roughness);
+    return fmaxf(0.00000000001f, roughness * roughness);
 }
 
 __inline_all__ float roughness_from_alpha(float alpha) {
@@ -33,10 +35,10 @@ __inline_all__ float roughness_from_alpha(float alpha) {
 }
 
 // Smith G1 term. Equation 34 in Walter et al 07.
-__inline_all__ float G1(float alpha, const optix::float3& w, const optix::float3& halfway) {
+__inline_all__ float G1(float alpha, const float3& w, const float3& halfway) {
 #if _DEBUG
     // Check if the w vector projected onto the halfway vector is in the same hemisphere as the halfway vector and the normal, otherwise something went horribly horribly wrong and we're leaking light.
-    if (optix::dot(w, halfway) * w.z <= 0.0f)
+    if (dot(w, halfway) * w.z <= 0.0f)
 #if GPU_DEVICE
         rtThrow(OPTIX_GGX_WRONG_HEMISPHERE_EXCEPTION);
 #else
@@ -46,18 +48,18 @@ __inline_all__ float G1(float alpha, const optix::float3& w, const optix::float3
 
     float alpha_sqrd = alpha * alpha;
     float cos_theta_sqrd = w.z * w.z;
-    float tan_theta_sqrd = optix::fmaxf(1.0f - cos_theta_sqrd, 0.0f) / cos_theta_sqrd;
+    float tan_theta_sqrd = fmaxf(1.0f - cos_theta_sqrd, 0.0f) / cos_theta_sqrd;
     return 2.0f / (1.0f + sqrt(1.0f + alpha_sqrd * tan_theta_sqrd));
 }
 
-__inline_all__ float smith_G(float alpha, const optix::float3& wo, const optix::float3& wi, const optix::float3& halfway) {
+__inline_all__ float smith_G(float alpha, const float3& wo, const float3& wi, const float3& halfway) {
     return G1(alpha, wo, halfway) * G1(alpha, wi, halfway);
 }
 
 // Heitz 14, equation 72.
-__inline_all__ float height_correlated_smith_delta(float alpha, const optix::float3& w, const optix::float3& halfway) {
+__inline_all__ float height_correlated_smith_delta(float alpha, const float3& w, const float3& halfway) {
     float cos_theta_sqrd = w.z * w.z;
-    float tan_theta_sqrd = optix::fmaxf(1.0f - cos_theta_sqrd, 0.0f) / cos_theta_sqrd;
+    float tan_theta_sqrd = fmaxf(1.0f - cos_theta_sqrd, 0.0f) / cos_theta_sqrd;
     float a_sqrd = 1.0f / (alpha * alpha * tan_theta_sqrd);
     return (-1.0f + sqrt(1.0f + 1.0f / a_sqrd)) / 2.0f;
 }
@@ -65,7 +67,7 @@ __inline_all__ float height_correlated_smith_delta(float alpha, const optix::flo
 // Equation 43. 
 // Naty Hofmann mentioned that the geometric term should be 1 / (1 + delta + delta), 
 // but that breaks energy conservation eer so slightly.
-__inline_all__ float height_correlated_smith_G(float alpha, const optix::float3& wo, const optix::float3& wi, const optix::float3& halfway) {
+__inline_all__ float height_correlated_smith_G(float alpha, const float3& wo, const float3& wi, const float3& halfway) {
     // return 1.0f / (1.0f + (height_correlated_smith_delta(alpha, wo, halfway) + height_correlated_smith_delta(alpha, wi, halfway)));
     return 1.0f / (1.0f + height_correlated_smith_delta(alpha, wo, halfway)) * 1.0f / (1.0f + height_correlated_smith_delta(alpha, wi, halfway));
 }
@@ -74,23 +76,23 @@ __inline_all__ float height_correlated_smith_G(float alpha, const optix::float3&
 // GGX BSDF, Walter et al 07.
 //----------------------------------------------------------------------------
 
-__inline_all__ float evaluate(float alpha, const optix::float3& wo, const optix::float3& wi, const optix::float3& halfway) {
+__inline_all__ float evaluate(float alpha, const float3& wo, const float3& wi, const float3& halfway) {
     float G = height_correlated_smith_G(alpha, wo, wi, halfway);
     float D = Distributions::GGX::D(alpha, halfway.z);
     float F = 1.0f; // No fresnel.
     return (D * F * G) / (4.0f * wo.z * wi.z);
 }
 
-__inline_all__ optix::float3 evaluate(const optix::float3& tint, float alpha, const optix::float3& wo, const optix::float3& wi) {
-    const optix::float3 halfway = optix::normalize(wi + wo);
+__inline_all__ float3 evaluate(const float3& tint, float alpha, const float3& wo, const float3& wi) {
+    const float3 halfway = normalize(wi + wo);
     return tint * evaluate(alpha, wo, wi, halfway);
 }
 
-__inline_all__ float PDF(float alpha, const optix::float3& wo, const optix::float3& wi) {
-    const optix::float3 halfway = optix::normalize(wo + wi);
+__inline_all__ float PDF(float alpha, const float3& wo, const float3& wi) {
+    const float3 halfway = normalize(wo + wi);
 
 #if _DEBUG
-    if (optix::dot(wo, halfway) < 0.0f || halfway.z < 0.0f)
+    if (dot(wo, halfway) < 0.0f || halfway.z < 0.0f)
 #if GPU_DEVICE
         rtThrow(OPTIX_GGX_WRONG_HEMISPHERE_EXCEPTION);
 #else
@@ -98,37 +100,37 @@ __inline_all__ float PDF(float alpha, const optix::float3& wo, const optix::floa
 #endif
 #endif
 
-    return Distributions::GGX::PDF(alpha, halfway.z) / (4.0f * optix::dot(wo, halfway));
+    return Distributions::GGX::PDF(alpha, halfway.z) / (4.0f * dot(wo, halfway));
 }
 
-__inline_all__ BSDFResponse evaluate_with_PDF(const optix::float3& tint, float alpha, const optix::float3& wo, const optix::float3& wi, const optix::float3& halfway) {
+__inline_all__ BSDFResponse evaluate_with_PDF(const float3& tint, float alpha, const float3& wo, const float3& wi, const float3& halfway) {
     float G = height_correlated_smith_G(alpha, wo, wi, halfway);
     float D = Distributions::GGX::D(alpha, halfway.z);
     float F = 1.0f; // No fresnel.
     float f = (D * F * G) / (4.0f * wo.z * wi.z);
     BSDFResponse res;
     res.weight = tint * f;
-    res.PDF = (D * halfway.z) / (4.0f * optix::dot(wo, halfway));
+    res.PDF = (D * halfway.z) / (4.0f * dot(wo, halfway));
     return res;
 }
 
-__inline_all__ BSDFResponse evaluate_with_PDF(const optix::float3& tint, float alpha, const optix::float3& wo, const optix::float3& wi) {
-    return evaluate_with_PDF(tint, alpha, wo, wi, optix::normalize(wo + wi));
+__inline_all__ BSDFResponse evaluate_with_PDF(const float3& tint, float alpha, const float3& wo, const float3& wi) {
+    return evaluate_with_PDF(tint, alpha, wo, wi, normalize(wo + wi));
 }
 
-__inline_all__ BSDFSample sample(const optix::float3& tint, float alpha, const optix::float3& wo, optix::float2 random_sample) {
+__inline_all__ BSDFSample sample(const float3& tint, float alpha, const float3& wo, float2 random_sample) {
 
     BSDFSample bsdf_sample;
 
     Distributions::DirectionalSample halfway_sample = Distributions::GGX::sample(alpha, random_sample);
-    bsdf_sample.direction = optix::reflect(-wo, halfway_sample.direction);
+    bsdf_sample.direction = reflect(-wo, halfway_sample.direction);
     bool discardSample = halfway_sample.PDF < 0.00001f || bsdf_sample.direction.z < 0.00001f; // Discard samples if the pdf is too low (precision issues) or if the new direction points into the surface (energy loss).
     if (discardSample) return BSDFSample::none();
 
-    bsdf_sample.PDF = halfway_sample.PDF / (4.0f * optix::dot(wo, halfway_sample.direction));
+    bsdf_sample.PDF = halfway_sample.PDF / (4.0f * dot(wo, halfway_sample.direction));
 
     { // Evaluate using the already computed sample PDF, which is D * cos_theta.
-        const optix::float3& wi = bsdf_sample.direction; // alias for readability.
+        const float3& wi = bsdf_sample.direction; // alias for readability.
         float G = height_correlated_smith_G(alpha, wo, wi, halfway_sample.direction);
         float D = halfway_sample.PDF / halfway_sample.direction.z;
         float F = 1.0f;
--- cogs/OptiXRenderer/OptiXRenderer/Shading/BSDFs/Lambert.h
+++ cogs/OptiXRenderer/OptiXRenderer/Shading/BSDFs/Lambert.h
@@ -17,30 +17,32 @@ namespace Shading {
 namespace BSDFs {
 namespace Lambert {
 
+using namespace optix;
+
 __inline_all__ float evaluate() {
     return RECIP_PIf;
 }
 
-__inline_all__ optix::float3 evaluate(const optix::float3& tint) {
+__inline_all__ float3 evaluate(const float3& tint) {
     return tint * RECIP_PIf;
 }
 
-__inline_all__ optix::float3 evaluate(const optix::float3& tint, const optix::float3& wo, const optix::float3& wi) {
+__inline_all__ float3 evaluate(const float3& tint, const float3& wo, const float3& wi) {
     return tint * RECIP_PIf;
 }
 
-__inline_all__ float PDF(const optix::float3& wo, const optix::float3& wi) {
-    return wi.z * RECIP_PIf;
+__inline_all__ float PDF(const float3& wo, const float3& wi) {
+    return Distributions::Cosine::PDF(wi.z);
 }
 
-__inline_all__ BSDFResponse evaluate_with_PDF(const optix::float3& tint, const optix::float3& wo, const optix::float3& wi) {
+__inline_all__ BSDFResponse evaluate_with_PDF(const float3& tint, const float3& wo, const float3& wi) {
     BSDFResponse response;
     response.weight = evaluate(tint);
     response.PDF = PDF(wo, wi);
     return response;
 }
 
-__inline_all__ BSDFSample sample(const optix::float3& tint, optix::float2 random_sample) {
+__inline_all__ BSDFSample sample(const float3& tint, float2 random_sample) {
     Distributions::DirectionalSample cosine_sample = Distributions::Cosine::sample(random_sample);
     BSDFSample bsdf_sample;
     bsdf_sample.direction = cosine_sample.direction;
