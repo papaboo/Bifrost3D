8ac4fef09d55360f2a4b6d1842371ce3221a485c
Asger Hoedt <asgerhoedt@gmail.com>
2016-09-11 13:19:50 +0200

    Fix height correlated geometric term. (Unfortunately that breaks energy conservation in DefaultShading even more than it already was.) Minor cleanup.

--- cogs/OptiXRenderer/OptiXRenderer/Shading/BSDFs/GGX.h
+++ cogs/OptiXRenderer/OptiXRenderer/Shading/BSDFs/GGX.h
@@ -11,6 +11,7 @@
 
 #include <OptiXRenderer/Distributions.h>
 #include <OptiXRenderer/Types.h>
+#include <OptiXRenderer/Utils.h>
 
 namespace OptiXRenderer {
 namespace Shading {
@@ -56,27 +57,23 @@ __inline_all__ float smith_G(float alpha, const float3& wo, const float3& wi, co
     return G1(alpha, wo, halfway) * G1(alpha, wi, halfway);
 }
 
-// Heitz 14, equation 72.
-__inline_all__ float height_correlated_smith_delta(float alpha, const float3& w, const float3& halfway) {
-    float cos_theta_sqrd = w.z * w.z;
-    float tan_theta_sqrd = fmaxf(1.0f - cos_theta_sqrd, 0.0f) / cos_theta_sqrd;
-    float a_sqrd = 1.0f / (alpha * alpha * tan_theta_sqrd);
-    return (-1.0f + sqrt(1.0f + 1.0f / a_sqrd)) / 2.0f;
-}
-// Height correlated smith geometric term. Understanding the Masking-Shadowing Function in Microfacet-Based BRDFs, Eric Heitz, 2014.
-// Equation 43. 
-// Naty Hofmann mentioned that the geometric term should be 1 / (1 + delta + delta), 
-// but that breaks energy conservation eer so slightly.
+// Height correlated smith geometric term.
+// Understanding the Masking-Shadowing Function in Microfacet-Based BRDFs, Heitz 14, equation 99. 
 __inline_all__ float height_correlated_smith_G(float alpha, const float3& wo, const float3& wi, const float3& halfway) {
-    // return 1.0f / (1.0f + (height_correlated_smith_delta(alpha, wo, halfway) + height_correlated_smith_delta(alpha, wi, halfway)));
-    return 1.0f / (1.0f + height_correlated_smith_delta(alpha, wo, halfway)) * 1.0f / (1.0f + height_correlated_smith_delta(alpha, wi, halfway));
+    // float numerator = heaviside(dot(wo, halfway)) * heaviside(dot(wi, halfway)); // Used to zero contribution from backsides. In practice this can be avoided by the material using GGX instead of here.
+    return 1.0f / (1.0f + G1(alpha, wo, halfway) + G1(alpha, wi, halfway));
 }
 
 //----------------------------------------------------------------------------
 // GGX BSDF, Walter et al 07.
-// Future work
-// * Reduce argument directions cos(angle)s: VoN, LoN, HoN and VoH.
-//   Should use fever registers.
+// Here we have seperated the BRDF from the BTDF. 
+// This makes sampling slightly less performant, but allows for a full 
+// overview of the individual components, which can then later be composed 
+// in a new material/BSDF.
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// GGX BRDF, Walter et al 07.
 //----------------------------------------------------------------------------
 
 __inline_all__ float evaluate(float alpha, const float3& wo, const float3& wi, const float3& halfway) {
@@ -114,10 +111,6 @@ __inline_all__ BSDFResponse evaluate_with_PDF(const float3& tint, float alpha, c
     return res;
 }
 
-__inline_all__ BSDFResponse evaluate_with_PDF(const float3& tint, float alpha, const float3& wo, const float3& wi) {
-    return evaluate_with_PDF(tint, alpha, wo, wi, normalize(wo + wi));
-}
-
 __inline_all__ BSDFSample sample(const float3& tint, float alpha, const float3& wo, float2 random_sample) {
 
     BSDFSample bsdf_sample;
@@ -127,7 +120,7 @@ __inline_all__ BSDFSample sample(const float3& tint, float alpha, const float3& 
     bool discardSample = halfway_sample.PDF < 0.00001f || bsdf_sample.direction.z < 0.00001f; // Discard samples if the pdf is too low (precision issues) or if the new direction points into the surface (energy loss).
     if (discardSample) return BSDFSample::none();
 
-    bsdf_sample.PDF = halfway_sample.PDF / (4.0f * dot(wo, halfway_sample.direction));
+    bsdf_sample.PDF = halfway_sample.PDF / (4.0f * dot(wo, halfway_sample.direction)); // TODO Discard based on this PDF, not the mirofacet normal PDF. And that aside, shouldn't the integrator do the discarding?
 
     { // Evaluate using the already computed sample PDF, which is D * cos_theta.
         const float3& wi = bsdf_sample.direction; // alias for readability.
