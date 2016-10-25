f0f5da31ff19988fd3238986ac7eb75f18b074f4
Asger Hoedt <asgerhoedt@gmail.com>
2016-10-25 22:47:49 +0200

    TODO

--- cogs/DX12Renderer/DX12Renderer/Renderer.h
+++ cogs/DX12Renderer/DX12Renderer/Renderer.h
@@ -27,10 +27,17 @@ namespace DX12Renderer {
 // DirectX 12 renderer.
 // TODO
 // * Is FindDirectX needed in VS 2015?
+// * Draw model(s).
 // * Abstract Mesh and Material. Possible? What is a material in DX12? PSO?
+// ** Perhaps store the mesh as a set of buffers and their description.
+// ** The material is a bunch of material parameters.
+// ** The model combines a mesh, with material parameters and a configuration ID that matches the two. The configuration ID is used to connect to multiple PSO's. One for the Z-prepass, one for rendering, one for shadows and so on and so forth.
+// * Actual material and integrator.
 // * SRGB backbuffer format, DXGI_FORMAT_R8G8B8A8_UNORM_SRGB.
-// * Draw models.
 // * Reimplement MiniEngine's CommandListManager and all dependencies.
+// * Can I create an interface similar to CUDA's streams?
+// ** I would need to reset whatever is associated with the stream each frame. 
+// ** The main components would of course be a command list pr stream and fences, but what else needs to be reset pr frame?
 // * Debug layer define.
 // ** Compile shaders with debug flags and no optimizations. Load HLSL files instead of cso's.
 // * Stochastic coverage.
