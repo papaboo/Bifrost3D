9a984d812796a93d6e14e6df6d41a8c7502a4635
Asger Hoedt <asgerhoedt@gmail.com>
2016-11-27 09:47:26 +0100

    Directional light support in fragment shader.

--- cogs/DX11Renderer/DX11Renderer/Shaders/FragmentShader.hlsl
+++ cogs/DX11Renderer/DX11Renderer/Shaders/FragmentShader.hlsl
@@ -23,9 +23,15 @@ struct LightData {
     float4 spatial_softness;
 
     float type() { return type_power.x; }
+
+    // Sphere light property accessors.
     float3 sphere_power() { return type_power.yzw; }
     float3 sphere_position() { return spatial_softness.xyz; }
     float sphere_radius() { return spatial_softness.w; }
+
+    // Directinoal light property accessors.
+    float3 directional_radiance() { return type_power.yzw; }
+    float3 directional_direction() { return spatial_softness.xyz; } // Oh what a concise method name.
 };
 
 struct LightSample {
@@ -33,6 +39,15 @@ struct LightSample {
     float angle_subtended;
     float3 direction_to_light;
     float distance;
+
+    static LightSample empty() {
+        LightSample light_sample;
+        light_sample.radiance = float3(0, 0, 0);
+        light_sample.angle_subtended = 0.0;
+        light_sample.direction_to_light = float3(0, 1, 0);
+        light_sample.distance = 1e30f;
+        return light_sample;
+    }
 };
 
 LightSample sample_sphere_light(LightData light, float3 world_position) {
@@ -49,12 +64,22 @@ LightSample sample_sphere_light(LightData light, float3 world_position) {
     return light_sample;
 }
 
-LightSample sample_light(LightData light, float3 world_position) {
-    if (light.type() == 1.0)
-        sample_sphere_light(light, world_position);
-
+LightSample sample_directional_light(LightData light) {
     LightSample light_sample;
+    light_sample.radiance = light.directional_radiance();
+    light_sample.direction_to_light = -light.directional_direction();
+    light_sample.distance = 1e30f;
+    light_sample.angle_subtended = 0.0;
     return light_sample;
+}
+
+LightSample sample_light(LightData light, float3 world_position) {
+    if (light.type() > 1.5)
+        return sample_directional_light(light);
+    else if (light.type() > 0.5)
+        return sample_sphere_light(light, world_position);
+
+    return LightSample::empty();
 }
 
 cbuffer lights : register(b1) {
@@ -70,12 +95,10 @@ struct PixelInput {
 
 float4 main(PixelInput input) : SV_TARGET{
     float3 normal = normalize(input.normal.xyz);
-    // return float4(normal * 0.5f + 0.5f, 1.0);
-    // return float4(input.world_pos.xyz, 1.0);
 
     float3 radiance = float3(0.0, 0.0, 0.0);
     for (int l = 0; l < light_count.x; ++l) {
-        LightSample light_sample = sample_sphere_light(light_data[l], input.world_pos.xyz);
+        LightSample light_sample = sample_light(light_data[l], input.world_pos.xyz);
         radiance += color.rgb * light_sample.radiance * max(0.0, dot(normal, light_sample.direction_to_light));
     }
     return float4(radiance, 1.0);
