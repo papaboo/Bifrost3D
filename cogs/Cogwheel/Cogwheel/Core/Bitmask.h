f47d4c6ff4b1d3b5124c820e824e9af1fd2e62a6
Asger Hoedt <asgerhoedt@gmail.com>
2017-01-27 15:12:20 +0100

    Convert initializer list of flags to a bitmask.

--- cogs/Cogwheel/Cogwheel/Core/Bitmask.h
+++ cogs/Cogwheel/Cogwheel/Core/Bitmask.h
@@ -1,10 +1,10 @@
 // Cogwheel bitmask using enum class.
-// ---------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 // Copyright (C) 2016, Cogwheel. See AUTHORS.txt for authors
 //
-// This program is open source and distributed under the New BSD License. See
-// LICENSE.txt for more detail.
-// ---------------------------------------------------------------------------
+// This program is open source and distributed under the New BSD License. 
+// See LICENSE.txt for more detail.
+// ------------------------------------------------------------------------------------------------
 
 #ifndef _COGWHEEL_CORE_BITMASK_H_
 #define _COGWHEEL_CORE_BITMASK_H_
@@ -14,9 +14,9 @@
 namespace Cogwheel {
 namespace Core {
 
-// ---------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 // Typesafe bitmask templated with an enumeration.
-// ---------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 template <typename E>
 struct Bitmask final {
 public:
@@ -28,16 +28,17 @@ private:
 
 public:
 
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     // Constructors.
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     Bitmask() { }
     Bitmask(E v) : m_mask(T(v)) { }
     Bitmask(T mask) : m_mask(mask) { }
+    Bitmask(std::initializer_list<E> flags) : m_mask(0) {  for (E flag : flags) m_mask |= T(flag); }
 
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     // Modifiers.
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     inline Bitmask<E>& operator=(E v) { m_mask = T(v); return *this; }
     inline Bitmask<E>& operator&=(E v) { m_mask &= T(v); return *this; }
     inline Bitmask<E>& operator&=(Bitmask v) { m_mask &= v.m_mask; return *this; }
@@ -46,9 +47,9 @@ public:
     inline Bitmask<E>& operator^=(E v) { m_mask ^= T(v); return *this; }
     inline Bitmask<E>& operator^=(Bitmask v) { m_mask ^= v.m_mask; return *this; }
 
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     // Bit operations.
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     inline Bitmask<E> operator&(E v) const { return Bitmask(m_mask & T(v)); }
     inline Bitmask<E> operator&(Bitmask v) const { return Bitmask(m_mask & v.m_mask); }
     inline Bitmask<E> operator|(E v) const { return Bitmask(m_mask | T(v)); }
@@ -56,9 +57,9 @@ public:
     inline Bitmask<E> operator^(E v) const { return Bitmask(m_mask ^ T(v)); }
     inline Bitmask<E> operator^(Bitmask v) const { return Bitmask(m_mask ^ v.m_mask); }
 
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     // Accessors.
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     inline bool none_set() const { return m_mask == T(0); }
     inline bool not_set(E v) const { return (T(v) & m_mask) == T(0); }
     inline bool all_set(E v1, E v2) const { T v = (T(v1) | T(v2);  return v & m_mask) == v; }
@@ -66,18 +67,18 @@ public:
     inline bool any_set(E v) const { return (T(v) & m_mask) != T(0); }
     inline bool any_set(E v1, E v2) const { return ((T(v1) | T(v2)) & m_mask) != T(0); }
 
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     // Comparisons.
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     inline bool operator==(E v) const { return T(v) == m_mask; }
     inline bool operator==(Bitmask v) const { return v.m_mask == m_mask; }
     inline bool operator!=(E v) const { return T(v) != m_mask; }
     inline bool operator!=(Bitmask v) const { return v.m_mask != m_mask; }
     inline operator bool() const { return m_mask != T(0); }
 
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     // Raw data access.
-    // -----------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     inline T raw() { return m_mask; }
 };
 
