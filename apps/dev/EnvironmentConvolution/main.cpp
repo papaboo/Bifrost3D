de6a8ede7440c17b628f865b2cf27f7ba2116d3e
Asger Hoedt <asgerhoedt@gmail.com>
2017-03-24 14:14:45 +0100

    Light sampling support.

--- apps/dev/EnvironmentConvolution/main.cpp
+++ apps/dev/EnvironmentConvolution/main.cpp
@@ -58,10 +58,50 @@ inline Sample sample(float alpha, Vector2f random_sample) {
 
 } // NS GGX
 
+enum class SampleMethod {
+    MIS, Light, BSDF
+};
+
+struct Options {
+    SampleMethod sample_method;
+    int sample_count;
+
+    static Options parse(int argc, char** argv) {
+        Options options = { SampleMethod::BSDF, 256 };
+
+        // Skip the first two arguments, the application name and image path.
+        for (int argument = 2; argument < argc; ++argument) {
+            if (strcmp(argv[argument], "--mis-sampling") == 0 || strcmp(argv[argument], "-m") == 0)
+                options.sample_method = SampleMethod::MIS;
+            else if (strcmp(argv[argument], "--light-sampling") == 0 || strcmp(argv[argument], "-l") == 0)
+                options.sample_method = SampleMethod::Light;
+            else if (strcmp(argv[argument], "--bsdf-sampling") == 0 || strcmp(argv[argument], "-b") == 0)
+                options.sample_method = SampleMethod::BSDF;
+        }
+
+        return options;
+    }
+
+    std::string to_string() const {
+        std::ostringstream out;
+        switch (sample_method) {
+        case SampleMethod::MIS: out << "MIS sampling, "; break;
+        case SampleMethod::Light: out << "Light sampling, "; break;
+        case SampleMethod::BSDF: out << "BSDF sampling, "; break;
+        }
+        out << sample_count << " samples pr pixel.";
+        return out.str();
+    }
+};
+
 void print_usage() {
     char* usage =
         "usage EnvironmentConvolution <path/to/environment.ext>:\n"
-        "  -h  | --help: Show command line usage for EnvironmentConvolution.\n";
+        "  -h | --help: Show command line usage for EnvironmentConvolution.\n"
+        "  -s | --sample-count. The number of samples pr pixel.\n"
+        "  -m | --mis-sampling. Combine light and bsdf samples by multiple importance sampling.\n"
+        "  -l | --light-sampling. Draw samples from the environment.\n"
+        "  -b | --bsdf-sampling. Draw samples from the GGX distribution.\n";
     printf("%s", usage);
 }
 
@@ -73,15 +113,20 @@ int main(int argc, char** argv) {
         return 0;
     }
 
+    Options options = Options::parse(argc, argv);
+
     printf("Convolute '%s'\n", argv[1]);
+    printf("  %s\n", options.to_string().c_str());
 
     Images::allocate(1);
     Textures::allocate(1);
 
     Image image = StbImageLoader::load(std::string(argv[1]));
 
-    Textures::UID environment_ID = Textures::create2D(image.get_ID(), MagnificationFilter::Linear, MinificationFilter::Linear, WrapMode::Repeat, WrapMode::Clamp);
-    LatLongDistribution infinite_area_light = LatLongDistribution(environment_ID);
+    Textures::UID texture_ID = Textures::create2D(image.get_ID(), MagnificationFilter::Linear, MinificationFilter::Linear, WrapMode::Repeat, WrapMode::Clamp);
+    LatLongDistribution* infinite_area_light = nullptr;
+    if (options.sample_method == SampleMethod::Light)
+        infinite_area_light = new LatLongDistribution(texture_ID);
 
     Image output = Images::create("Convoluted image", PixelFormat::RGB24, 2.2f, Vector2ui(image.get_width(), image.get_height())); // TODO Wrong gamma
 
@@ -95,27 +140,46 @@ int main(int argc, char** argv) {
             Quaternionf up_rotation = Quaternionf::look_in(up_vector);
 
             // TODO We can precompute the unrotated GGX samples and the area_light samples. Does that speed anything up? Will there be correlation artefacts?
+            //      Perhaps just draw samplecount * 16 and reuse different permutations of them.
             // TODO What is faster for rotation? A matrix or quaternion?
             const int SAMPLE_COUNT = 256;
             RGB radiance = RGB::black();
             
-            // Material sampling.
-            for (int s = 0; s < SAMPLE_COUNT; ++s) {
-                GGX::Sample sample = GGX::sample(alpha, RNG::sample02(s));
-                sample.direction = up_rotation * sample.direction;
-                radiance += infinite_area_light.evaluate(sample.direction);
+            switch (options.sample_method) {
+            case SampleMethod::MIS:
+                // TODO MIS.
+                break;
+            case SampleMethod::Light:
+                for (int s = 0; s < SAMPLE_COUNT; ++s) {
+                    LightSample sample = infinite_area_light->sample(RNG::sample02(s));
+                    Vector3f local_direction = inverse_unit(up_rotation) * sample.direction_to_light;
+                    float ggx_f = GGX::D(alpha, local_direction.z);
+                    float cos_theta = fmaxf(local_direction.z, 0.0f);
+                    radiance += sample.radiance * ggx_f * cos_theta / sample.PDF;
+                }
+                break;
+            case SampleMethod::BSDF:
+                for (int s = 0; s < SAMPLE_COUNT; ++s) {
+                    GGX::Sample sample = GGX::sample(alpha, RNG::sample02(s));
+                    sample.direction = up_rotation * sample.direction;
+                    Vector2f sample_uv = direction_to_latlong_texcoord(sample.direction);
+                    radiance += sample2D(texture_ID, sample_uv).rgb();
+                }
+                break;
             }
-
-            // TODO Light sampling.
-
-            // TODO MIS.
 
             radiance /= SAMPLE_COUNT;
 
             output.set_pixel(RGBA(radiance), Vector2ui(x, y));
         }
 
-    StbImageWriter::write("C:/Users/asger/Desktop/output.png", output);
+    // TODO Remove!
+    if (options.sample_method == SampleMethod::BSDF)
+        StbImageWriter::write("C:/Users/asger/Desktop/output_BSDF.png", output);
+    else if (options.sample_method == SampleMethod::Light)
+        StbImageWriter::write("C:/Users/asger/Desktop/output_light.png", output);
+    else
+        StbImageWriter::write("C:/Users/asger/Desktop/output_MIS.png", output);
 
     return 0;
 }
