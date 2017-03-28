7514ce9493df24c4fd68a4af76c15cdfc4abc24f
Asger Hoedt <asgerhoedt@gmail.com>
2017-03-28 21:46:45 +0200

    Compute PDF function that fills an existing array.

--- cogs/Cogwheel/Cogwheel/Assets/InfiniteAreaLight.h
+++ cogs/Cogwheel/Cogwheel/Assets/InfiniteAreaLight.h
@@ -85,20 +85,32 @@ public:
     // Static utility functions.
     //*********************************************************************************************
     static float* compute_PDF(TextureND latlong) {
+        float* PDF = new float[latlong.get_image().get_pixel_count()];
+        compute_PDF(latlong, PDF);
+        return PDF;
+    }
+
+    // Computes the array of per pixel PDFs for an infinite area light. 
+    // The importance is based on the average radiance of a pixel.
+    // The PDF input must contain room for at least pixelcount floats.
+    static void compute_PDF(TextureND latlong, float* PDF_result) {
         Image image = latlong.get_image();
         int width = image.get_width(), height = image.get_height();
 
-        float* PDF = new float[width * height];
+        bool filter_pixels = latlong.get_magnification_filter() == Assets::MagnificationFilter::Linear;
+
+        // Use a temporary PDF array if the PDFs should be filtered afterwards, otherwise use the result array.
+        float* PDF = filter_pixels ? new float[width * height] : PDF_result;
 
         #pragma omp parallel for schedule(dynamic, 16)
         for (int y = 0; y < height; ++y) {
             // PBRT p. 728. Account for the non-uniform surface area of the pixels, e.g. the higher density near the poles.
             float sin_theta = sinf(Math::PI<float>() * (y + 0.5f) / float(height));
 
-            float* per_pixel_PDF_row = PDF + y * width;
+            float* PDF_row = PDF + y * width;
             for (int x = 0; x < width; ++x) {
                 Math::RGB pixel = image.get_pixel(Math::Vector2ui(x, y)).rgb();
-                per_pixel_PDF_row[x] = (pixel.r + pixel.g + pixel.b) * sin_theta;
+                PDF_row[x] = (pixel.r + pixel.g + pixel.b) * sin_theta;
             }
         }
 
@@ -107,9 +119,7 @@ public:
         // Generally this doesn't change much in terms of convergence, but it helps us to 
         // avoid artefacts in cases where a black pixel would have a PDF of 0,
         // but due to filtering the entire texel wouldn't actually be black.
-        if (latlong.get_magnification_filter() == Assets::MagnificationFilter::Linear) {
-            float* filtered_PDF = new float[width * height];
-
+        if (filter_pixels) {
             #pragma omp parallel for schedule(dynamic, 16)
             for (int y = 0; y < height; ++y) {
                 // Blur per pixel importance to account for linear interpolation.
@@ -118,55 +128,53 @@ public:
                 // Neighbours in the corners contribute by 1 / 32.
                 // Weights have been estimated based on linear interpolation.
                 for (int x = 0; x < width; ++x) {
-                    float& pixel_PDF = filtered_PDF[x + y * width];
+                    float& pixel_PDF = PDF_result[x + y * width];
                     pixel_PDF = 0.0f;
-
-                    // TODO Defer the division by 32.0f.
 
                     { // Add contribution from left column.
                         int left_x = x - 1 < 0 ? (width - 1) : (x - 1); // Repeat mode.
 
                         int lower_left_index = left_x + std::max(0, y - 1) * width;
-                        pixel_PDF += float(PDF[lower_left_index]) * (1.0f / 32.0f);
+                        pixel_PDF += float(PDF[lower_left_index]);
 
                         int middle_left_index = left_x + y * width;
-                        pixel_PDF += float(PDF[middle_left_index]) * (2.0f / 32.0f);
+                        pixel_PDF += float(PDF[middle_left_index]) * 2.0f;
 
                         int upper_left_index = left_x + std::min(int(height) - 1, y + 1) * width;
-                        pixel_PDF += float(PDF[upper_left_index]) * (1.0f / 32.0f);
+                        pixel_PDF += float(PDF[upper_left_index]);
                     }
 
                     { // Add contribution from right column.
                         int right_x = x + 1 == width ? 0 : (x + 1); // Repeat mode.
 
                         int lower_right_index = right_x + std::max(0, y - 1) * width;
-                        pixel_PDF += float(PDF[lower_right_index]) * (1.0f / 32.0f);
+                        pixel_PDF += float(PDF[lower_right_index]);
 
                         int middle_right_index = right_x + y * width;
-                        pixel_PDF += float(PDF[middle_right_index]) * (2.0f / 32.0f);
+                        pixel_PDF += float(PDF[middle_right_index]) * 2.0f;
 
                         int upper_right_index = right_x + std::min(int(height) - 1, y + 1) * width;
-                        pixel_PDF += float(PDF[upper_right_index]) * (1.0f / 32.0f);
+                        pixel_PDF += float(PDF[upper_right_index]);
                     }
 
                     { // Add contribution from middle column.
                         int lower_middle_index = x + std::max(0, y - 1) * width;
-                        pixel_PDF += float(PDF[lower_middle_index]) * (2.0f / 32.0f);
+                        pixel_PDF += float(PDF[lower_middle_index]) * 2.0f;
 
                         int upper_middle_index = x + std::min(int(height) - 1, y + 1) * width;
-                        pixel_PDF += float(PDF[upper_middle_index]) * (2.0f / 32.0f);
+                        pixel_PDF += float(PDF[upper_middle_index]) * 2.0f;
 
                         // Center last as it has the highest weight.
-                        pixel_PDF += float(PDF[x + y * width]) * (20.0f / 32.0f);
+                        pixel_PDF += float(PDF[x + y * width]) * 20.0f;
                     }
+
+                    // Normalize.
+                    pixel_PDF /= 32.0f;
                 }
             }
 
             delete[] PDF;
-            PDF = filtered_PDF;
         }
-
-        return PDF;
     }
 };
 
