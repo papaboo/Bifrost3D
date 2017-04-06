8f6c9a327262ca592cf79aced301ebd40d12147d
Asger Hoedt <asgerhoedt@gmail.com>
2017-04-06 21:08:20 +0200

    Add hash function.

--- cogs/Cogwheel/Cogwheel/Math/RNG.h
+++ cogs/Cogwheel/Cogwheel/Math/RNG.h
@@ -40,6 +40,18 @@ inline Vector2f sample02(unsigned int n, Vector2ui scramble = Vector2ui(5569, 95
     return Vector2f(van_der_corput(n, scramble.x), sobol2(n, scramble.y));
 }
 
+// Robert Jenkins hash function.
+// https://gist.github.com/badboy/6267743
+inline unsigned int hash(unsigned int a) {
+    a = (a + 0x7ed55d16) + (a << 12);
+    a = (a ^ 0xc761c23c) ^ (a >> 19);
+    a = (a + 0x165667b1) + (a << 5);
+    a = (a + 0xd3a2646c) ^ (a << 9);
+    a = (a + 0xfd7046c5) + (a << 3);
+    a = (a ^ 0xb55a4f09) ^ (a >> 16);
+    return a;
+}
+
 } // NS RNG
 } // NS Math
 } // NS Cogwheel
