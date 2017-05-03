f7d8b6258a9c79cefaa28cb0c3bcf7aae4dcf8be
Asger Hoedt <asgerhoedt@gmail.com>
2017-05-03 09:55:04 +0200

    Remove misplaced future work. OwnedUIDs should be created and specialized where (and if) needed, instead of a as a general type.

--- cogs/Cogwheel/Cogwheel/Core/UniqueIDGenerator.h
+++ cogs/Cogwheel/Cogwheel/Core/UniqueIDGenerator.h
@@ -20,8 +20,6 @@ namespace Core {
 // See http://bitsquid.blogspot.de/2011/09/managing-decoupling-part-4-id-lookup.html.
 // Future work
 // * Enable assert in has(UID). It requires the last free element to always point inside the array, preferably at the sentinel element. If that happens, then I need to special case erase() to the case where next_element is 0, because then last_element is invalid.
-// * We could generate something like OwnedUIDs, that can only be stored in one place and will erase themselves when they are no longer needed.
-// ** In order to avoid the user accidentally deleting an OwnedUID, the UID member would of course have to be unaccessable. (or require an owned ID for erase, but then it needs to be used everywhere!)
 //----------------------------------------------------------------------------
 template <typename T>
 class TypedUIDGenerator final {
