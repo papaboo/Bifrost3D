7b426283982d5be1736cfced077ae93368cc4d7e
Asger Hoedt <asgerhoedt@gmail.com>
2017-06-22 22:21:05 +0200

    DX11Renderer: Add unique pointer specialized for resources.

---
+++ cogs/DX11Renderer/DX11Renderer/UniqueResourcePtr.h
@@ -0,0 +1,68 @@
+// DirectX 11 renderer unique resource ptr.
+// ------------------------------------------------------------------------------------------------
+// Copyright (C) 2016, Cogwheel. See AUTHORS.txt for authors
+//
+// This program is open source and distributed under the New BSD License.
+// See LICENSE.txt for more detail.
+// ------------------------------------------------------------------------------------------------
+
+#ifndef _DX11RENDERER_RENDERER_UNIQUE_RESOURCE_PTR_H_
+#define _DX11RENDERER_RENDERER_UNIQUE_RESOURCE_PTR_H_
+
+namespace DX11Renderer {
+
+// ------------------------------------------------------------------------------------------------
+// A unique pointer for DirectX 11 resources.
+// The pointer is used when there will only ever be a single, unique owner of the resource.
+// For shared ownership use Microsoft::WRL::ComPtr.
+// ------------------------------------------------------------------------------------------------
+template <typename T>
+class UniqueResourcePtr {
+public:
+    T* resource;
+
+    // --------------------------------------------------------------------------------------------
+    // Constructors and destructor.
+    // --------------------------------------------------------------------------------------------
+    UniqueResourcePtr(T* res)
+        : resource(res) { }
+
+    UniqueResourcePtr(UniqueResourcePtr&& other)
+        : resource(other.detach()) { }
+
+    UniqueResourcePtr& operator=(UniqueResourcePtr&& rhs) {
+        resource = rhs.detach;
+        return *this;
+    }
+
+    ~UniqueResourcePtr() {
+        if (resource != nullptr) {
+            resource->Release();
+            resource = nullptr;
+        }
+    }
+
+    // --------------------------------------------------------------------------------------------
+    // Data access.
+    // --------------------------------------------------------------------------------------------
+
+    inline operator bool() const { return resource != nullptr; }
+
+    inline operator T*() { return resource; }
+    inline T* get() { return resource; }
+    inline T* operator->() { return resource; }
+    inline const T* const operator->() const { return resource; }
+
+    inline T* detach() const { T* tmp = resource; resource = nullptr; return tmp; }
+    inline unsigned int release() { resource->Release(); resource = nullptr; }
+
+private:
+
+    // NOTE If these are ever implemented (which I hope they're not) they should transfer ownership.
+    UniqueResourcePtr(UniqueResourcePtr& other) = delete;
+    UniqueResourcePtr& operator=(UniqueResourcePtr& rhs) = delete;
+};
+
+} // NS DX11Renderer
+
+#endif // _DX11RENDERER_RENDERER_UNIQUE_RESOURCE_PTR_H_
