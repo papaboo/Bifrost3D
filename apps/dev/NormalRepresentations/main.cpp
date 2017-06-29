d44231c854564f80f5ace89e2e6f526a4c84c767
Asger Hoedt <asgerhoedt@gmail.com>
2017-06-29 15:40:30 +0200

    NormalRepresentations: Precise encoding for Oct32s_dx11.

--- apps/dev/NormalRepresentations/main.cpp
+++ apps/dev/NormalRepresentations/main.cpp
@@ -117,13 +117,48 @@ struct Oct32s_dx11 {
         }
         return normalize(n);
     }
+
+    static Oct32s_dx11 encode_precise(Vector3f n) {
+        // Project the sphere onto the octahedron, and then onto the xy plane.
+        Vector2f p = Vector2f(n.x, n.y) / (abs(n.x) + abs(n.y) + abs(n.z));
+
+        // Reflect the folds of the lower hemisphere over the diagonals.
+        Vector2f p2 = n.z < 0 ? (Vector2f(1.0f) - Vector2f(abs(p.y), abs(p.x))) * sign(p) : p;
+
+        // Fixed point encoding.
+        Oct32s_dx11 floored_oct = { Vector2s(short(floor(clamp(p2.x, -1.0f, 1.0f) * SHRT_MAX)),
+                                             short(floor(clamp(p2.y, -1.0f, 1.0f) * SHRT_MAX))) };
+
+        Oct32s_dx11 best_representation = floored_oct;
+        float lowest_sqrd_mag = squared_magnitude(best_representation.decode() - n);
+
+        auto best_oct_tester = [&](Oct32s_dx11 new_oct) {
+            float m = squared_magnitude(new_oct.decode() - n);
+            if (m < lowest_sqrd_mag) {
+                best_representation = new_oct;
+                lowest_sqrd_mag = m;
+            }
+        };
+
+        Oct32s_dx11 upper_left = { floored_oct.encoding + Vector2s(0, 1)};
+        best_oct_tester(upper_left);
+        Oct32s_dx11 lower_right = { floored_oct.encoding + Vector2s(1, 0) };
+        best_oct_tester(lower_right);
+        Oct32s_dx11 upper_right = { floored_oct.encoding + Vector2s(1, 1) };
+        best_oct_tester(upper_right);
+
+        return best_representation;
+    }
 };
 
-template <typename T>
-void test_encoding(const std::string& name) {
-    Statistics stats = Statistics(0, 100000, [](int i) -> double {
+typedef Vector3f (*EncodeDecode)(Vector3f);
+
+void test_encoding(const std::string& name, EncodeDecode encode_decode) {
+    const int sample_count = 100000;
+
+    Statistics stats = Statistics(0, sample_count, [=](int i) -> double {
         Vector3d normal = normalize((Vector3d)Distributions::Sphere::Sample(RNG::sample02(i)));
-        Vector3d decoded_normal = (Vector3d)T::encode((Vector3f)normal).decode();
+        Vector3d decoded_normal = (Vector3d)encode_decode((Vector3f)normal);
         return magnitude(normal - decoded_normal);
     });
 
@@ -137,7 +172,8 @@ void test_encoding(const std::string& name) {
 int main(int argc, char** argv) {
     printf("Unit vector representation tests\n");
 
-    test_encoding<Oct32u>("Oct32u");
-    test_encoding<Oct32s>("Oct32s");
-    test_encoding<Oct32s_dx11>("Oct32s_dx11");
+    test_encoding("Oct32u", [](Vector3f normal) { return Oct32u::encode(normal).decode(); });
+    test_encoding("Oct32s", [](Vector3f normal) { return Oct32s::encode(normal).decode(); });
+    test_encoding("Oct32s_dx11", [](Vector3f normal) { return Oct32s_dx11::encode(normal).decode(); });
+    test_encoding("Oct32s_dx11 precise", [](Vector3f normal) { return Oct32s_dx11::encode_precise(normal).decode(); });
 }
