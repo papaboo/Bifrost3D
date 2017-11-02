042130251ceeca8bf6816dd103d2480feb8ee4d2
Asger Hoedt <asgerhoedt@gmail.com>
2017-11-02 20:43:40 +0100

    OptiXRenderer: Spherical pivot transformation distribution.

--- cogs/OptiXRenderer/OptiXRenderer/Distributions.h
+++ cogs/OptiXRenderer/OptiXRenderer/Distributions.h
@@ -1,15 +1,16 @@
 // OptiX distributions for monte carlo integration.
-// ---------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 // Copyright (C) 2016, Cogwheel. See AUTHORS.txt for authors
 //
-// This program is open source and distributed under the New BSD License. See
-// LICENSE.txt for more detail.
-// ---------------------------------------------------------------------------
+// This program is open source and distributed under the New BSD License.
+// See LICENSE.txt for more detail.
+// ------------------------------------------------------------------------------------------------
 
 #ifndef _OPTIXRENDERER_DISTRIBUTIONS_H_
 #define _OPTIXRENDERER_DISTRIBUTIONS_H_
 
 #include <OptiXRenderer/Defines.h>
+#include <OptiXRenderer/Types.h>
 #include <OptiXRenderer/Utils.h>
 
 #include <optixu/optixu_math_namespace.h>
@@ -22,31 +23,31 @@ struct __align__(16) DirectionalSample {
     float PDF;
 };
 
-//==============================================================================
+//=================================================================================================
 // Uniform cone distribution.
-//==============================================================================
+//=================================================================================================
 namespace Cone {
 
-__inline_all__ float PDF(float cos_theta_max) {
-    return 1.0f / (2.0f * PIf * (1.0f - cos_theta_max));
-}
+    __inline_all__ float PDF(float cos_theta_max) {
+        return 1.0f / (2.0f * PIf * (1.0f - cos_theta_max));
+    }
 
-__inline_all__ DirectionalSample sample(float cos_theta_max, optix::float2 random_sample) {
-    float cos_theta = (1.0f - random_sample.x) + random_sample.x * cos_theta_max;
-    float sin_theta = sqrt(1.0f - cos_theta * cos_theta);
-    float phi = 2.0f * PIf * random_sample.y;
+    __inline_all__ DirectionalSample sample(float cos_theta_max, optix::float2 random_sample) {
+        float cos_theta = (1.0f - random_sample.x) + random_sample.x * cos_theta_max;
+        float sin_theta = sqrt(1.0f - cos_theta * cos_theta);
+        float phi = 2.0f * PIf * random_sample.y;
 
-    DirectionalSample res;
-    res.direction = optix::make_float3(cos(phi) * sin_theta, sin(phi) * sin_theta, cos_theta);
-    res.PDF = PDF(cos_theta_max);
-    return res;
-}
+        DirectionalSample res;
+        res.direction = optix::make_float3(cos(phi) * sin_theta, sin(phi) * sin_theta, cos_theta);
+        res.PDF = PDF(cos_theta_max);
+        return res;
+    }
 
 } // NS Cone
 
-//==============================================================================
+//=================================================================================================
 // Cosine distribution.
-//==============================================================================
+//=================================================================================================
 namespace Cosine {
 
     __inline_all__ float PDF(float abs_cos_theta) {
@@ -67,11 +68,11 @@ namespace Cosine {
 
 } // NS Cosine
 
-//==============================================================================
+//=================================================================================================
 // GGX distribution.
 // Future work
 // * Reference equations in Walter07.
-//==============================================================================
+//=================================================================================================
 namespace GGX {
 
     __inline_all__ float D(float alpha, float abs_cos_theta) {
@@ -215,6 +216,93 @@ namespace VNDF_GGX {
     }
 
 } // NS VNDF_GGX
+
+//=================================================================================================
+// A Spherical Cap Preserving Parameterization for Spherical Distributions
+//=================================================================================================
+namespace SPTD {
+    using namespace optix;
+
+    __inline_all__ float2 pivot_transform(float2 r, float pivot) {
+        float2 tmp1 = make_float2(r.x - pivot, r.y);
+        float2 tmp2 = pivot * r - make_float2(1, 0);
+        float x = dot(tmp1, tmp2);
+        float y = tmp1.y * tmp2.x - tmp1.x * tmp2.y;
+        float qf = dot(tmp2, tmp2);
+
+        return make_float2(x, y) / qf;
+    }
+
+    // Equation 2 in SPDT, Heitz et al. 17.
+    __inline_all__ float3 pivot_transform(const float3& r, const float3& pivot) {
+        float3 numerator = (dot(r, pivot) - 1.0f) * (r - pivot) - cross(r - pivot, cross(r, pivot));
+        float denominator = pow2(dot(r, pivot) - 1.0f) + length_squared(cross(r, pivot));
+        return numerator / denominator;
+    }
+
+    __inline_all__ OptiXRenderer::Cone pivot_transform(const OptiXRenderer::Cone& cone, const float3& pivot) {
+        // extract pivot length and direction
+        float pivot_mag = length(pivot);
+        // special case: the pivot is at the origin
+        if (pivot_mag < 0.001f)
+            return OptiXRenderer::Cone::make(-cone.direction, cone.cos_theta);
+        float3 pivot_dir = pivot / pivot_mag;
+
+        // 2D cap dir
+        float cos_phi = dot(cone.direction, pivot_dir);
+        float sin_phi = sqrt(1.0f - cos_phi * cos_phi);
+
+        // 2D basis = (pivotDir, PivotOrthogonalDirection)
+        float3 pivot_ortho_dir;
+        if (abs(cos_phi) < 0.9999f)
+            pivot_ortho_dir = (cone.direction - cos_phi * pivot_dir) / sin_phi;
+        else
+            pivot_ortho_dir = make_float3(0, 0, 0);
+
+        // compute cap 2D end points
+        float sin_theta_sqrd = sqrt(1.0f - cone.cos_theta * cone.cos_theta);
+        float a1 = cos_phi * cone.cos_theta;
+        float a2 = sin_phi * sin_theta_sqrd;
+        float a3 = sin_phi * cone.cos_theta;
+        float a4 = cos_phi * sin_theta_sqrd;
+        float2 dir1 = make_float2(a1 + a2, a3 - a4);
+        float2 dir2 = make_float2(a1 - a2, a3 + a4);
+
+        // project in 2D
+        float2 dir1_xf = pivot_transform(dir1, pivot_mag);
+        float2 dir2_xf = pivot_transform(dir2, pivot_mag);
+
+        // compute the cap 2D direction
+        float area = dir1_xf.x * dir2_xf.y - dir1_xf.y * dir2_xf.x;
+        float s = area > 0.0f ? 1.0f : -1.0f;
+        float2 dir_xf = s * normalize(dir1_xf + dir2_xf);
+
+        return OptiXRenderer::Cone::make(dir_xf.x * pivot_dir + dir_xf.y * pivot_ortho_dir,
+            dot(dir_xf, dir1_xf));
+    }
+
+    __inline_all__ float solidangle(const OptiXRenderer::Cone& c) { return TWO_PIf - TWO_PIf * c.cos_theta; }
+
+    // Based on Oat and Sander's 2007 technique in Ambient aperture lighting.
+    __inline_all__ float solidangle_of_union(const OptiXRenderer::Cone& c1, const OptiXRenderer::Cone& c2) {
+        float r1 = acos(c1.cos_theta);
+        float r2 = acos(c2.cos_theta);
+        float rd = acos(dot(c1.direction, c2.direction));
+
+        if (rd <= fmaxf(r1, r2) - fminf(r1, r2))
+            // One cap is completely inside the other
+            return TWO_PIf - TWO_PIf * fmaxf(c1.cos_theta, c2.cos_theta);
+        else if (rd >= r1 + r2)
+            // No intersection exists
+            return 0.0f;
+        else {
+            float diff = abs(r1 - r2);
+            float den = r1 + r2 - diff;
+            float x = 1.0f - clamp((rd - diff) / den, 0.0f, 1.0f); // TODO smoothstep clamps, so clamping here shouldn't be needed.
+            return smoothstep(0.0f, 1.0f, x) * (TWO_PIf - TWO_PIf * fmaxf(c1.cos_theta, c2.cos_theta));
+        }
+    }
+} // NS SPTD
 
 } // NS Distributions
 } // NS OptiXRenderer
