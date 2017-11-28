5e5acb618681b1d94840441dcbaf0b85b30b8fed
Asger Hoedt <asgerhoedt@gmail.com>
2017-11-28 22:33:18 +0100

    FitSPTD: Remove the extra pivot transform and use the one in OptiXRenderer.

--- apps/dev/FitSPTD/Pivot.h
+++ apps/dev/FitSPTD/Pivot.h
@@ -10,35 +10,10 @@
 #define _FIT_SPTD_PIVOT_H_
 
 #include <OptiXRenderer/Utils.h>
+#include <OptiXRenderer/Distributions.h>
 
 using namespace optix;
 using namespace OptiXRenderer;
-
-// pivot operator
-// x: sample on the sphere
-// xi: pivot
-float3 pivot_transform(const float3& x, const float3& xi) {
-    float3 tmp = x - xi;
-    float3 cp1 = cross(x, xi);
-    float3 cp2 = cross(tmp, cp1);
-    float dp = dot(x, xi) - 1.0f;
-    float qf = dp * dp + dot(cp1, cp1);
-
-    return ((dp * tmp - cp2) / qf);
-}
-
-// evaluate the pdf with uniform original distribution
-// x: point on the sphere
-// xi: pivot
-float pdf_uniform(const float3& x, const float3& xi) {
-    float num = 1.0f - dot(xi, xi);
-    float3 tmp = x - xi;
-    float den = dot(tmp, tmp);
-    float p = num / den;
-    float jacobian = p * p;
-    float pdf_ = jacobian / (4.0f * PIf);
-    return pdf_;
-}
 
 // TODO Parameterize by distribution, fx uniform or cosine.
 struct Pivot {
@@ -54,14 +29,21 @@ struct Pivot {
     inline float3 position() const { return distance * make_float3(sinf(theta), 0.0f, cosf(theta)); }
 
     float eval(const float3& wi) const {
-        return amplitude * pdf_uniform(wi, position());
+        float3 xi = position();
+        float num = 1.0f - dot(xi, xi);
+        float3 tmp = wi - xi;
+        float den = dot(tmp, tmp);
+        float p = num / den;
+        float jacobian = p * p;
+        float pdf = jacobian / (4.0f * PIf);
+        return amplitude * pdf;
     }
 
     float3 sample(const float U1, const float U2) const {
-        const float sphere_theta = acosf(-1.0f + 2.0f*U1);
-        const float sphere_phi = 2.0f*3.14159f * U2;
+        const float sphere_theta = acosf(-1.0f + 2.0f * U1);
+        const float sphere_phi = 2.0f * 3.14159f * U2;
         const float3 sphere_sample = make_float3(sinf(sphere_theta) * cosf(sphere_phi), sinf(sphere_theta) * sinf(sphere_phi), -cosf(sphere_theta));
-        return pivot_transform(sphere_sample, position());
+        return OptiXRenderer::Distributions::SPTD::pivot_transform(sphere_sample, position());
     }
 
     /*
