b18af91804f2c1feef6a2da0a276e5ead7601497
Asger Hoedt <asgerhoedt@gmail.com>
2017-12-30 13:40:03 +0100

    DX11Renderer: Improved elongated highlights.

--- cogs/DX11Renderer/DX11Renderer/Shaders/FragmentShader.hlsl
+++ cogs/DX11Renderer/DX11Renderer/Shaders/FragmentShader.hlsl
@@ -35,7 +35,6 @@ struct BRDFPivotTransform {
 };
 
 BRDFPivotTransform sptd_ggx_pivot(float roughness, float3 wo) {
-    // float2 sptd_ggx_fit_uv = float2(2.0f * acos(wo.z) / PI, roughness);
     float2 sptd_ggx_fit_uv = float2(abs(wo.z), roughness);
     float4 pivot_params = sptd_ggx_fit_tex.Sample(sptd_ggx_fit_sampler, sptd_ggx_fit_uv);
 
@@ -49,7 +48,7 @@ BRDFPivotTransform sptd_ggx_pivot(float roughness, float3 wo) {
     // Convert the pivot from local / wo space to tangent space. TODO Isn't there some faster way to move the vector than create the matrix explicitly?
     float3x3 basis;
     basis[0] = wo.z < 0.999f ? normalize(wo - float3(0, 0, wo.z)) : float3(1, 0, 0);
-    basis[1] = cross(float3(0, 0, 1), basis[0]);
+    basis[1] = cross(float3(0, 0, 1), basis[0]); // Has no effect. Looks like the whole thing can be inlined to use basis[0] and basis[2].z
     basis[2] = float3(0, 0, 1);
     res.pivot = mul(pivot, basis);
     return res;
@@ -71,6 +70,25 @@ BRDFPivotTransform sptd_lambert_pivot(float3 wo) {
     return res;
 }
 
+float3 find_reflection_point(float3 p0, float3 p1) {
+    float3 v = p1 - p0;
+    float3 m = p0 + v / (abs(p1.z) + abs(p0.z)) * abs(p0.z); // TODO Only needed as long as the direction to light and camera can be on opposite sides.
+    return float3(m.x, m.y, 0.0f);
+}
+
+float3 elongated_highlight_offset(float3 direction_to_camera, float3 direction_to_light, float elongation) {
+    float3 perfect_reflection_point = find_reflection_point(direction_to_camera, direction_to_light); // TODO inline to reuse camera_to_light.
+    float2 perfect_reflection_point_2D = perfect_reflection_point.xy;
+
+    float2 bitangent = normalize(direction_to_light.xy - direction_to_camera.xy);
+    float2 tangent = float2(-bitangent.y, bitangent.x);
+
+    float2 delta_x = tangent * dot(perfect_reflection_point_2D, tangent);
+    float2 delta_y = bitangent * dot(perfect_reflection_point_2D, bitangent);
+    float2 warped_reflection_point = perfect_reflection_point_2D - delta_x * elongation - delta_y / elongation;
+    return float3(warped_reflection_point, 0.0);
+}
+    
 struct PixelInput {
     float4 position : SV_POSITION;
     float4 world_position : WORLD_POSITION;
@@ -83,6 +101,7 @@ float3 integration(PixelInput input) {
 
     // Apply IBL
     float3 wo = camera_position.xyz - input.world_position.xyz;
+    float3 _wo = camera_position.xyz - input.world_position.xyz;
     float3 wi = -reflect(normalize(wo), normal);
     float3 radiance = environment_tint.rgb * material.IBL(normal, wi, input.texcoord);
 
@@ -110,36 +129,32 @@ float3 integration(PixelInput input) {
             float3 diffuse_tint, specular_tint;
             material.evaluate_tints(wo, centroid_of_union, input.texcoord, diffuse_tint, specular_tint);
 
-            { // Evaluate GGX/microfacet.
-                { // Stretch highlight based on roughness and cos_theta.
-                    // The stretching is performed by warping the lights sphere cap in local space along the view vector and its tangent.
-                    float stretch_scale = 1.0 + 32.0 * material.roughness() * material.roughness() * (1.0f - sqrt(abs(wo.z)));
-                    float3 perfect_reflection = float3(-wo.x, -wo.y, wo.z);
-                    float2 delta_reflection = light_sphere_cap.direction.xy - perfect_reflection.xy;
-
-                    // Project the delta reflection on to wo, which is itself projected down on the plane defined by the normal.
-                    float2 wo_bitangent = wo.xy / length(wo.xy);
-                    float2 wo_tangent = float2(-wo_bitangent.y, wo_bitangent.x);
-                    float2 delta_x = wo_bitangent * dot(delta_reflection, wo_bitangent);
-                    float2 delta_y = wo_tangent * dot(delta_reflection, wo_tangent);
-
-                    // Warp the direction by compressing it horizontally and stretching it vertically.
-                    light_sphere_cap.direction.xy = perfect_reflection + delta_x / stretch_scale + delta_y * stretch_scale;
-                    light_sphere_cap.direction = normalize(light_sphere_cap.direction);
-                }
-
-                Cone ggx_light_sphere_cap = SPTD::pivot_transform(light_sphere_cap, ggx_sptd.pivot);
-                float light_solidangle = SPTD::solidangle_of_union(ggx_light_sphere_cap, ggx_sptd_cap);
-                float3 l = light.sphere_power() / (PI * sphere_surface_area(light.sphere_radius()));
-                radiance += specular_tint * light_solidangle / (4.0f * PI) * l;
-            }
-
             { // Evaluate Lambert. // TODO Optimize by perhaps approximating the union solidangle and centroid.
-                // TODO Combine solidangle and centroid calculations.
+              // TODO Combine solidangle and centroid calculations.
                 float solidangle_of_light = SPTD::solidangle(light_sphere_cap);
                 float solidangle_of_union = SPTD::solidangle_of_union(hemisphere_sphere_cap, light_sphere_cap);
                 float light_radiance_scale = solidangle_of_union / solidangle_of_light;
                 radiance += diffuse_tint * BSDFs::Lambert::evaluate() * abs(centroid_of_union.z) * light_sample.radiance * light_radiance_scale;
+            }
+
+            { // Evaluate GGX/microfacet.
+                // Stretch highlight based on roughness and cos_theta.
+                // TODO Use centroid for evaluation instead of light_sphere_cap.direction
+                float3 direction_to_camera = wo * length(_wo);
+                float3 direction_to_light = light_sphere_cap.direction * length(sphere_position);
+                float3 halfway = normalize(wo + centroid_of_union);
+                float elongation = 1.0 + 4.0 * material.roughness() * (1.0f - sqrt(abs(wo.z)));
+                float3 intersection_offset = elongated_highlight_offset(direction_to_camera, direction_to_light, elongation);
+                light_sphere_cap.direction = normalize(direction_to_light - intersection_offset);
+
+                float3 adjusted_wo = normalize(direction_to_camera - intersection_offset);
+                BRDFPivotTransform adjusted_ggx_sptd = sptd_ggx_pivot(material.roughness(), adjusted_wo);
+                Cone adjusted_ggx_sptd_cap = SPTD::pivot_transform(Cone::make(float3(0.0f, 0.0f, 1.0f), 0.0f), adjusted_ggx_sptd.pivot);
+
+                Cone ggx_light_sphere_cap = SPTD::pivot_transform(light_sphere_cap, adjusted_ggx_sptd.pivot);
+                float light_solidangle = SPTD::solidangle_of_union(ggx_light_sphere_cap, adjusted_ggx_sptd_cap);
+                float3 l = light.sphere_power() / (PI * sphere_surface_area(light.sphere_radius()));
+                radiance += specular_tint * light_solidangle / (4.0f * PI) * l;
             }
 
         } else {
