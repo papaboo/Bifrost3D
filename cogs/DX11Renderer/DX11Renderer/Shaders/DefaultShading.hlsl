ec0fccc0b02d33599c65ef4f587ba1356d90b914
Asger Hoedt <asgerhoedt@gmail.com>
2018-01-11 20:18:25 +0100

    DX11Renderer: Unified DefaultShading evaluate and evaluate_tints.

--- cogs/DX11Renderer/DX11Renderer/Shaders/DefaultShading.hlsl
+++ cogs/DX11Renderer/DX11Renderer/Shaders/DefaultShading.hlsl
@@ -92,31 +92,13 @@ struct DefaultShading {
     }
 
     float3 evaluate(float3 wo, float3 wi, float2 texcoord) {
-        // TODO Use evaluate_tints.
-        bool is_same_hemisphere = wi.z * wo.z >= 0.00000001f;
-        if (!is_same_hemisphere)
-            return float3(0.0f, 0.0f, 0.0f);
-
-        // Flip directions if on the backside of the material.
-        if (wo.z < 0.0f) {
-            wi.z = -wi.z;
-            wo.z = -wo.z;
-        }
-
-        float3 tint = m_tint;
-        if (m_tint_texture_index != 0)
-            tint *= color_tex.Sample(color_sampler, texcoord).rgb;
-
-        float specularity = compute_specularity(m_specularity, m_metallic);
-        float3 specular_tint = lerp(float3(1.0f, 1.0f, 1.0f), m_tint, m_metallic);
-        float3 diffuse_tint = tint * (1.0f - compute_specular_rho(specularity, wo.z, m_roughness));
-
-        float3 halfway = normalize(wo + wi);
-        float ggx_alpha = BSDFs::GGX::alpha_from_roughness(m_roughness);
-        float fresnel = schlick_fresnel(specularity, dot(wo, halfway));
-        float3 specular = specular_tint * BSDFs::GGX::evaluate(ggx_alpha, wo, wi, halfway);
+        float3 diffuse_tint, specular_tint;
+        evaluate_tints(wo, wi, texcoord, diffuse_tint, specular_tint);
         float3 diffuse = diffuse_tint * BSDFs::Lambert::evaluate();
-        return diffuse + specular * fresnel;
+        float ggx_alpha = BSDFs::GGX::alpha_from_roughness(m_roughness);
+        float3 halfway = normalize(wo + wi);
+        float3 specular = specular_tint * BSDFs::GGX::evaluate(ggx_alpha, wo, wi, halfway);
+        return diffuse + specular;
     }
 
     // Apply the shading model to the IBL.
