f11b3c19619c84071d47c5baf4d0e41c7bd70f5c
Asger Hoedt <asgerhoedt@gmail.com>
2018-02-18 07:38:00 +0100

    Cogwheel: Add unreal engine 4 filmic tonemapper.

--- cogs/Cogwheel/Cogwheel/Math/Tonemapping.h
+++ cogs/Cogwheel/Cogwheel/Math/Tonemapping.h
@@ -10,6 +10,8 @@
 #define _COGWHEEL_MATH_TONEMAPPING_H_
 
 #include <Cogwheel/Math/Color.h>
+#include <Cogwheel/Math/Matrix.h>
+#include <Cogwheel/Math/Utils.h>
 
 namespace Cogwheel {
 namespace Math {
@@ -39,7 +41,9 @@ struct Parameters final {
 // Free functions.
 // ------------------------------------------------------------------------------------------------
 
+// ------------------------------------------------------------------------------------------------
 // Advanced tonemapping operator
+// ------------------------------------------------------------------------------------------------
 // http://perso.univ-lyon1.fr/jean-claude.iehl/Public/educ/GAMA/2007/gdc07/Post-Processing_Pipeline.pdf
 inline RGB reinhard(RGB color, float white_level_sqrd) {
     float luminance = luma(color);
@@ -47,7 +51,9 @@ inline RGB reinhard(RGB color, float white_level_sqrd) {
     return color * (tonemapped_luminance / luminance);
 }
 
+// ------------------------------------------------------------------------------------------------
 // Uncharted 2's filmic operator.
+// ------------------------------------------------------------------------------------------------
 inline RGB uncharted2(RGB color, float shoulder_strength, float linear_strength, float linear_angle, float toe_strength, float toe_numerator, float toe_denominator, float linear_white) {
     auto uncharted2_tonemap_helper = [](RGB color, float shoulder_strength, float linear_strength, float linear_angle, float toe_strength, float toe_numerator, float toe_denominator) -> RGB {
         RGB x = color;
@@ -62,6 +68,103 @@ inline RGB uncharted2(RGB color, float shoulder_strength, float linear_strength,
 
     return uncharted2_tonemap_helper(color, shoulder_strength, linear_strength, linear_angle, toe_strength, toe_numerator, toe_denominator) /
         uncharted2_tonemap_helper(RGB(linear_white), shoulder_strength, linear_strength, linear_angle, toe_strength, toe_numerator, toe_denominator);
+}
+
+// ------------------------------------------------------------------------------------------------
+// Unreal Engine 4 filmic tonemapping, see TonemapCommon.ush FilmToneMap function for the 
+// tonemapping and ACES.ush for the conversion matrices.
+// ------------------------------------------------------------------------------------------------
+
+// Bradford chromatic adaptation transforms between ACES white point (D60) and sRGB white point (D65)
+const Matrix3x3f D65_2_D60_CAT = {
+    1.01303f,    0.00610531f, -0.014971f,
+    0.00769823f, 0.998165f,   -0.00503203f,
+    -0.00284131f, 0.00468516f,  0.924507f,
+};
+
+const Matrix3x3f sRGB_2_XYZ_MAT = {
+    0.4124564f, 0.3575761f, 0.1804375f,
+    0.2126729f, 0.7151522f, 0.0721750f,
+    0.0193339f, 0.1191920f, 0.9503041f,
+};
+
+const Matrix3x3f XYZ_2_AP1_MAT = {
+    1.6410233797f, -0.3248032942f, -0.2364246952f,
+    -0.6636628587f,  1.6153315917f,  0.0167563477f,
+    0.0117218943f, -0.0082844420f,  0.9883948585f,
+};
+
+const Matrix3x3f AP1_2_XYZ_MAT = {
+    0.6624541811f, 0.1340042065f, 0.1561876870f,
+    0.2722287168f, 0.6740817658f, 0.0536895174f,
+    -0.0055746495f, 0.0040607335f, 1.0103391003f,
+};
+
+const Vector3f AP1_RGB2Y = {
+    0.2722287168f, //AP1_2_XYZ_MAT[0][1],
+    0.6740817658f, //AP1_2_XYZ_MAT[1][1],
+    0.0536895174f, //AP1_2_XYZ_MAT[2][1]
+};
+
+inline RGB unreal4(RGB color, float slope = 0.91f, float toe = 0.53f, float shoulder = 0.23f, float black_clip = 0.0f, float white_clip = 0.035f) {
+
+    const Matrix3x3f sRGB_to_AP1 = XYZ_2_AP1_MAT * D65_2_D60_CAT * sRGB_2_XYZ_MAT;
+
+    // Use ACEScg primaries as working space
+    Vector3f working_color = sRGB_to_AP1 * Vector3f(color.r, color.g, color.b);
+    working_color = max(Vector3f::zero(), working_color);
+
+    // Pre desaturate
+    working_color = lerp(Vector3f(dot(working_color, AP1_RGB2Y)), working_color, 0.96f);
+
+    const float toe_scale = 1.0f + black_clip - toe;
+    const float shoulder_scale = 1.0f + white_clip - shoulder;
+
+    const float in_match = 0.18f, out_match = 0.18f;
+
+    float toe_match;
+    if (toe > 0.8f)
+        // 0.18 will be on straight segment
+        toe_match = (1.0f - toe - out_match) / slope + log10(in_match);
+    else {
+        // 0.18 will be on toe segment
+
+        // Solve for toe_match such that input of InMatch gives output of OutMatch.
+        const float bt = (out_match + black_clip) / toe_scale - 1.0f;
+        toe_match = log10(in_match) - 0.5f * log((1.0f + bt) / (1.0f - bt)) * (toe_scale / slope);
+    }
+
+    float straight_match = (1.0f - toe) / slope - toe_match;
+    float shoulder_match = shoulder / slope - straight_match;
+
+    Vector3f log_color = Vector3f(log10(working_color.x), log10(working_color.y), log10(working_color.z));
+    Vector3f straight_color = (log_color + straight_match) * slope;
+
+    // Vector3f toe_color = (-black_clip) + (2 * toe_scale) / (1 + exp((-2 * slope / toe_scale) * (log_color - toe_match)));
+    // Vector3f shoulder_color = (1 + white_clip) - (2 * shoulder_scale) / (1 + exp((2 * slope / shoulder_scale) * (log_color - shoulder_match)));
+
+    auto exp = [](Vector3f v) -> Vector3f { return Vector3f(expf(v.x), expf(v.y), expf(v.z)); };
+
+    Vector3f toe_color = (-black_clip) + (2.0f * toe_scale) / (1.0f + exp((log_color - toe_match) * (-2 * slope / toe_scale)));
+    toe_color = Vector3f(log_color.x < toe_match ? toe_color.x : straight_color.x,
+                         log_color.y < toe_match ? toe_color.y : straight_color.y,
+                         log_color.z < toe_match ? toe_color.z : straight_color.z);
+
+    Vector3f shoulder_color = (1.0f + white_clip) - (2.0f * shoulder_scale) / (1.0f + exp((log_color - shoulder_match) * (2 * slope / shoulder_scale)));
+    shoulder_color = Vector3f(log_color.x > shoulder_match ? shoulder_color.x : straight_color.x,
+                              log_color.y > shoulder_match ? shoulder_color.y : straight_color.y,
+                              log_color.z > shoulder_match ? shoulder_color.z : straight_color.z);
+
+    Vector3f t = clamp((log_color - toe_match) / (shoulder_match - toe_match), Vector3f::zero(), Vector3f::one());
+    t = shoulder_match < toe_match ? 1.0f - t : t;
+    t = (3.0f - t * 2.0f) * t * t;
+    Vector3f tone_color = lerp(toe_color, shoulder_color, t);
+
+    // Post desaturate
+    tone_color = lerp(Vector3f(dot(tone_color, AP1_RGB2Y)), tone_color, 0.93f);
+
+    // Returning positive AP1 values
+    return RGB(fmaxf(0.0f, tone_color.x), fmaxf(0.0f, tone_color.y), fmaxf(0.0f, tone_color.z));
 }
 
 } // NS Tonemapping
