c48eb990e7f3dfcc971c1fe1629dd547b09da743
Asger Hoedt <asgerhoedt@gmail.com>
2018-03-16 21:17:17 +0100

    DX11Renderer: Add ExposureHistogram to Tonemapper.

--- cogs/DX11Renderer/DX11Renderer/Tonemapper.cpp
+++ cogs/DX11Renderer/DX11Renderer/Tonemapper.cpp
@@ -13,6 +13,59 @@ using namespace Cogwheel::Math;
 
 namespace DX11Renderer {
 
+// ------------------------------------------------------------------------------------------------
+// Exposure histogram
+// ------------------------------------------------------------------------------------------------
+ExposureHistogram::ExposureHistogram()
+    : m_histogram_reduction(nullptr), m_histogram_SRV(nullptr), m_histogram_UAV(nullptr)
+    , m_linear_exposure_computation(nullptr), m_linear_exposure_SRV(nullptr), m_linear_exposure_UAV(nullptr) { }
+
+ExposureHistogram::ExposureHistogram(ID3D11Device1& device, const std::wstring& shader_folder_path) {
+    // Create shaders.
+    OID3DBlob reduce_exposure_histogram_blob = compile_shader(shader_folder_path + L"Compute\\ReduceExposureHistogram.hlsl", "cs_5_0", "reduce");
+    THROW_ON_FAILURE(device.CreateComputeShader(UNPACK_BLOB_ARGS(reduce_exposure_histogram_blob), nullptr, &m_histogram_reduction));
+
+    OID3DBlob linear_exposure_computation_blob = compile_shader(shader_folder_path + L"Compute\\ReduceExposureHistogram.hlsl", "cs_5_0", "compute_exposure");
+    THROW_ON_FAILURE(device.CreateComputeShader(UNPACK_BLOB_ARGS(linear_exposure_computation_blob), nullptr, &m_linear_exposure_computation));
+
+    // Create buffers
+    create_default_buffer(device, DXGI_FORMAT_R32_UINT, bin_count, &m_histogram_SRV, &m_histogram_UAV);
+    create_default_buffer(device, DXGI_FORMAT_R32_FLOAT, 1, &m_linear_exposure_SRV, &m_linear_exposure_UAV);
+}
+
+OID3D11ShaderResourceView& ExposureHistogram::reduce_histogram(ID3D11DeviceContext1& context, ID3D11Buffer* constants,
+                                                               ID3D11ShaderResourceView* pixels, unsigned int image_width) {
+
+    context.CSSetShader(m_histogram_reduction, nullptr, 0u);
+    context.CSSetConstantBuffers(0, 1, &constants);
+    context.CSSetShaderResources(0, 1, &pixels);
+    context.CSSetUnorderedAccessViews(0, 1, &m_histogram_UAV, 0u);
+    unsigned int group_count_x = ceil_divide(image_width, group_width);
+    context.Dispatch(group_count_x, 1, 1);
+
+    return m_histogram_SRV;
+}
+
+OID3D11ShaderResourceView& ExposureHistogram::compute_average_exposure(ID3D11DeviceContext1& context, ID3D11Buffer* constants,
+                                                                       ID3D11ShaderResourceView* pixels, unsigned int image_width) {
+    context.CSSetConstantBuffers(0, 1, &constants);
+    context.CSSetShaderResources(0, 1, &pixels);
+    ID3D11UnorderedAccessView* UAVs[2] = { m_histogram_UAV, m_linear_exposure_UAV };
+    context.CSSetUnorderedAccessViews(0, 2, UAVs, 0u);
+
+    context.CSSetShader(m_histogram_reduction, nullptr, 0u);
+    unsigned int group_count_x = ceil_divide(image_width, group_width);
+    context.Dispatch(group_count_x, 1, 1);
+
+    context.CSSetShader(m_linear_exposure_computation, nullptr, 0u);
+    context.Dispatch(1, 1, 1);
+
+    return m_linear_exposure_SRV;
+}
+
+// ------------------------------------------------------------------------------------------------
+// Tonemapper
+// ------------------------------------------------------------------------------------------------
 Tonemapper::Tonemapper()
     : m_fullscreen_VS(nullptr), m_log_luminance_PS(nullptr)
     , m_linear_tonemapping_PS(nullptr), m_reinhard_tonemapping_PS(nullptr), m_uncharted2_tonemapping_PS(nullptr), m_filmic_tonemapping_PS(nullptr)
@@ -20,6 +73,14 @@ Tonemapper::Tonemapper()
 
 Tonemapper::Tonemapper(ID3D11Device1& device, const std::wstring& shader_folder_path)
     : m_width(0), m_height(0), m_log_luminance_RTV(nullptr), m_log_luminance_SRV(nullptr) {
+
+    m_host_constants.min_log_luminance = -8.0f;
+    m_host_constants.max_log_luminance = 4.0f;
+    m_host_constants.min_percentage = 0.7f;
+    m_host_constants.max_percentage = 0.95f;
+    THROW_ON_FAILURE(create_constant_buffer(device, m_host_constants, &m_constants /*, D3D11_USAGE_DEFAULT*/));
+
+    m_exposure_histogram = ExposureHistogram(device, shader_folder_path);
 
     { // Setup shaders
         const std::wstring shader_filename = shader_folder_path + L"Tonemapping.hlsl";
@@ -137,52 +198,6 @@ void Tonemapper::tonemap(ID3D11DeviceContext1& context, Tonemapping::Parameters 
             context.Draw(3, 0);
         }
     }
-}
-
-// ------------------------------------------------------------------------------------------------
-// Exposure histogram
-// ------------------------------------------------------------------------------------------------
-ExposureHistogram::ExposureHistogram(ID3D11Device1& device, const std::wstring& shader_folder_path) {
-    // Create shaders.
-    OID3DBlob reduce_exposure_histogram_blob = compile_shader(shader_folder_path + L"Compute\\ReduceExposureHistogram.hlsl", "cs_5_0", "reduce");
-    THROW_ON_FAILURE(device.CreateComputeShader(UNPACK_BLOB_ARGS(reduce_exposure_histogram_blob), nullptr, &m_histogram_reduction));
-
-    OID3DBlob linear_exposure_computation_blob = compile_shader(shader_folder_path + L"Compute\\ReduceExposureHistogram.hlsl", "cs_5_0", "compute_exposure");
-    THROW_ON_FAILURE(device.CreateComputeShader(UNPACK_BLOB_ARGS(linear_exposure_computation_blob), nullptr, &m_linear_exposure_computation));
-
-    // Create buffers
-    create_default_buffer(device, DXGI_FORMAT_R32_UINT, bin_count, &m_histogram_SRV, &m_histogram_UAV);
-    create_default_buffer(device, DXGI_FORMAT_R32_FLOAT, 1, &m_linear_exposure_SRV, &m_linear_exposure_UAV);
-}
-
-OID3D11ShaderResourceView& ExposureHistogram::reduce_histogram(ID3D11DeviceContext1& context, OID3D11Buffer& constants, 
-                                                               OID3D11ShaderResourceView& pixels, unsigned int image_width) {
-
-    context.CSSetShader(m_histogram_reduction, nullptr, 0u);
-    context.CSSetConstantBuffers(0, 1, &constants);
-    context.CSSetShaderResources(0, 1, &pixels);
-    context.CSSetUnorderedAccessViews(0, 1, &m_histogram_UAV, 0u);
-    unsigned int group_count_x = ceil_divide(image_width, group_width);
-    context.Dispatch(group_count_x, 1, 1);
-
-    return m_histogram_SRV;
-}
-
-OID3D11ShaderResourceView& ExposureHistogram::compute_average_exposure(ID3D11DeviceContext1& context, OID3D11Buffer& constants,
-                                                                       OID3D11ShaderResourceView& pixels, unsigned int image_width) {
-    context.CSSetConstantBuffers(0, 1, &constants);
-    context.CSSetShaderResources(0, 1, &pixels);
-    ID3D11UnorderedAccessView* UAVs[2] = { m_histogram_UAV.get(), m_linear_exposure_UAV.get() };
-    context.CSSetUnorderedAccessViews(0, 2, UAVs, 0u);
-
-    context.CSSetShader(m_histogram_reduction, nullptr, 0u);
-    unsigned int group_count_x = ceil_divide(image_width, group_width);
-    context.Dispatch(group_count_x, 1, 1);
-
-    context.CSSetShader(m_linear_exposure_computation, nullptr, 0u);
-    context.Dispatch(1, 1, 1);
-
-    return m_linear_exposure_SRV;
 }
 
 } // NS DX11Renderer
--- cogs/DX11Renderer/DX11Renderer/Tonemapper.h
+++ cogs/DX11Renderer/DX11Renderer/Tonemapper.h
@@ -16,6 +16,53 @@
 namespace DX11Renderer {
 
 // ------------------------------------------------------------------------------------------------
+// Functionality for computing a histogram from a list of pixels 
+// and dynamically determining the exposure of those pixels.
+// ------------------------------------------------------------------------------------------------
+class ExposureHistogram {
+public:
+    static const unsigned int bin_count = 64;
+    static const unsigned int group_width = 16u;
+    static const unsigned int group_height = 8u;
+    static const unsigned int group_size = group_width * group_height;
+
+    ExposureHistogram();
+
+    ExposureHistogram(ID3D11Device1& device, const std::wstring& shader_folder_path);
+
+    ExposureHistogram& operator=(ExposureHistogram&& rhs) {
+        m_histogram_reduction = std::move(rhs.m_histogram_reduction);
+        m_histogram_SRV = std::move(rhs.m_histogram_SRV);
+        m_histogram_UAV = std::move(rhs.m_histogram_UAV);
+
+        m_linear_exposure_computation = std::move(rhs.m_linear_exposure_computation);
+        m_linear_exposure_SRV = std::move(rhs.m_linear_exposure_SRV);
+        m_linear_exposure_UAV = std::move(rhs.m_linear_exposure_UAV);
+
+        return *this;
+    }
+
+    OID3D11ShaderResourceView& reduce_histogram(ID3D11DeviceContext1& context, ID3D11Buffer* constants,
+        ID3D11ShaderResourceView* pixels, unsigned int image_width);
+
+    OID3D11ShaderResourceView& compute_average_exposure(ID3D11DeviceContext1& context, ID3D11Buffer* constants,
+        ID3D11ShaderResourceView* pixels, unsigned int image_width);
+
+private:
+    ExposureHistogram(ExposureHistogram& other) = delete;
+    ExposureHistogram(ExposureHistogram&& other) = delete;
+    ExposureHistogram& operator=(ExposureHistogram& rhs) = delete;
+
+    OID3D11ComputeShader m_histogram_reduction;
+    OID3D11ShaderResourceView m_histogram_SRV;
+    OID3D11UnorderedAccessView m_histogram_UAV;
+
+    OID3D11ComputeShader m_linear_exposure_computation;
+    OID3D11ShaderResourceView m_linear_exposure_SRV;
+    OID3D11UnorderedAccessView m_linear_exposure_UAV;
+};
+
+// ------------------------------------------------------------------------------------------------
 // Tonemapping implementation with support for various tonemappers.
 // Sources:
 // * https://mynameismjp.wordpress.com/2010/04/30/a-closer-look-at-tone-mapping/
@@ -24,10 +71,23 @@ namespace DX11Renderer {
 class Tonemapper {
 public:
 
+    struct Constants {
+        float min_log_luminance;
+        float max_log_luminance;
+        float min_percentage;
+        float max_percentage;
+    };
+
     Tonemapper();
     Tonemapper(ID3D11Device1& device, const std::wstring& shader_folder_path);
 
     Tonemapper& operator=(Tonemapper&& rhs) {
+
+        m_host_constants = rhs.m_host_constants;
+        m_constants = std::move(rhs.m_constants);
+
+        m_exposure_histogram = std::move(rhs.m_exposure_histogram);
+
         m_fullscreen_VS = std::move(rhs.m_fullscreen_VS);
         m_log_luminance_PS = std::move(rhs.m_log_luminance_PS);
         m_linear_tonemapping_PS = std::move(rhs.m_linear_tonemapping_PS);
@@ -51,6 +111,11 @@ private:
     Tonemapper(Tonemapper&& other) = delete;
     Tonemapper& operator=(Tonemapper& rhs) = delete;
 
+    Constants m_host_constants;
+    OID3D11Buffer m_constants;
+
+    ExposureHistogram m_exposure_histogram;
+
     OID3D11VertexShader m_fullscreen_VS;
     OID3D11PixelShader m_log_luminance_PS;
     OID3D11PixelShader m_linear_tonemapping_PS;
@@ -62,51 +127,6 @@ private:
     OID3D11RenderTargetView m_log_luminance_RTV;
     OID3D11ShaderResourceView m_log_luminance_SRV;
     OID3D11SamplerState m_log_luminance_sampler;
-};
-
-// ------------------------------------------------------------------------------------------------
-// Functionality for computing a histogram from a list of pixels 
-// and dynamically determining the exposure of those pixels.
-// ------------------------------------------------------------------------------------------------
-class ExposureHistogram {
-public:
-    static const unsigned int bin_count = 64;
-    static const unsigned int group_width = 16u;
-    static const unsigned int group_height = 8u;
-    static const unsigned int group_size = group_width * group_height;
-
-    ExposureHistogram(ID3D11Device1& device, const std::wstring& shader_folder_path);
-
-    ExposureHistogram& operator=(ExposureHistogram&& rhs) {
-        m_histogram_reduction = std::move(rhs.m_histogram_reduction);
-        m_histogram_SRV = std::move(rhs.m_histogram_SRV);
-        m_histogram_UAV = std::move(rhs.m_histogram_UAV);
-
-        m_linear_exposure_computation = std::move(rhs.m_linear_exposure_computation);
-        m_linear_exposure_SRV = std::move(rhs.m_linear_exposure_SRV);
-        m_linear_exposure_UAV = std::move(rhs.m_linear_exposure_UAV);
-
-        return *this;
-    }
-
-    OID3D11ShaderResourceView& reduce_histogram(ID3D11DeviceContext1& context, OID3D11Buffer& constants, 
-                                                OID3D11ShaderResourceView& pixels, unsigned int image_width);
-
-    OID3D11ShaderResourceView& compute_average_exposure(ID3D11DeviceContext1& context, OID3D11Buffer& constants,
-                                                        OID3D11ShaderResourceView& pixels, unsigned int image_width);
-
-private:
-    ExposureHistogram(ExposureHistogram& other) = delete;
-    ExposureHistogram(ExposureHistogram&& other) = delete;
-    ExposureHistogram& operator=(ExposureHistogram& rhs) = delete;
-
-    OID3D11ComputeShader m_histogram_reduction;
-    OID3D11ShaderResourceView m_histogram_SRV;
-    OID3D11UnorderedAccessView m_histogram_UAV;
-
-    OID3D11ComputeShader m_linear_exposure_computation;
-    OID3D11ShaderResourceView m_linear_exposure_SRV;
-    OID3D11UnorderedAccessView m_linear_exposure_UAV;
 };
 
 } // NS DX11Renderer
