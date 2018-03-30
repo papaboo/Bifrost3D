e9ca12d5d1718c2e52f199c8285449667ff777ad
Asger Hoedt <asgerhoedt@gmail.com>
2018-03-30 22:37:39 +0200

    CogwheelTest: TransformTest should include transform, not quaternion.

--- tests/CogwheelTests/Math/TransformTest.h
+++ tests/CogwheelTests/Math/TransformTest.h
@@ -9,7 +9,7 @@
 #ifndef _COGWHEEL_MATH_TRANSFORM_TEST_H_
 #define _COGWHEEL_MATH_TRANSFORM_TEST_H_
 
-#include <Cogwheel/Math/Quaternion.h>
+#include <Cogwheel/Math/Transform.h>
 #include <Cogwheel/Math/Conversions.h>
 
 #include <gtest/gtest.h>
