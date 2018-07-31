5d94ab91c787eba1f44778afd8a1fa61bb2b9c6b
Asger Hoedt <asgerhoedt@gmail.com>
2018-07-31 22:01:32 +0200

    DX11Renderer: Always use texel fetch to debug g-buffer data to support larger g-buffers.

--- cogs/DX11Renderer/DX11Renderer/Shaders/Debug.hlsl
+++ cogs/DX11Renderer/DX11Renderer/Shaders/Debug.hlsl
@@ -18,7 +18,6 @@ cbuffer scene_variables : register(b13) {
 
 struct Varyings {
     float4 position : SV_POSITION;
-    float2 uv : TEXCOORD;
 };
 
 Varyings main_vs(uint vertex_ID : SV_VertexID) {
@@ -27,8 +26,6 @@ Varyings main_vs(uint vertex_ID : SV_VertexID) {
     output.position.x = vertex_ID == 2 ? 3 : -1;
     output.position.y = vertex_ID == 0 ? -3 : 1;
     output.position.zw = float2(0.0f, 1.0f);
-    output.uv = output.position.xy * 0.5 + 0.5;
-    output.uv.y = 1.0f - output.uv.y;
     return output;
 }
 
@@ -48,13 +45,12 @@ cbuffer debug_constants : register(b1) {
 float4 display_debug_ps(Varyings input) : SV_TARGET {
     float3 color;
     if (mode == 1) { // Normals
-        float2 encoded_normal = normal_tex.SampleLevel(point_sampler, input.uv, 0).xy;
-        // color = float3(0.5 * encoded_normal + 0.5, 0);
+        float2 encoded_normal = normal_tex[input.position.xy].xy;
         float3 normal = decode_ss_octahedral_normal(encoded_normal);
         color = 0.5 * normal + 0.5;
     }
     else if (mode == 2) // Depth
-        color = depth_tex.SampleLevel(point_sampler, input.uv, 0).rrr;
+        color = depth_tex[input.position.xy].rrr;
     else
         color = ao_tex[input.position.xy + scene_vars.g_buffer_to_ao_index_offset].rrr;
     return float4(color, 1);
