ff6189893055ed15069bb2a492bf363f92ceaba4
Asger Hoedt <asgerhoedt@gmail.com>
2018-11-06 11:38:45 +0100

    DX11Renderer: Remove already implemented future work and unused method that exposed inner workings of TransformManager.

--- cogs/DX11Renderer/DX11Renderer/TransformManager.h
+++ cogs/DX11Renderer/DX11Renderer/TransformManager.h
@@ -1,10 +1,10 @@
 // DirectX 11 transform manager.
-// ---------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 // Copyright (C) 2016, Cogwheel. See AUTHORS.txt for authors
 //
-// This program is open source and distributed under the New BSD License. See
-// LICENSE.txt for more detail.
-// ---------------------------------------------------------------------------
+// This program is open source and distributed under the New BSD License.
+// See LICENSE.txt for more detail.
+//-------------------------------------------------------------------------------------------------
 
 #ifndef _DX11RENDERER_RENDERER_TRANSFORM_MANAGER_H_
 #define _DX11RENDERER_RENDERER_TRANSFORM_MANAGER_H_
@@ -19,12 +19,10 @@
 
 namespace DX11Renderer {
 
-//----------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 // Transform manager.
 // Uploads and manages a buffer of transforms.
-// Future work:
-// * Support a variable number of transforms.
-//----------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 class TransformManager {
 private:
 
@@ -40,7 +38,6 @@ public:
     TransformManager& operator=(TransformManager&& rhs) = default;
     
     inline Cogwheel::Math::Transform& get_transform(unsigned int transform_index) { return m_transforms[transform_index]; }
-    inline ID3D11Buffer** get_constant_buffer_addr() { return m_constant_array.get_buffer_addr(); }
     inline void bind_transform(ID3D11DeviceContext1& context, unsigned int slot, unsigned int transform_index) { m_constant_array.VS_set(&context, slot, transform_index); }
 
     void handle_updates(ID3D11Device1& device, ID3D11DeviceContext1& context);
