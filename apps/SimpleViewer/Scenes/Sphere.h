9b5a674217599f6356627015efc142e397354977
Asger Hoedt <asgerhoedt@gmail.com>
2018-11-11 14:43:20 +0100

    SimpleViewer: Sphere scene modifies original material instead of swapping between predefined materials.

--- apps/SimpleViewer/Scenes/Sphere.h
+++ apps/SimpleViewer/Scenes/Sphere.h
@@ -40,69 +40,54 @@ void create_sphere_scene(Core::Engine& engine, Scene::Cameras::UID camera_ID, Sc
         Cameras::set_transform(camera_ID, cam_transform);
     }
 
-    { // Setup material swapper.
-        class MaterialSwapper {
-            MeshModel m_model;
-            Materials::UID* m_mat_IDs = new Materials::UID[5];
-        public:
-            MaterialSwapper(Scene::SceneRoots::UID scene_ID) {
-                Materials::Data plastic_mat_data = Materials::Data::create_dielectric(RGB(0.005f, 0.01f, 0.25f), 0.05f, 0.5f);
-                m_mat_IDs[0] = Materials::create("Plastic", plastic_mat_data);
+    { // Create sphere.
+        auto plastic_mat_data = Materials::Data::create_dielectric(RGB(0.005f, 0.01f, 0.25f), 0.05f, 0.5f);
+        Materials::UID material_ID = Materials::create("Material", plastic_mat_data);
 
-                Materials::Data white_mat_data = Materials::Data::create_dielectric(RGB(1.0f), 0.0f, 0.25);
-                m_mat_IDs[1] = Materials::create("White", white_mat_data);
+        Meshes::UID sphere_mesh_ID = MeshCreation::revolved_sphere(1024, 512);
 
-                Materials::Data rubber_mat_data = Materials::Data::create_dielectric(RGB(1.0f), 0.95f, 0.5f);
-                m_mat_IDs[2] = Materials::create("Rubber", rubber_mat_data);
+        SceneNode node = SceneNodes::create("Sphere");
+        MeshModel model = MeshModels::create(node.get_ID(), sphere_mesh_ID, material_ID);
 
-                Materials::Data gold_mat_data = Materials::Data::create_metal(RGB(1.0f, 0.766f, 0.336f), 0.02f, 0.0f);
-                m_mat_IDs[3] = Materials::create("Gold", gold_mat_data);
+        SceneNode root_node = SceneRoots::get_root_node(scene_ID);
+        node.set_parent(root_node);
+    }
 
-                Materials::Data copper_mat_data = Materials::Data::create_metal(RGB(0.8f, 0.4f, 0.3f), 0.5f, 0.0f);
-                m_mat_IDs[4] = Materials::create("Copper", copper_mat_data);
+    { // Setup sphere and material swapper.
+        static auto swap_material = [](Core::Engine& engine) {
+            using namespace Input;
 
-                { // Create sphere.
-                    Materials::Data material_data = Materials::Data::create_dielectric(RGB(1.0f), 0.0f, 0.25);
-                    Materials::UID material_ID = Materials::create("White material", material_data);
+            static auto update_material = [](Materials::Data data) {
+                Material m_material = *Materials::get_iterable().begin();
+                m_material.set_tint(data.tint);
+                m_material.set_roughness(data.roughness);
+                m_material.set_metallic(data.metallic);
+                m_material.set_specularity(data.specularity);
+            };
 
-                    Meshes::UID sphere_mesh_ID = MeshCreation::revolved_sphere(1024, 512);
-
-                    SceneNode node = SceneNodes::create("Sphere");
-                    m_model = MeshModels::create(node.get_ID(), sphere_mesh_ID, m_mat_IDs[0]);
-
-                    SceneNode root_node = SceneRoots::get_root_node(scene_ID);
-                    node.set_parent(root_node);
-                }
-            }
-
-            void update_material(int id) {
-                m_model.set_material_ID(m_mat_IDs[id]);
-            }
-
-            void update(Core::Engine& engine) {
-                using namespace Input;
-                const Keyboard* const keyboard = engine.get_keyboard();
-                if (keyboard->was_released(Keyboard::Key::Key1))
-                    update_material(0);
-                else if (keyboard->was_released(Keyboard::Key::Key2))
-                    update_material(1);
-                else if (keyboard->was_released(Keyboard::Key::Key3))
-                    update_material(2);
-                else if (keyboard->was_released(Keyboard::Key::Key4))
-                    update_material(3);
-                else if (keyboard->was_released(Keyboard::Key::Key5))
-                    update_material(4);
+            const Keyboard* const keyboard = engine.get_keyboard();
+            if (keyboard->was_released(Keyboard::Key::Key1)) {
+                auto plastic_mat_data = Materials::Data::create_dielectric(RGB(0.005f, 0.01f, 0.25f), 0.05f, 0.5f);
+                update_material(plastic_mat_data);
+            } else if (keyboard->was_released(Keyboard::Key::Key2)) {
+                auto white_mat_data = Materials::Data::create_dielectric(RGB::white(), 0.0f, 0.25);
+                update_material(white_mat_data);
+            } else if (keyboard->was_released(Keyboard::Key::Key3)) {
+                auto rubber_mat_data = Materials::Data::create_dielectric(RGB::white(), 0.95f, 0.5f);
+                update_material(rubber_mat_data);
+            } else if (keyboard->was_released(Keyboard::Key::Key4)) {
+                auto gold_mat_data = Materials::Data::create_metal(RGB(1.0f, 0.766f, 0.336f), 0.02f, 0.0f);
+                update_material(gold_mat_data);
+            } else if (keyboard->was_released(Keyboard::Key::Key5)) {
+                auto copper_mat_data = Materials::Data::create_metal(RGB(0.8f, 0.4f, 0.3f), 0.5f, 0.0f);
+                update_material(copper_mat_data);
             }
         };
 
-        MaterialSwapper* mat_swapper = new MaterialSwapper(scene_ID);
-        engine.add_mutating_callback([=, &engine]() { mat_swapper->update(engine); });
-
-        // TODO Destroy on shutdown.
+        engine.add_mutating_callback([=, &engine]() { swap_material(engine); });
     }
 
     { // Setup GUI.
-
         class MaterialGUI final : public ImGui::IImGuiFrame {
         public:
             MaterialGUI(Material material) : m_material(material) { }
