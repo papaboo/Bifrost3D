aed34471c540d00a1120053a5cd42573a60fb416
Asger Vejen Hoedt <asger.hoedt@3shape.com>
2019-01-14 07:30:09 +0100

    OptiXRendererTest: Updating one more file header.

--- tests/OptiXRendererTests/ShadingModels/DefaultShadingTest.h
+++ tests/OptiXRendererTests/ShadingModels/DefaultShadingTest.h
@@ -1,9 +1,9 @@
 // Test OptiXRenderer's default shading model.
 // ---------------------------------------------------------------------------
-// Copyright (C) 2015-2016, Bifrost. See AUTHORS.txt for authors
+// Copyright (C) Bifrost. See AUTHORS.txt for authors.
 //
-// This program is open source and distributed under the New BSD License. See
-// LICENSE.txt for more detail.
+// This program is open source and distributed under the New BSD License.
+// See LICENSE.txt for more detail.
 // ---------------------------------------------------------------------------
 
 #ifndef _OPTIXRENDERER_SHADING_MODEL_DEFAULT_TEST_H_
