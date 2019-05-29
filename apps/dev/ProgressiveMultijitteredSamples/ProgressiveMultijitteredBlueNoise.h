0ab95d28deaf011084b4928f2e9a0a266e511916
Asger Vejen Hoedt <asger.hoedt@3shape.com>
2019-05-29 22:24:42 +0200

    PMJ: 3D LCG, PMJ and PMJBN sample generation.

--- apps/dev/ProgressiveMultijitteredSamples/ProgressiveMultijitteredBlueNoise.h
+++ apps/dev/ProgressiveMultijitteredSamples/ProgressiveMultijitteredBlueNoise.h
@@ -194,4 +194,154 @@ std::vector<Bifrost::Math::Vector2f> generate_progressive_multijittered_bluenois
     return samples;
 }
 
+std::vector<Bifrost::Math::Vector3f> generate_3D_progressive_multijittered_samples(unsigned int subdivisions, unsigned int blue_noise_samples = 8) {
+    using namespace Bifrost::Math;
+
+    auto rng = RNG::LinearCongruential(19349669);
+    auto rnd = [&]() -> float { return rng.sample1f(); };
+
+    unsigned int m = 1 << subdivisions;
+    unsigned int M = m * m * m;
+    auto samples = std::vector<Vector3f>(); samples.reserve(M);
+
+    // Create occupied array. The array is filled with the sample occupying it or nan if unoccupied.
+    auto stratum_samples_x = std::vector<Vector3f>(M);
+    auto stratum_samples_y = std::vector<Vector3f>(M);
+    auto stratum_samples_z = std::vector<Vector3f>(M);
+
+    auto occupied_octants = std::vector<unsigned char>(M);
+
+    auto sample_to_octant_flag = [&](Vector3f sample, unsigned int grid_size) -> unsigned char {
+        Vector3i grid_coord = Vector3i(sample * float(grid_size));
+        Vector3i next_grid_coord = Vector3i(sample * (grid_size * 2.0f));
+        Vector3i occupied_octant = next_grid_coord - 2 * grid_coord;
+        int occupied_octant_index = occupied_octant.x + occupied_octant.y * 2 + occupied_octant.z * 4;
+        return 1 << occupied_octant_index;
+    };
+
+    auto generate_sample_point = [&](int old_sample_index, Vector3i grid_coord, int grid_size, int N) {
+        int NN = 8 * N;
+
+        Vector3f best_pt = { NAN, NAN, NAN };
+        float best_distance = 0;
+
+        for (unsigned int s = 0; s < blue_noise_samples; ++s) {
+
+            // Select octant.
+            int octant_index;
+            do {
+                octant_index = int(8 * rnd());
+            } while (occupied_octants[old_sample_index] & (1 << octant_index));
+            Vector3i octant_coord = { octant_index % 2, (octant_index >> 1) % 2, (octant_index >> 2) % 2 };
+
+            // Generate candidate sample coord in a free stratum.
+            Vector3f pt;
+            Vector3i stratum;
+            std::vector<Vector3f> stratum_samples[3] = { stratum_samples_x, stratum_samples_y, stratum_samples_z };
+            for (int d = 0; d < 3; ++d) {
+                do {
+                    pt[d] = (grid_coord[d] + 0.5f * (octant_coord[d] + rnd())) / grid_size;
+                    stratum[d] = int(NN * pt[d]);
+                } while (!isnan(stratum_samples[d][stratum[d]].x));
+            }
+
+            float distance_to_neighbour = magnitude(samples[old_sample_index] - pt);
+            int max_search_stratum = int(NN * distance_to_neighbour);
+
+            for (int offset = 1; offset <= max_search_stratum; ++offset) {
+                auto test_neighbour_sample = [&](Vector3f neighbour_sample) {
+                    // Samples should be distributed wrt a repeating sample pattern, so modify the sample such that it is closest in this pattern.
+                    if (neighbour_sample.x < pt.x - 0.5f)
+                        neighbour_sample.x += 1.0f;
+                    else if (neighbour_sample.x > pt.x + 0.5f)
+                        neighbour_sample.x -= 1.0f;
+                    if (neighbour_sample.y < pt.y - 0.5f)
+                        neighbour_sample.y += 1.0f;
+                    else if (neighbour_sample.y > pt.y + 0.5f)
+                        neighbour_sample.y -= 1.0f;
+                    if (neighbour_sample.z < pt.y - 0.5f)
+                        neighbour_sample.z += 1.0f;
+                    else if (neighbour_sample.z > pt.z + 0.5f)
+                        neighbour_sample.z -= 1.0f;
+
+                    distance_to_neighbour = fminf(distance_to_neighbour, magnitude(neighbour_sample - pt));
+                    max_search_stratum = int(NN * distance_to_neighbour);
+                };
+
+                test_neighbour_sample(stratum_samples_x[(stratum.x + offset) % stratum_samples_x.size()]);
+                test_neighbour_sample(stratum_samples_x[(stratum.x + stratum_samples_x.size() - offset) % stratum_samples_x.size()]);
+                test_neighbour_sample(stratum_samples_y[(stratum.y + offset) % stratum_samples_y.size()]);
+                test_neighbour_sample(stratum_samples_y[(stratum.y + stratum_samples_y.size() - offset) % stratum_samples_y.size()]);
+                test_neighbour_sample(stratum_samples_z[(stratum.z + offset) % stratum_samples_z.size()]);
+                test_neighbour_sample(stratum_samples_z[(stratum.z + stratum_samples_z.size() - offset) % stratum_samples_z.size()]);
+            }
+
+            if (best_distance < distance_to_neighbour) {
+                best_distance = distance_to_neighbour;
+                best_pt = pt;
+            }
+        }
+
+        // Mark 1D strata and octant as occupied
+        Vector3i stratum = Vector3i(best_pt * float(NN));
+        stratum_samples_x[stratum.x] = stratum_samples_y[stratum.y] = stratum_samples_z[stratum.z] = best_pt;
+        // occupied_octants[old_sample_index] |= 1 << octant_index;
+        occupied_octants[old_sample_index] |= sample_to_octant_flag(best_pt, grid_size);
+
+        // Assign new sample point
+        samples.push_back(best_pt);
+    };
+
+    // Mark all occupied 1D strata.
+    auto mark_occupied_strata = [&](unsigned int N) {
+        unsigned int NN = 8 * N;
+        stratum_samples_x.resize(NN);
+        stratum_samples_y.resize(NN);
+        stratum_samples_z.resize(NN);
+        for (unsigned int i = 0; i < NN; ++i)
+            stratum_samples_x[i] = stratum_samples_y[i] = stratum_samples_z[i] = { NAN, NAN, NAN }; // init array
+
+        for (unsigned int s = 0; s < N; ++s) {
+            Vector3i stratum = Vector3i(samples[s] * float(NN));
+            stratum_samples_x[stratum.x] = stratum_samples_y[stratum.y] = stratum_samples_z[stratum.z] = samples[s];
+        }
+    };
+
+    auto mark_occupied_octants = [&](unsigned int grid_size, unsigned int N) {
+        occupied_octants.resize(N);
+        for (unsigned int s = 0; s < N; ++s)
+            occupied_octants[s] = sample_to_octant_flag(samples[s], grid_size);
+    };
+
+    // Generate next N sample points
+    auto extend_sequence = [&](unsigned int N) {
+        unsigned int grid_size = (unsigned int)cbrt(N);
+
+        // Mark already occupied 1D strata so we can avoid them
+        mark_occupied_strata(N);
+
+        // Mark which octants of a cell is filled.
+        mark_occupied_octants(grid_size, N);
+        
+        // Loop over 7N empty samples and generate new samples.
+        for (unsigned int s = 0; s < 7 * N; ++s) {
+            Vector3f oldpt = samples[s % N];
+            Vector3i grid_coord = Vector3i(oldpt * float(grid_size));
+
+            // Generate a sample point
+            generate_sample_point(s % N, grid_coord, grid_size, N);
+        }
+    };
+
+    samples.push_back({ rnd(), rnd(), rnd() });
+    unsigned int N = 1;
+    while (N < M) {
+        extend_sequence(N);
+        N *= 8;
+    }
+
+    return samples;
+}
+
+
 #endif // _PMJ_PMJBN_H_
--- apps/dev/ProgressiveMultijitteredSamples/main.cpp
+++ apps/dev/ProgressiveMultijitteredSamples/main.cpp
@@ -55,8 +55,20 @@ std::vector<Vector2f> generate_linear_congruential_random_samples(unsigned int s
     return samples;
 }
 
+std::vector<Vector3f> generate_3D_linear_congruential_random_samples(unsigned int subdivisions) {
+    unsigned int m = 1 << subdivisions;
+    unsigned int M = m * m * m;
+    auto samples = std::vector<Vector3f>(M);
+
+    auto rng = RNG::LinearCongruential(19349669);
+    for (unsigned int s = 0; s < M; ++s)
+        samples[s] = { rng.sample1f(), rng.sample1f(), rng.sample1f() };
+
+    return samples;
+}
+
 // ------------------------------------------------------------------------------------------------
-// Output image
+// Output images of random samples.
 // ------------------------------------------------------------------------------------------------
 
 void output_image(const std::vector<Vector2f>& samples, const char* const path) {
@@ -77,6 +89,63 @@ void output_image(const std::vector<Vector2f>& samples, const char* const path) 
         printf("Failed to output sample image to '%s'.\n", path);
 }
 
+// Flatten the samples along each dimension and output the 2D image.
+void output_images(const std::vector<Vector3f>& samples, const char* const path) {
+
+    auto flat_samples = std::vector<Vector2f>(samples.size());
+
+    // remove the last 4 chars of the path, e.g. the extension and the .
+    std::string path_sans_extension = std::string(path, path + strlen(path) - 4);
+
+    for (int i = 0; i < samples.size(); ++i)
+        flat_samples[i] = { samples[i].x, samples[i].y };
+    output_image(flat_samples, (path_sans_extension + "_z.png").c_str());
+
+    for (int i = 0; i < samples.size(); ++i)
+        flat_samples[i] = { samples[i].x, samples[i].z };
+    output_image(flat_samples, (path_sans_extension + "_y.png").c_str());
+
+    for (int i = 0; i < samples.size(); ++i)
+        flat_samples[i] = { samples[i].y, samples[i].z };
+    output_image(flat_samples, (path_sans_extension + "_x.png").c_str());
+}
+
+// ------------------------------------------------------------------------------------------------
+// Options
+// ------------------------------------------------------------------------------------------------
+
+struct Options {
+    Generator generator = Generator::PMJBN;
+    int subdivisions = 3;
+    int dimensions = 2;
+    int blue_noise_candidates = 8;
+
+    static Options parse(int argc, char** argv) {
+        Options res;
+        for (int argument = 1; argument < argc; ++argument) {
+            if (strcmp(argv[argument], "--lcg") == 0)
+                res.generator = Generator::LCG;
+            else if (strcmp(argv[argument], "--pj") == 0)
+                res.generator = Generator::PJ;
+            else if (strcmp(argv[argument], "--pmj") == 0)
+                res.generator = Generator::PMJ;
+            else if (strcmp(argv[argument], "--pmjbn") == 0)
+                res.generator = Generator::PMJBN;
+            else if (strcmp(argv[argument], "-s") == 0 || strcmp(argv[argument], "--subdivisions") == 0)
+                res.subdivisions = atoi(argv[++argument]);
+            else if (strcmp(argv[argument], "-d") == 0 || strcmp(argv[argument], "--dimensions") == 0)
+                res.dimensions = atoi(argv[++argument]);
+            else
+                printf("Unsupported argument: '%s'\n", argv[argument]);
+        }
+        return res;
+    }
+};
+
+// ------------------------------------------------------------------------------------------------
+// Main
+// ------------------------------------------------------------------------------------------------
+
 void print_usage() {
     char* usage =
         "usage:\n"
@@ -85,7 +154,8 @@ void print_usage() {
         "     | --pj: Generate samples using a progressive jittered generator.\n"
         "     | --pmj: Generate samples using a progressive multi-jittered generator.\n"
         "     | --pmjbn: Generate samples using a progressive multi-jittered pseudo blue noise generator.\n"
-        "  -s | --subdivisions: Number of image subdivisions. Total sample count is subdivisions^4\n";
+        "  -s | --subdivisions: Number of image subdivisions. Total sample count is subdivisions^4.\n"
+        "  -d | --dimensions: Number of dimensions in the samples. 2 or 3 supported.\n";
     printf("%s", usage);
 }
 
@@ -98,70 +168,91 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    // Parse options
-    Generator generator = Generator::PMJBN;
-    int subdivisions = 3;
+    auto options = Options::parse(argc, argv);
 
-    for (int argument = 1; argument < argc; ++argument) {
-        if (strcmp(argv[argument], "--lcg") == 0)
-            generator = Generator::LCG;
-        else if (strcmp(argv[argument], "--pj") == 0)
-            generator = Generator::PJ;
-        else if (strcmp(argv[argument], "--pmj") == 0)
-            generator = Generator::PMJ;
-        else if (strcmp(argv[argument], "--pmjbn") == 0)
-            generator = Generator::PMJBN;
-        else if (strcmp(argv[argument], "-s") == 0 || strcmp(argv[argument], "--subdivisions") == 0)
-            subdivisions = atoi(argv[++argument]);
-    }
-
-    printf("Generate %s samples.\n", generator_name(generator));
-
-    // Generate samples.
-    auto generata_samples = [](Generator generator, int subdivisions) -> std::vector<Vector2f> {
-        switch (generator) {
-        case Generator::LCG: return generate_linear_congruential_random_samples(subdivisions);
-        case Generator::PJ: return generate_progressive_jittered_samples(subdivisions);
-        case Generator::PMJ: return generate_progressive_multijittered_samples(subdivisions);
-        case Generator::PMJBN: return generate_progressive_multijittered_bluenoise_samples(subdivisions);
-        }
-        return std::vector<Vector2f>();
-    };
-
-    auto starttime = std::chrono::system_clock::now();
-    auto samples = generata_samples(generator, subdivisions);
-    auto endtime = std::chrono::system_clock::now();
-    float delta_miliseconds = (float)std::chrono::duration_cast<std::chrono::milliseconds>(endtime - starttime).count();
-    printf("Time to generate %d samples: %.3fseconds\n", int(samples.size()), delta_miliseconds / 1000);
+    printf("Generate %uD %s samples.\n", options.dimensions, generator_name(options.generator));
 
     std::ostringstream path;
-    path << "C:\\temp\\" << subdivisions << "_" << generator_shortname(generator) << "_samples.png";
-    printf("Output image to %s\n", path.str().c_str());
-    output_image(samples, path.str().c_str());
+    path << "C:\\temp\\" << options.subdivisions << "_" << generator_shortname(options.generator) << "_samples.png";
 
-    // Property tests.
-    // TODO Test if progressive??
+    if (options.dimensions == 2) {
+        // Generate samples.
+        auto generata_samples = [](Generator generator, int subdivisions) -> std::vector<Vector2f> {
+            switch (generator) {
+            case Generator::LCG: return generate_linear_congruential_random_samples(subdivisions);
+            case Generator::PJ: return generate_progressive_jittered_samples(subdivisions);
+            case Generator::PMJ: return generate_progressive_multijittered_samples(subdivisions);
+            case Generator::PMJBN: return generate_progressive_multijittered_bluenoise_samples(subdivisions);
+            }
+            return std::vector<Vector2f>();
+        };
 
-    if (Test::is_multijittered(samples.data(), (unsigned int)samples.size()))
-        printf("... is multijittered\n");
-    else
-        printf("... is not multijittered\n");
+        auto starttime = std::chrono::system_clock::now();
+        auto samples = generata_samples(options.generator, options.subdivisions);
+        auto endtime = std::chrono::system_clock::now();
+        float delta_miliseconds = (float)std::chrono::duration_cast<std::chrono::milliseconds>(endtime - starttime).count();
+        printf("Time to generate %d samples: %.3fseconds\n", int(samples.size()), delta_miliseconds / 1000);
 
-    float blue_noise_score = Test::compute_blue_noise_score(samples.data(), (unsigned int)samples.size());
-    printf("... has blue noise score %f\n", blue_noise_score);
+        printf("Output image to %s\n", path.str().c_str());
+        output_image(samples, path.str().c_str());
 
-    // Convergence tests.
-    auto test = [&](const char* name, std::function<float(Vector2f*, int)> test) {
-        printf("%s convergence:\n", name);
-        printf("  1 sample: error %.4f\n", test(samples.data(), 1));
-        for (int s = 2; s <= samples.size(); s *= 2)
-            printf("  %u samples: error %.4f\n", s, test(samples.data(), s));
-        printf("\n");
-    };
+        { // Property tests.
+        // TODO Test if progressive??
 
-    test("Disc", Test::disc_convergence);
-    test("Triangle", Test::triangle_convergence);
-    test("Step", Test::step_convergence);
-    test("Gaussian", Test::gaussian_convergence);
-    test("Bilinear", Test::bilinear_convergence);
+            if (Test::is_multijittered(samples.data(), (unsigned int)samples.size()))
+                printf("... is multijittered\n");
+            else
+                printf("... is not multijittered\n");
+
+            float blue_noise_score = Test::compute_blue_noise_score(samples.data(), (unsigned int)samples.size());
+            printf("... has blue noise score %f\n", blue_noise_score);
+        }
+
+        { // Convergence tests.
+            auto test = [&](const char* name, std::function<float(Vector2f*, int)> test) {
+                printf("%s convergence:\n", name);
+                printf("  1 sample: error %.4f\n", test(samples.data(), 1));
+                for (int s = 2; s <= samples.size(); s *= 2)
+                    printf("  %u samples: error %.4f\n", s, test(samples.data(), s));
+                printf("\n");
+            };
+
+            test("Disc", Test::disc_convergence);
+            test("Triangle", Test::triangle_convergence);
+            test("Step", Test::step_convergence);
+            test("Gaussian", Test::gaussian_convergence);
+            test("Bilinear", Test::bilinear_convergence);
+        }
+    } else if (options.dimensions == 3) {
+        // Generate samples.
+        auto generata_samples = [](Generator generator, int subdivisions) -> std::vector<Vector3f> {
+            switch (generator) {
+            case Generator::LCG: return generate_3D_linear_congruential_random_samples(subdivisions);
+            case Generator::PJ:
+                printf("3D progressive jittered samples not supported. Generate PMJ samples instead.\n");
+            case Generator::PMJ: return generate_3D_progressive_multijittered_samples(subdivisions, 1);
+            case Generator::PMJBN: return generate_3D_progressive_multijittered_samples(subdivisions, 8);
+            default:
+                return std::vector<Vector3f>();
+            }
+        };
+
+        auto starttime = std::chrono::system_clock::now();
+        auto samples = generata_samples(options.generator, options.subdivisions);
+        auto endtime = std::chrono::system_clock::now();
+        float delta_miliseconds = (float)std::chrono::duration_cast<std::chrono::milliseconds>(endtime - starttime).count();
+        printf("Time to generate %d samples: %.3fseconds\n", int(samples.size()), delta_miliseconds / 1000);
+
+        // Flatten along each dimension and save the resulting image.
+        printf("Output image to %s\n", path.str().c_str());
+        output_images(samples, path.str().c_str());
+
+        { // Property tests.
+            float blue_noise_score = Test::compute_blue_noise_score(samples.data(), (unsigned int)samples.size());
+            printf("... has blue noise score %f\n", blue_noise_score);
+        }
+
+        { // Convergence tests.
+        }
+    }
 }
