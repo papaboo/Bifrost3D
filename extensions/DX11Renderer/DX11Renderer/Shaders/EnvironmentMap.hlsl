08a28ef43350876d05051c56ad0cc0a0aab53bd7
Asger Vejen Hoedt <asger.hoedt@3shape.com>
2019-11-08 17:05:12 +0100

    DX11Renderer: Move environment map ray projection from pixel shader to vertex shader to reduce the number of projections computed.

--- extensions/DX11Renderer/DX11Renderer/Shaders/EnvironmentMap.hlsl
+++ extensions/DX11Renderer/DX11Renderer/Shaders/EnvironmentMap.hlsl
@@ -8,13 +8,6 @@
 
 #include "Utils.hlsl"
 
-float3 project_ray_direction(float2 viewport_pos, float4x4 inverted_view_projection_matrix) {
-    float4 projected_pos = float4(viewport_pos.x, viewport_pos.y, -1.0f, 1.0f);
-    float4 projected_world_pos = mul(projected_pos, inverted_view_projection_matrix);
-    float3 ray_direction = projected_world_pos.xyz / projected_world_pos.w;
-    return normalize(ray_direction);
-}
-
 // ------------------------------------------------------------------------------------------------
 // Scene constants.
 // ------------------------------------------------------------------------------------------------
@@ -29,7 +22,7 @@ cbuffer scene_variables : register(b13) {
 
 struct Varyings {
     float4 position : SV_POSITION;
-    float2 texcoord : TEXCOORD;
+    float3 view_direction : TEXCOORD;
 };
 
 Varyings main_vs(uint vertex_ID : SV_VertexID) {
@@ -38,7 +31,11 @@ Varyings main_vs(uint vertex_ID : SV_VertexID) {
     output.position.x = vertex_ID == 2 ? 3 : -1;
     output.position.y = vertex_ID == 0 ? -3 : 1;
     output.position.zw = float2(1.0f, 1.0f);
-    output.texcoord = output.position.xy;
+
+    float2 viewport_pos = output.position.xy;
+    float4 projected_pos = float4(viewport_pos.x, viewport_pos.y, 1.0f, 1.0f);
+    output.view_direction = mul(projected_pos, scene_vars.inverted_projection_matrix).xyz;
+    output.view_direction = mul(scene_vars.world_to_view_matrix, output.view_direction).xyz;
     return output;
 }
 
@@ -54,10 +51,6 @@ Texture2D env_tex : register(t0);
 SamplerState env_sampler : register(s0);
 
 float4 main_ps(Varyings input) : SV_TARGET {
-    float2 viewport_pos = input.texcoord;
-    float3 view_dir = project_ray_direction(viewport_pos, scene_vars.inverted_projection_matrix);
-    view_dir = mul(scene_vars.world_to_view_matrix, view_dir).xyz;
-
-    float2 tc = direction_to_latlong_texcoord(view_dir);
+    float2 tc = direction_to_latlong_texcoord(normalize(input.view_direction));
     return float4(scene_vars.environment_tint.rgb * env_tex.SampleLevel(env_sampler, tc, 0).rgb, 1);
 }
