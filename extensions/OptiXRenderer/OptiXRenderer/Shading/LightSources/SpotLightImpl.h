d6f1b9320303261af887b87666ec8dbe8e9bab30
Asger Vejen Hoedt <asger.hoedt@3shape.com>
2020-06-13 14:06:32 +0200

    OptiXRenderer: Fix area spotlight
    * Correct conversion from area PDF to solid angle PDF.
    * Fix evaluate such that spot lights with tiny radii are indistinguishable from delta lights.

--- extensions/OptiXRenderer/OptiXRenderer/Shading/LightSources/SpotLightImpl.h
+++ extensions/OptiXRenderer/OptiXRenderer/Shading/LightSources/SpotLightImpl.h
@@ -1,10 +1,10 @@
 // OptiX renderer functions for spot lights.
-// ---------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 // Copyright (C) Bifrost. See AUTHORS.txt for authors.
 //
 // This program is open source and distributed under the New BSD License.
 // See LICENSE.txt for more detail.
-// ---------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 
 #ifndef _OPTIXRENDERER_SPOT_LIGHT_IMPLEMENTATION_H_
 #define _OPTIXRENDERER_SPOT_LIGHT_IMPLEMENTATION_H_
@@ -17,6 +17,15 @@
 namespace OptiXRenderer {
 namespace LightSources {
 
+// ------------------------------------------------------------------------------------------------
+// Disk spot light.
+// Future work:
+// * Sample the disk or a cone from the lit position projected onto the spot lights plane,
+//   whichever has the smallest radius.
+// * Improved sampling of the disk wrt solid angle.
+//   https://www.arnoldrenderer.com/research/egsr2017_spherical_ellipse.pdf.
+// ------------------------------------------------------------------------------------------------
+
 __inline_all__ float surface_area(const SpotLight& light) {
     return PIf * pow2(light.radius);
 }
@@ -28,12 +37,13 @@ __inline_all__ float is_delta_light(const SpotLight& light) {
 __inline_all__ float PDF(const SpotLight& light, optix::float3 lit_position, optix::float3 direction_to_light) {
     using namespace optix;
 
-    if (!is_delta_light(light)) {
+    float cos_theta = fmaxf(0, -dot(light.direction, normalize(direction_to_light)));
+
+    if (cos_theta > 0.0f && !is_delta_light(light)) {
         direction_to_light = normalize(direction_to_light);
         float t = Intersect::ray_disk(lit_position, direction_to_light, light.position, light.direction, light.radius);
         if (t >= 0.0f) {
-            float cos_theta = fmaxf(0, -dot(light.direction, direction_to_light));
-            float area_PDF_to_solid_angle_PDF = cos_theta / (t * t);
+            float area_PDF_to_solid_angle_PDF = (t * t) / cos_theta;
             return Distributions::Disk::PDF(light.radius) * area_PDF_to_solid_angle_PDF;
         }
     }
@@ -44,14 +54,16 @@ __inline_all__ float PDF(const SpotLight& light, optix::float3 lit_position, opt
 __inline_all__ optix::float3 evaluate(const SpotLight& light, optix::float3 lit_position, optix::float3 direction_to_light) {
     using namespace optix;
 
-    float normalization = TWO_PIf * (1 - light.cos_angle);
-    float3 radiance = light.power / (normalization * length_squared(direction_to_light));
-    if (!is_delta_light(light))
-        radiance /= 3.0f * surface_area(light);
-
     float cos_theta = fmaxf(0, -dot(light.direction, normalize(direction_to_light)));
-    radiance *= (cos_theta > light.cos_angle) ? 1.0f : 0.0f;
-    return radiance;
+
+    float normalization = TWO_PIf * (1 - light.cos_angle);
+    if (is_delta_light(light))
+        normalization *= length_squared(direction_to_light);
+    else
+        normalization *= surface_area(light) * cos_theta;
+    float3 radiance = light.power / normalization;
+
+    return (cos_theta > light.cos_angle) ? radiance : make_float3(0.0f, 0.0f, 0.0f);
 }
 
 __inline_all__ LightSample sample_radiance(const SpotLight& light, optix::float3 lit_position, optix::float2 random_sample) {
