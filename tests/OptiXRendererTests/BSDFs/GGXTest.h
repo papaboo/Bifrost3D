cb079c951731515c82a3d186d220b6d6e0e38ef6
Asger Hoedt <asger.hoedt@3shape.com>
2022-06-28 14:05:49 +0200

    OptixRendererTests: Remove GGX_R::sampling_variance test as GGX vanilla sampling isn't supported.

--- tests/OptiXRendererTests/BSDFs/GGXTest.h
+++ tests/OptiXRendererTests/BSDFs/GGXTest.h
@@ -132,59 +132,6 @@ GTEST_TEST(GGX_R, minimal_alpha) {
     EXPECT_FALSE(isnan(f));
 }
 
-// Test that sampling from the distribution of visible normals has lower variance than the original GGX sampling and they converge to the same result.
-GTEST_TEST(GGX_R, sampling_variance) {
-    using namespace optix;
-
-    static auto sample_GGX_vanilla = [](float alpha, float3 specularity, float3 wo, float2 random_sample) -> BSDFSample {
-        BSDFSample bsdf_sample;
-
-        Distributions::DirectionalSample ggx_sample = Distributions::GGX::sample(alpha, random_sample);
-        bsdf_sample.direction = reflect(-wo, ggx_sample.direction);
-
-        bsdf_sample.PDF = ggx_sample.PDF / (4.0f * dot(wo, ggx_sample.direction));
-        bsdf_sample.reflectance = Shading::BSDFs::GGX_R::evaluate(alpha, specularity, wo, bsdf_sample.direction, ggx_sample.direction);
-
-        bool discardSample = bsdf_sample.PDF < 0.00001f || bsdf_sample.direction.z < 0.00001f; // Discard samples if the pdf is too low (precision issues) or if the new direction points into the surface (energy loss).
-        return discardSample ? BSDFSample::none() : bsdf_sample;
-    };
-
-    typedef BSDFSample(*SampleFunction)(float alpha, float3 specularity, float3 wo, float2 random_sample);
-    auto sampling_mean_and_variance = [&](SampleFunction function, float alpha, float3 wo, double& mean, double& variance) {
-        const float3 specularity = { 1, 1, 1 };
-
-        const unsigned int MAX_SAMPLES = 4098;
-        auto ws = std::vector<double>(MAX_SAMPLES);
-        auto ws_squared = std::vector<double>(MAX_SAMPLES);
-
-        for (unsigned int i = 0u; i < MAX_SAMPLES; ++i) {
-            BSDFSample sample = function(alpha, specularity, wo, RNG::sample02(i));
-            if (is_PDF_valid(sample.PDF)) {
-                ws[i] = sample.reflectance.x * abs(sample.direction.z) / sample.PDF; // f * ||cos_theta|| / pdf
-                ws_squared[i] = ws[i] * ws[i];
-            }
-            else
-                ws_squared[i] = ws[i] = 0.0f;
-        }
-
-        mean = Bifrost::Math::sort_and_pairwise_summation(ws.begin(), ws.end()) / MAX_SAMPLES;
-        double mean_squared = Bifrost::Math::sort_and_pairwise_summation(ws_squared.begin(), ws_squared.end()) / MAX_SAMPLES;
-        variance = mean_squared - mean * mean;
-    };
-
-    const float3 wo = normalize(make_float3(1.0f, 0.0f, 1.0f));
-    for (float alpha : { 0.0675f, 0.125f, 0.25f, 0.5f, 1.0f }) {
-        double vanilla_GGX_mean, vanilla_GGX_variance;
-        sampling_mean_and_variance(sample_GGX_vanilla, alpha, wo, vanilla_GGX_mean, vanilla_GGX_variance);
-
-        double GGX_VNDF_mean, GGX_VNDF_variance;
-        sampling_mean_and_variance(Shading::BSDFs::GGX_R::sample, alpha, wo, GGX_VNDF_mean, GGX_VNDF_variance);
-
-        EXPECT_TRUE(almost_equal_eps(float(vanilla_GGX_mean), float(GGX_VNDF_mean), 0.001f));
-        EXPECT_LT(GGX_VNDF_variance, vanilla_GGX_variance);
-    }
-}
-
 GTEST_TEST(GGX_R, estimate_alpha_from_max_PDF) {
     using namespace Bifrost;
     using namespace Bifrost::Assets::Shading::EstimateGGXAlpha;
