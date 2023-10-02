2d251110320aacdab8f992a6b4334e79270bdc22
Asger Hoedt <asger.hoedt@3shape.com>
2023-10-02 07:11:44 +0200

    StbImageWriter: Helper for easily dumping images with common memory lauouts

--- extensions/StbImageWriter/StbImageWriter/StbImageWriter.cpp
+++ extensions/StbImageWriter/StbImageWriter/StbImageWriter.cpp
@@ -22,10 +22,6 @@ enum class FileType {
     BMP, HDR, JPG, PNG, TGA, Unknown
 };
 
-enum class ChannelType {
-    Char, Float, Unknown
-};
-
 FileType get_file_type(const std::string& path) {
     if (path[path.size() - 4] != '.')
         return FileType::Unknown;
@@ -45,20 +41,40 @@ FileType get_file_type(const std::string& path) {
         return FileType::Unknown;
 }
 
-bool save_image(const std::string& path, FileType file_type, unsigned int width, unsigned int height, int channel_count, void* data) {
+bool save_image(const std::string& path, FileType file_type, unsigned int width, unsigned int height, int channel_count, void* pixels) {
     switch (file_type) {
     case FileType::BMP:
-        return stbi_write_bmp(path.c_str(), width, height, channel_count, data) != 0;
+        return stbi_write_bmp(path.c_str(), width, height, channel_count, pixels) != 0;
     case FileType::HDR:
-        return stbi_write_hdr(path.c_str(), width, height, channel_count, static_cast<float*>(data)) != 0;
+        return stbi_write_hdr(path.c_str(), width, height, channel_count, static_cast<float*>(pixels)) != 0;
     case FileType::JPG:
-        return stbi_write_jpg(path.c_str(), width, height, channel_count, data, 90) != 0;
+        return stbi_write_jpg(path.c_str(), width, height, channel_count, pixels, 90) != 0;
     case FileType::PNG:
-        return stbi_write_png(path.c_str(), width, height, channel_count, data, 0) != 0;
+        return stbi_write_png(path.c_str(), width, height, channel_count, pixels, 0) != 0;
     case FileType::TGA:
-        return stbi_write_tga(path.c_str(), width, height, channel_count, data) != 0;
+        return stbi_write_tga(path.c_str(), width, height, channel_count, pixels) != 0;
+    default:
+        printf("StbImageWriter found unsupported file type. Path: '%s'\n", path.c_str());
+        return false;
     }
     return false;
+}
+
+// Flip texture vertically, as stb_image_writer uses the upper left corner as origo,
+template <typename T>
+std::vector<T> flip_horizontally(T* pixels, unsigned int width, unsigned int height, int channel_count) {
+    auto flipped_pixels = std::vector<T>(width * height * channel_count);
+    T* flipped_pixels_ptr = flipped_pixels.data();
+
+    int row_element_count = width * channel_count;
+    int row_size = row_element_count * sizeof(T);
+    for (unsigned int row = 0; row < height; row++) {
+        T* src_row = pixels + row * row_element_count;
+        T* dst_row = flipped_pixels_ptr + (height - row - 1) * row_element_count;
+        memcpy(dst_row, src_row, row_size);
+    }
+
+    return flipped_pixels;
 }
 
 bool write(Image image, const std::string& path) {
@@ -67,10 +83,7 @@ bool write(Image image, const std::string& path) {
         return false;
 
     FileType file_type = get_file_type(path);
-    if (file_type == FileType::Unknown) {
-        printf("StbImageWriter found unsupported file type. Path: '%s'\n", path.c_str());
-        return false;
-    }
+
     unsigned int width = image.get_width(), height = image.get_height();
     int channel_count = Bifrost::Assets::channel_count(image.get_pixel_format());
 
@@ -79,24 +92,24 @@ bool write(Image image, const std::string& path) {
     // NOTE This could be optimized if the image already has the correct format and gamma.
     bool did_succeed = false;
     if (file_type == FileType::HDR) {
-        float* data = new float[width * height * channel_count];
+        float* pixels = new float[width * height * channel_count];
         for (unsigned int y = 0; y < height; ++y)
             for (unsigned int x = 0; x < width; ++x) {
                 int data_index = x + (height - 1 - y) * width;
-                float* pixel_data = data + data_index * channel_count;
+                float* pixel_data = pixels + data_index * channel_count;
                 RGBA pixel = image.get_pixel(Vector2ui(x, y));
                 memcpy(pixel_data, pixel.begin(), sizeof(float) * channel_count);
             }
 
-        did_succeed = save_image(path, file_type, width, height, channel_count, data);
-        delete[] data;
+        did_succeed = save_image(path, file_type, width, height, channel_count, pixels);
+        delete[] pixels;
     } else {
-        unsigned char* data = new unsigned char[width * height * channel_count];
+        unsigned char* pixels = new unsigned char[width * height * channel_count];
         float gamma = 1.0f / 2.2f;
         for (unsigned int y = 0; y < height; ++y)
             for (unsigned int x = 0; x < width; ++x) {
                 int data_index = x + (height - 1 - y) * width;
-                unsigned char* pixel_data = data + data_index * channel_count;
+                unsigned char* pixel_data = pixels + data_index * channel_count;
                 RGBA pixel = image.get_pixel(Vector2ui(x, y));
                 for (int c = 0; c < channel_count; ++c) {
                     float channel_intensity = c != 3 ? pow(pixel[c], gamma) : pixel[c]; // Gamma correct colors but leave alpha as linear.
@@ -104,11 +117,25 @@ bool write(Image image, const std::string& path) {
                 }
             }
 
-        did_succeed = save_image(path, file_type, width, height, channel_count, data);
-        delete[] data;
+        did_succeed = save_image(path, file_type, width, height, channel_count, pixels);
+        delete[] pixels;
     }
 
     return did_succeed;
+}
+
+bool write(unsigned char* pixels, unsigned int width, unsigned int height, unsigned int channel_count, const std::string& path) {
+    auto flipped_pixels = flip_horizontally(pixels, width, height, channel_count);
+
+    FileType file_type = get_file_type(path);
+    return save_image(path, file_type, width, height, channel_count, flipped_pixels.data());
+}
+
+bool write(float* pixels, unsigned int width, unsigned int height, unsigned int channel_count, const std::string& path) {
+    auto flipped_pixels = flip_horizontally(pixels, width, height, channel_count);
+
+    FileType file_type = get_file_type(path);
+    return save_image(path, file_type, width, height, channel_count, flipped_pixels.data());
 }
 
 } // NS StbImageWriter
