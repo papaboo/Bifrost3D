faa3a0697238cca24fc7d78acc4a8844588b5b70
Asger Hoedt <asger.hoedt@3shape.com>
2023-12-31 12:19:16 +0100

    SimpleViewer: Handle case where no OptiX rendering backend is selected yet

--- apps/SimpleViewer/GUI/RenderingGUI.cpp
+++ apps/SimpleViewer/GUI/RenderingGUI.cpp
@@ -404,10 +404,10 @@ void RenderingGUI::layout_frame() {
         ImGui::PoppedTreeNode("OptiX", [&]() {
             auto camera_ID = *Cameras::get_iterable().begin();
 
-            const char* backend_modes[] = { "Path tracer", "AI denoised GI", "Depth", "Albedo", "Tint", "Roughness", "Shading normal" };
-            int current_backend = int(m_optix_renderer->get_backend(camera_ID)) - 1;
-            if (ImGui::Combo("Backend", &current_backend, backend_modes, IM_ARRAYSIZE(backend_modes))) {
-                auto backend = OptiXRenderer::Backend(current_backend + 1);
+            static const char* backend_modes[] = { "Uninitialized", "Path tracer", "AI denoised GI", "Depth", "Albedo", "Tint", "Roughness", "Shading normal" };
+            int current_backend = int(m_optix_renderer->get_backend(camera_ID));
+            if (ImGui::Combo("Backend", &current_backend, backend_modes, IM_ARRAYSIZE(backend_modes)) && current_backend != 0) {
+                auto backend = OptiXRenderer::Backend(current_backend);
                 m_optix_renderer->set_backend(camera_ID, backend);
             }
 
