2bf74aaf195b7cae5b6e4ea06f9c7b37386e748e
Asger Hoedt <asger.hoedt@3shape.com>
2024-02-23 22:08:00 +0100

    OptiXRenderer: Move GGX minimal roughness estimation to utils to be shared across shading models

--- tests/OptiXRendererTests/ShadingModels/UtilsTest.h
+++ tests/OptiXRendererTests/ShadingModels/UtilsTest.h
@@ -29,6 +29,24 @@ GTEST_TEST(ShadingModelUtils, GGX_rho_texture_size) {
     EXPECT_EQ(actual_roughness_sample_count, expected_roughness_sample_count);
 }
 
+GTEST_TEST(ShadingModelUtils, GGX_minimum_roughness_texture_size) {
+    int expected_angle_sample_count = Bifrost::Assets::Shading::Estimate_GGX_bounded_VNDF_alpha::wo_dot_normal_sample_count;
+    int actual_angle_sample_count = Shading::ShadingModels::GGXMinimumRoughness::angle_sample_count;
+    EXPECT_EQ(actual_angle_sample_count, expected_angle_sample_count);
+
+    int expected_max_PDF_sample_count = Bifrost::Assets::Shading::Estimate_GGX_bounded_VNDF_alpha::max_PDF_sample_count;
+    int actual_max_PDF_sample_count = Shading::ShadingModels::GGXMinimumRoughness::max_PDF_sample_count;
+    EXPECT_EQ(actual_max_PDF_sample_count, expected_max_PDF_sample_count);
+}
+
+GTEST_TEST(ShadingModelUtils, GGX_minimum_roughness_PDF_encoding_consistent) {
+    for (float pdf : {0.1f, 1.0f, 10.0f, 1000.0f, 100000.0f}) {
+        float expected_encoded_PDF = Bifrost::Assets::Shading::Estimate_GGX_bounded_VNDF_alpha::encode_PDF(pdf);
+        float actual_encoded_PDF = Shading::ShadingModels::GGXMinimumRoughness::encode_PDF(pdf);
+        EXPECT_FLOAT_EQ(actual_encoded_PDF, expected_encoded_PDF);
+    }
+}
+
 } // NS OptiXRenderer
 
 #endif // _OPTIXRENDERER_SHADING_MODELS_UTILS_TEST_H_
