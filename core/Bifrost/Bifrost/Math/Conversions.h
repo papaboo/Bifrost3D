papaboo/Bifrost3D

commit b15ce7e3775da183d6935b961e558007e109a798

Bifrost: Remove special case when converting mat3x3 to a quaternion. Marek says it'll never happen. :)

core/Bifrost/Bifrost/Math/Conversions.h
@@ -58,7 +58,6 @@ inline Quaternion<T> to_quaternion(const Matrix3x3<T> m) {
         T s = std::sqrt((m[i][i] - (m[j][j] + m[k][k])) + 1.0f);
         Vector3<T> imaginary;
         imaginary[i] = s * 0.5f;
-        if (s != 0.f) s = 0.5f / s;
         T real = (m[k][j] - m[j][k]) * s;
         imaginary[j] = (m[j][i] + m[i][j]) * s;
         imaginary[k] = (m[k][i] + m[i][k]) * s;

commit 9075c10903bd4cca40ea4bd0c8a86f132f2dc4ea

Reintroduce missing expression in matrix3x3 to quaternion conversion
The bug was introduced in b15ce7e3775da183d6935b961e558007e109a798, because a redundant check was removed as well as the expression it was guarding.

core/Bifrost/Bifrost/Math/Conversions.h
@@ -58,6 +58,7 @@ inline Quaternion<T> to_quaternion(const Matrix3x3<T> m) {
         T s = std::sqrt((m[i][i] - (m[j][j] + m[k][k])) + 1.0f);
         Vector3<T> imaginary;
         imaginary[i] = s * 0.5f;
+        s = 0.5f / s;
         T real = (m[k][j] - m[j][k]) * s;
         imaginary[j] = (m[j][i] + m[i][j]) * s;
         imaginary[k] = (m[k][i] + m[i][k]) * s;