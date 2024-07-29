30d80fa24865855e6403e7501a492ef7afcc9d9c
Asger Hoedt <asger.hoedt@3shape.com>
2024-07-30 00:02:27 +0200

    Bifrost: Generalize tonemapping settings
    It's still only used for unreal/filmic tonemapper, but it could be used for linear as well.

--- extensions/DX11Renderer/DX11Renderer/Shaders/CameraEffects/Utils.hlsl
+++ extensions/DX11Renderer/DX11Renderer/Shaders/CameraEffects/Utils.hlsl
@@ -33,15 +33,13 @@ cbuffer constants : register(b0) {
     float vignette_strength;
     float film_grain_strength;
 
-    // float8 tonemapping params
-    float4 tonemapping_params_1;
-    float4 tonemapping_params_2;
+    float tonemapping_black_clip;
+    float tonemapping_toe;
+    float tonemapping_slope;
+    float tonemapping_shoulder;
+    float tonemapping_white_clip;
 
-    float filmic_black_clip() { return tonemapping_params_1.x; }
-    float filmic_toe() { return tonemapping_params_1.y; }
-    float filmic_slope() { return tonemapping_params_1.z; }
-    float filmic_shoulder() { return tonemapping_params_1.w; }
-    float filmic_white_clip() { return tonemapping_params_2.x; }
+    float3 _padding;
 }
 
 float eye_adaptation(float current_exposure, float target_exposure) {
