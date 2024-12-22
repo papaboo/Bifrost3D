b8207f6373c85a9b5cb6258509f1e1e6acda7e9d
Asger Hoedt <asger.hoedt@3shape.com>
2024-12-22 10:07:08 +0100

    OptiXRendererTests: Ensure that Bifrost and OptiXRenderer flags are aligned

---
+++ tests/OptiXRendererTests/Assets/FlagsTest.h
@@ -0,0 +1,33 @@
+// Test asset flags in the OptiXRenderer.
+// ---------------------------------------------------------------------------
+// Copyright (C) Bifrost. See AUTHORS.txt for authors.
+//
+// This program is open source and distributed under the New BSD License.
+// See LICENSE.txt for more detail.
+// ---------------------------------------------------------------------------
+
+#ifndef _OPTIXRENDERER_ASSETS_FLAGS_TEST_H_
+#define _OPTIXRENDERER_ASSETS_FLAGS_TEST_H_
+
+#include <Bifrost/Assets/Material.h>
+
+#include <OptiXRenderer/Types.h>
+
+#include <gtest/gtest.h>
+
+namespace OptiXRenderer {
+
+GTEST_TEST(Assets, material_flags_has_same_layout_as_Bifrost) {
+    EXPECT_EQ((int)Bifrost::Assets::MaterialFlag::ThinWalled, OptiXRenderer::Material::Flags::ThinWalled);
+    EXPECT_EQ((int)Bifrost::Assets::MaterialFlag::Cutout, OptiXRenderer::Material::Flags::Cutout);
+}
+
+GTEST_TEST(Assets, shading_models_has_same_layout_as_Bifrost) {
+    EXPECT_EQ((int)Bifrost::Assets::ShadingModel::Default, OptiXRenderer::Material::ShadingModel::Default);
+    EXPECT_EQ((int)Bifrost::Assets::ShadingModel::Diffuse, OptiXRenderer::Material::ShadingModel::Diffuse);
+    EXPECT_EQ((int)Bifrost::Assets::ShadingModel::Count, 2);
+}
+
+} // NS OptiXRenderer
+
+#endif // _OPTIXRENDERER_ASSETS_FLAGS_TEST_H_
