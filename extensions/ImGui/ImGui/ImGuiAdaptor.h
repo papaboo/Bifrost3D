710be21888c20b7329422855af0b411665adfb3a
Asger Hoedt <asger.hoedt@3shape.com>
2025-03-20 18:16:09 +0100

    ImGui: Remove ImGui types from the renderer interface

--- extensions/ImGui/ImGui/ImGuiAdaptor.h
+++ extensions/ImGui/ImGui/ImGuiAdaptor.h
@@ -10,20 +10,18 @@
 #define _BIFROST_IMGUI_ADAPTOR_H_
 
 #include <Bifrost/Input/Keyboard.h>
+#include <Bifrost/Math/Vector.h>
 
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include <ImGui/Src/imgui.h> // Convenience include of ImGui functionality
 
 #include <functional>
-#include <memory>
 #include <vector>
 
 // ------------------------------------------------------------------------------------------------
 // Forward declerations
 // ------------------------------------------------------------------------------------------------
-namespace Bifrost::Core {
-class Engine;
-}
+namespace Bifrost::Core { class Engine; }
 
 namespace ImGui {
 
@@ -71,10 +69,8 @@ static inline void new_frame_callback(Bifrost::Core::Engine& engine, void* adapt
 // ------------------------------------------------------------------------------------------------
 // ImGui utility functions.
 // ------------------------------------------------------------------------------------------------
-inline bool InputUint(const char* label, unsigned int* v, unsigned int step = 1u, unsigned int step_fast = 100u, ImGuiInputTextFlags extra_flags = 0) {
-    // Hexadecimal input provided as a convenience but the flag name is awkward. Typically you'd use InputText() to parse your own data, if you want to handle prefixes.
-    const char* format = (extra_flags & ImGuiInputTextFlags_CharsHexadecimal) ? "%08X" : "%u";
-    return InputScalar(label, ImGuiDataType_U32, (void*)v, (void*)(step > 0 ? &step : NULL), (void*)(step_fast > 0 ? &step_fast : NULL), format, extra_flags);
+inline bool InputUint(const char* label, unsigned int* v, unsigned int step = 1u, unsigned int step_fast = 100u) {
+    return InputScalar(label, ImGuiDataType_U32, (void*)v, (void*)(step > 0 ? &step : NULL), (void*)(step_fast > 0 ? &step_fast : NULL));
 }
 
 struct PlotData {
@@ -84,8 +80,8 @@ struct PlotData {
     const char* label;
 };
 
-void PlotLines(const char* label, PlotData* plots, int plot_count, const char* overlay_text, float scale_min, float scale_max, ImVec2 graph_size);
-void PlotHistograms(const char* label, PlotData* plots, int plot_count, const char* overlay_text, float scale_min, float scale_max, ImVec2 graph_size);
+void PlotLines(const char* label, PlotData* plots, int plot_count, const char* overlay_text, float scale_min, float scale_max, Bifrost::Math::Vector2f graph_size);
+void PlotHistograms(const char* label, PlotData* plots, int plot_count, const char* overlay_text, float scale_min, float scale_max, Bifrost::Math::Vector2f graph_size);
 
 inline void PoppedTreeNode(const char* label, std::function<void()> body) {
     if (ImGui::TreeNode(label)) {
