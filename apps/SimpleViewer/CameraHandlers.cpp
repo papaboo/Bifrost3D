785720a107f86fb05085e63d09a15b1d5009164c
Asger Hoedt <asger.hoedt@3shape.com>
2025-09-03 11:31:08 +0200

    SimpleViewer: Add support for rotating the scene
    Additionally don't set new transforms if the rotation hasn't changed. This allows the path tracer to start converging when the left mouse button is held, but nothing moves.

--- apps/SimpleViewer/CameraHandlers.cpp
+++ apps/SimpleViewer/CameraHandlers.cpp
@@ -40,7 +40,7 @@ void CameraNavigation::navigate(Engine& engine) {
     const Keyboard* keyboard = engine.get_keyboard();
     const Mouse* mouse = engine.get_mouse();
 
-    Transform transform = Cameras::get_transform(m_camera_ID);
+    Transform camera_transform = Cameras::get_transform(m_camera_ID);
 
     { // Translation
         float strafing = 0.0f;
@@ -60,27 +60,43 @@ void CameraNavigation::navigate(Engine& engine) {
             velocity *= 5.0f;
 
         if (strafing != 0.0f || forward != 0.0f) {
-            Vector3f translation_offset = transform.rotation * Vector3f(strafing, 0.0f, forward);
+            Vector3f translation_offset = camera_transform.rotation * Vector3f(strafing, 0.0f, forward);
             float dt = engine.get_time().is_paused() ? engine.get_time().get_raw_delta_time() : engine.get_time().get_smooth_delta_time();
-            transform.translation += normalize(translation_offset) * velocity * dt;
+            camera_transform.translation += normalize(translation_offset) * velocity * dt;
         }
     }
 
     { // Rotation
-        if (mouse->is_pressed(Mouse::Button::Left)) {
+        if (mouse->is_pressed(Mouse::Button::Left) && mouse->get_delta() != Vector2i::zero()) {
 
-            m_vertical_rotation += degrees_to_radians(float(mouse->get_delta().x));
+            float vertical_angle_delta = degrees_to_radians(float(mouse->get_delta().x));
+            float horizontal_angle_delta = degrees_to_radians(float(mouse->get_delta().y));
 
-            // Clamp horizontal rotation to -89 and 89 degrees to avoid turning the camera on it's head and the singularities of cross products at the poles.
-            m_horizontal_rotation -= degrees_to_radians(float(mouse->get_delta().y));
-            m_horizontal_rotation = clamp(m_horizontal_rotation, -PI<float>() * 0.49f, PI<float>() * 0.49f);
+            bool rotate_scene = keyboard->is_pressed(Keyboard::Key::LeftControl) || keyboard->is_pressed(Keyboard::Key::RightControl);
+            if (rotate_scene) {
+                // Rotate the scene root relative to the camera axis.
+                Vector3f vertical_axis = camera_transform.rotation.up();
+                Quaternionf vertical_rotation = Quaternionf::from_angle_axis(-vertical_angle_delta, vertical_axis);
 
-            transform.rotation = Quaternionf::from_angle_axis(m_vertical_rotation, Vector3f::up()) * Quaternionf::from_angle_axis(m_horizontal_rotation, -Vector3f::right());
+                Vector3f horizontal_axis = camera_transform.rotation.right();
+                Quaternionf horizontal_rotation = Quaternionf::from_angle_axis(-horizontal_angle_delta, horizontal_axis);
+
+                SceneRoot scene_root = Cameras::get_scene_ID(m_camera_ID);
+                Transform scene_transform = scene_root.get_root_node().get_local_transform();
+                scene_transform.rotation = horizontal_rotation * vertical_rotation * scene_transform.rotation;
+                scene_root.get_root_node().set_local_transform(scene_transform);
+            } else {
+                // Clamp horizontal rotation to -89 and 89 degrees to avoid turning the camera on it's head and the singularities of cross products at the poles.
+                m_horizontal_rotation = clamp(m_horizontal_rotation - horizontal_angle_delta, -PI<float>() * 0.49f, PI<float>() * 0.49f);
+                m_vertical_rotation += vertical_angle_delta;
+
+                camera_transform.rotation = Quaternionf::from_angle_axis(m_vertical_rotation, Vector3f::up()) * Quaternionf::from_angle_axis(m_horizontal_rotation, -Vector3f::right());
+            }
         }
     }
 
-    if (transform != Cameras::get_transform(m_camera_ID))
-        Cameras::set_transform(m_camera_ID, transform);
+    if (camera_transform != Cameras::get_transform(m_camera_ID))
+        Cameras::set_transform(m_camera_ID, camera_transform);
 
     if (keyboard->was_pressed(Keyboard::Key::Space)) {
         float new_time_scale = engine.get_time().is_paused() ? 1.0f : 0.0f;
