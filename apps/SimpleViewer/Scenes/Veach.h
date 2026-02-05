868b79a3ebeb5d2b3170b513714515508fe117c3
Asger Hoedt <asger.hoedt@3shape.com>
2026-02-05 18:58:56 +0100

    SimpleViewer: Add polygonal light Veach scene

--- apps/SimpleViewer/Scenes/Veach.h
+++ apps/SimpleViewer/Scenes/Veach.h
@@ -24,14 +24,14 @@ using namespace Bifrost;
 
 namespace Scenes {
 
-void create_veach_scene(Core::Engine& engine, Scene::CameraID camera_ID, Scene::SceneRoot scene) {
+void create_veach_scene(Core::Engine& engine, Scene::CameraID camera_ID, Scene::SceneRoot scene, bool use_polygonal_lights) {
     using namespace Bifrost::Assets;
     using namespace Bifrost::Math;
     using namespace Bifrost::Scene;
 
     // Black background if no environment map is specified.
     if (!scene.get_environment_map().exists())
-        scene.set_environment_tint(RGB::black());
+        scene.set_environment_tint(RGB(0.02f));
 
     { // Setup camera transform. Look downwards by 22.5 degress
         Vector3f camera_translation = Vector3f(0.0f, 2.0f, 0.0f);
@@ -70,8 +70,8 @@ void create_veach_scene(Core::Engine& engine, Scene::CameraID camera_ID, Scene::
     { // Create lights.
 
         const int light_count = 4;
-        const RGB light_colors[] = { RGB(1, 1, 0.1f), RGB(0.55f, 1.0f, 0.55f), RGB(0.55f, 0.55f, 1.0f), RGB(1.0f, 0.6f, 0.5f) };
-        
+        const RGB light_colors[light_count] = { RGB(1, 1, 0.1f), RGB(0.55f, 1.0f, 0.55f), RGB(0.55f, 0.55f, 1.0f), RGB(1.0f, 0.6f, 0.5f) };
+
         // Compute light positions.
         Vector3f light_positions[light_count];
         for (int i = 0; i < light_count; ++i) {
@@ -85,13 +85,94 @@ void create_veach_scene(Core::Engine& engine, Scene::CameraID camera_ID, Scene::
 
         // Create lights
         float interlight_distance = light_distance / (light_count - 1);
-        float light_radius = interlight_distance / 4.0f;
-        for (int i = light_count - 1; i >= 0; --i) {
-            Transform light_transform = Transform(light_positions[i]);
-            SceneNode light_node = SceneNode("Light" + std::to_string(i), light_transform);
-            SphereLight(light_node, light_colors[i] * 10, light_radius);
-            light_node.set_parent(scene.get_root_node());
-            light_radius *= 0.333f;
+
+        if (!use_polygonal_lights) {
+            float light_radius = interlight_distance / 4.0f;
+            for (int i = light_count - 1; i >= 0; --i) {
+                Transform light_transform = Transform(light_positions[i]);
+                SceneNode light_node = SceneNode("Light" + std::to_string(i), light_transform);
+                SphereLight(light_node, light_colors[i] * 10, light_radius);
+                light_node.set_parent(scene.get_root_node());
+                light_radius *= 0.333f;
+            }
+        } else {
+
+            Material emissive_material = Material::create_emissive("Emissive", RGB::white());
+            emissive_material.set_flags(MaterialFlag::ThinWalled);
+            Quaternionf quad_rotation = Quaternionf::from_angle_axis(-0.5f * PI<float>(), Vector3f::right());
+
+            { // Small quad light
+                Mesh small_quad_mesh = MeshCreation::plane(1, { MeshFlag::Position });
+                MeshUtils::scale_mesh(small_quad_mesh, interlight_distance * 0.2f);
+                Transform small_quad_light_transform = Transform(light_positions[0], quad_rotation);
+                SceneNode small_quad_light_node = SceneNode("Small quad light", small_quad_light_transform);
+                MeshModel(small_quad_light_node, small_quad_mesh, emissive_material);
+                small_quad_light_node.set_parent(scene.get_root_node());
+            }
+
+            { // Large quad light
+                Mesh large_quad_mesh = MeshCreation::plane(1, { MeshFlag::Position, MeshFlag::Emissive });
+                RGB* emission = large_quad_mesh.get_emission();
+                for (int i = 0; i < 4; ++i)
+                    emission[i] = light_colors[i];
+
+                MeshUtils::scale_mesh(large_quad_mesh, interlight_distance * 0.8f);
+                Transform large_quad_light_transform = Transform(light_positions[1], quad_rotation);
+                SceneNode large_quad_light_node = SceneNode("Large quad light", large_quad_light_transform);
+                MeshModel(large_quad_light_node, large_quad_mesh, emissive_material);
+                large_quad_light_node.set_parent(scene.get_root_node());
+            }
+
+            { // Line light
+                Mesh line_mesh = MeshCreation::plane(1, { MeshFlag::Position });
+                MeshUtils::scale_mesh(line_mesh, Vector3f(interlight_distance * 0.1f, 1.0f, interlight_distance * 0.8f));
+                Transform line_light_transform = Transform(light_positions[2], quad_rotation);
+                SceneNode line_light_node = SceneNode("Line light", line_light_transform);
+                MeshModel(line_light_node, line_mesh, emissive_material);
+                line_light_node.set_parent(scene.get_root_node());
+            }
+
+            { // Star light
+                //       v0
+                //       /\
+                //      /  \
+                // v8--v9--v1--v2
+                //  \          /
+                //   \v7 v5 v3/
+                //   /   /\   \
+                //  / /      \ \
+                //  v6        v4
+
+                Mesh star_mesh = Mesh("Star", 4, 10, { MeshFlag::Position });
+
+                // Create vertex positions by rotating the star in increments of 5.
+                // The outer vertices are called point and the innermost are named shoulders below.
+                float point_distance = interlight_distance * 0.4f;
+                float shoulder_distance = interlight_distance * 0.152786f; // Distance scale constant computed by intersecting the lines [V0, V4] and [V8, V2]
+                Vector3f* positions = star_mesh.get_positions();
+                for (int i = 0; i < 5; ++i) {
+                    float point_angle = 2 * PI<float>() * i / 5.0f;
+                    positions[2 * i] = point_distance * Vector3f(cosf(point_angle), 0.0f, sinf(point_angle));
+
+                    float shoulder_angle = 2 * PI<float>() * (i + 0.5f) / 5.0f;
+                    positions[2 * i + 1] = shoulder_distance * Vector3f(cosf(shoulder_angle), 0.0f, sinf(shoulder_angle));
+                }
+                star_mesh.compute_bounds();
+
+                // Create triangles
+                Vector3ui* triangles = star_mesh.get_primitives();
+                triangles[0] = Vector3ui(0, 9, 1);
+                triangles[1] = Vector3ui(8, 5, 2);
+                triangles[2] = Vector3ui(3, 5, 4);
+                triangles[3] = Vector3ui(5, 7, 6);
+
+                Material star_material = Material::create_emissive("Emissive star", RGB(1, 0.5f, 0));
+                star_material.set_flags(MaterialFlag::ThinWalled);
+                Transform star_light_transform = Transform(light_positions[3], quad_rotation);
+                SceneNode star_light_node = SceneNode("Star light", star_light_transform);
+                MeshModel(star_light_node, star_mesh, star_material);
+                star_light_node.set_parent(scene.get_root_node());
+            }
         }
     }
 
