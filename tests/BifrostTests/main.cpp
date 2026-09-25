55412b73e265ca04eac6fb0e7d0376e48212d446
Asger Hoedt <asger.hoedt@3shape.com>
2026-09-25 09:06:03 +0200

    BifrostTests: Add tests for general area light properties

--- tests/BifrostTests/main.cpp
+++ tests/BifrostTests/main.cpp
@@ -13,6 +13,7 @@
 #include <Assets/Shading/BSDFs/GGXTest.h>
 #include <Assets/Shading/BSDFs/LambertTest.h>
 #include <Assets/Shading/BSDFs/OrenNayarTest.h>
+#include <Assets/Shading/LightSources/AreaLightsTest.h>
 #include <Assets/Shading/LightSources/DiskLightTest.h>
 #include <Assets/Shading/LightSources/SphereLightTest.h>
 #include <Assets/Shading/LightSources/TriangleLightTest.h>
